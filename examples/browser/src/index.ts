a158236b31148f37b317d4b36e3bdcf45692ef3e
Sven Efftinge <sven.efftinge@typefox.io>
2017-03-03 16:45:03 +0100

    [example] reformatted code

--- examples/browser/src/index.ts
+++ examples/browser/src/index.ts
@@ -1,7 +1,7 @@
-import {TheiaApplication, shellModule} from "@theia/shell-dom";
-import {navigatorModule} from "@theia/navigator-dom";
-import {FileSystem, Path, inmemoryModule} from "@theia/fs-common";
-import {Container} from "inversify";
+import { TheiaApplication, shellModule } from "@theia/shell-dom";
+import { navigatorModule } from "@theia/navigator-dom";
+import { FileSystem, Path, inmemoryModule } from "@theia/fs-common";
+import { Container } from "inversify";
 import "@theia/shell-dom/style/index.css";
 import "@theia/navigator-dom/style/index.css";
 
@@ -16,23 +16,23 @@ const application = container.get(TheiaApplication);
 
 window.onload = () => application.start();
 
-const fileSystem = container.get(FileSystem) as FileSystem;
+const fileSystem = container.get<FileSystem>(FileSystem);
 
 let index = 0;
-    let level = 0;
-    const dirName = 'foo';
-    let dirPath = dirName;
-    setInterval(() => {
-        fileSystem.writeFile(Path.fromString(`${dirPath}/Foo_${index}.txt`), 'Hello World');
-        index++;
-        if (index === 10) {
-            index = 0;
-            level++;
-            dirPath += `/${dirName}`;
-        }
-        if (level === 4) {
-            level = 0;
-            dirPath = dirName;
-            fileSystem.rmdir(Path.fromString(dirPath));
-        }
-    }, 500);
+let level = 0;
+const dirName = 'foo';
+let dirPath = dirName;
+setInterval(() => {
+    fileSystem.writeFile(Path.fromString(`${dirPath}/Foo_${index}.txt`), 'Hello World');
+    index++;
+    if (index === 10) {
+        index = 0;
+        level++;
+        dirPath += `/${dirName}`;
+    }
+    if (level === 4) {
+        level = 0;
+        dirPath = dirName;
+        fileSystem.rmdir(Path.fromString(dirPath));
+    }
+}, 500);
