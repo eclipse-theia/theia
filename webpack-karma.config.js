890835e693145ad367def211e8d7590152ad4ccc
Akos Kitta <kittaakos@gmail.com>
2017-03-07 15:29:39 +0100

    Enabled headless Mocha tests. Cleaned up obsolete Karma configs.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- webpack-karma.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(config) {
-    const argv = require('minimist')(process.argv.slice(2));
-    const karma = require('../../config/karma/webpack-karma.config')(config, __dirname, argv);
-    config.set(karma);
-    return karma;
-};
