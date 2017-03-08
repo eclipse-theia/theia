26f679b0461d552594a99213359d84ee092a0701
Sven Efftinge <sven.efftinge@typefox.io>
2017-03-08 16:27:18 +0100

    fixed compile errors

--- src/filesystem/browser/filesystem-client-module.ts
+++ src/filesystem/browser/filesystem-client-module.ts
@@ -1,7 +1,9 @@
 import {ContainerModule} from "inversify";
 import {FileSystem} from "../common";
 import {FileSystemClient} from "../common/messaging/filesystem-client";
-import {createClientWebSocketConnection} from "../../messaging/browser/connection";
+import { createClientWebSocketConnection } from "../../messaging/browser/connection";
+import { CommandContribution, SimpleCommand } from "../../application/common/command";
+import { MenuBarContribution } from "../../application/common/menu";
 
 export const fileSystemClientModule = (url: string) => new ContainerModule(bind => {
     const fileSystemClient = new FileSystemClient();
