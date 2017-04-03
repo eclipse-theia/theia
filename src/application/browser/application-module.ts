3dbdfa8b5e73d6530b31aa9c66c1e458fa089a3b
Als <alspaladin@gmail.com>
2017-04-03 13:59:01 +0500

    moved the bindings to commands and menu contributions from browser menu to browser common

--- src/application/browser/application-module.ts
+++ src/application/browser/application-module.ts
@@ -1,14 +1,17 @@
 import { SelectionService } from '../common/selection-service';
+import { CommonCommandContribution, CommonMenuContribution } from '../common/commands-common';
 import { ContainerModule } from "inversify";
 import { TheiaApplication } from './application';
 import { OpenerService } from "./opener-service";
-import { CommandRegistry } from "../common/command";
-import { MenuModelRegistry } from "../common/menu";
+import { CommandRegistry, CommandContribution } from "../common/command";
+import { MenuModelRegistry, MenuContribution } from "../common/menu";
 
 export const browserApplicationModule = new ContainerModule(bind => {
     bind(TheiaApplication).toSelf().inSingletonScope();
     bind(OpenerService).toSelf().inSingletonScope();
     bind(CommandRegistry).toSelf().inSingletonScope();
+    bind(CommandContribution).to(CommonCommandContribution);
+    bind(MenuContribution).to(CommonMenuContribution);
     bind(MenuModelRegistry).toSelf().inSingletonScope();
     bind(SelectionService).toSelf().inSingletonScope();
 });
--- src/application/browser/menu/menu-module.ts
+++ src/application/browser/menu/menu-module.ts
@@ -1,6 +1,3 @@
-import { CommonCommandContribution, CommonMenuContribution } from '../../common/commands-common';
-import { CommandContribution } from '../../common/command';
-import { MenuContribution } from '../../common/menu';
 import {ContainerModule} from "inversify";
 import {TheiaPlugin} from "../application";
 import { BrowserMenuBarContribution, MainMenuFactory } from "./menu-plugin";
@@ -8,6 +5,4 @@ import { BrowserMenuBarContribution, MainMenuFactory } from "./menu-plugin";
 export const browserMenuModule = new ContainerModule(bind => {
     bind(TheiaPlugin).to(BrowserMenuBarContribution);
     bind(MainMenuFactory).toSelf().inSingletonScope();
-    bind(CommandContribution).to(CommonCommandContribution);
-    bind(MenuContribution).to(CommonMenuContribution);
 });
