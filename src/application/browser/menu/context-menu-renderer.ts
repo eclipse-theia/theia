c9c830295d1c137f148b603728326f4ba048b31a
Akos Kitta <kittaakos@gmail.com>
2017-04-05 14:38:40 +0200

    GH-22: Changed context menu renderer API.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- src/application/browser/menu/context-menu-renderer.ts
+++ src/application/browser/menu/context-menu-renderer.ts
@@ -1,21 +1,31 @@
 import { inject, injectable } from "inversify";
 import { MainMenuFactory } from "./menu-plugin";
 
+export type Anchor = MouseEvent | { x: number, y: number };
+
 export const ContextMenuRenderer = Symbol("ContextMenuRenderer");
 
 export interface ContextMenuRenderer {
-    render(path: string, event: MouseEvent): void;
+    render(path: string, anchor: Anchor): void;
 }
 
 @injectable()
 export class BrowserContextMenuRenderer implements ContextMenuRenderer {
 
     constructor(
-        @inject(MainMenuFactory) private menuFactory: MainMenuFactory) {}
+        @inject(MainMenuFactory) private menuFactory: MainMenuFactory) { }
 
-    render(path: string, event: MouseEvent): void {
+    render(path: string, anchor: Anchor): void {
         const contextMenu = this.menuFactory.createContextMenu(path);
-
-        contextMenu.open(event.clientX, event.clientY);
+        const {x, y} = this.getAnchor(anchor);
+        contextMenu.open(x, y);
     }
+
+    private getAnchor(event: MouseEvent | { x: number, y: number }): { x: number, y: number } {
+        if (event instanceof MouseEvent) {
+            return { x: event.clientX, y: event.clientY };
+        }
+        return event;
+    }
+
 }
