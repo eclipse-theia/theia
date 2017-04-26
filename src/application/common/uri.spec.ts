7550c9616a5067e15d4ea7644cfee4e88d8cbbb9
Akos Kitta <kittaakos@gmail.com>
2017-04-26 16:55:37 +0200

    GH-39: Added 'append' to URI.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- src/application/common/uri.spec.ts
+++ src/application/common/uri.spec.ts
@@ -1,4 +1,3 @@
-import "mocha";
 import * as chai from "chai"
 import URI from "./uri"
 
@@ -13,17 +12,32 @@ before(() => {
 beforeEach(() => {
 });
 
-describe('uri', () => {
+describe("uri", () => {
 
-    describe('01 #getParent', () => {
-        it('should return the parent.', () => {
-            expect(new URI('file:///foo/bar.txt').parent().toString()).equals("file:///foo");
+    describe("01 #getParent", () => {
+        it("should return the parent.", () => {
+            expect(new URI("file:///foo/bar.txt").parent().toString()).equals("file:///foo");
         });
     });
-    describe('01 #lastSegment', () => {
-        it('should return the last segment.', () => {
-            expect(new URI('file:///foo').lastSegment()).equals("foo");
-            expect(new URI('file:///foo/bar.txt').lastSegment()).equals("bar.txt");
+
+    describe("02 #lastSegment", () => {
+        it("should return the last segment.", () => {
+            expect(new URI("file:///foo").lastSegment()).equals("foo");
+            expect(new URI("file:///foo/bar.txt").lastSegment()).equals("bar.txt");
         });
     });
+
+    describe("03 #append", () => {
+        it("should return with this when the the segments array is empty.", () => {
+            const uri = new URI("file:///foo");
+            expect(uri.append()).to.be.equal(uri);
+        });
+        it("should append a single segment.", () => {
+            expect(new URI("file:///foo").append("bar")).to.be.deep.equal(new URI("file:///foo/bar"));
+        })
+        it("should append multiple segments.", () => {
+            expect(new URI("file:///foo").append("bar", "baz")).to.be.deep.equal(new URI("file:///foo/bar/baz"));
+        })
+    });
+
 });
--- src/application/common/uri.ts
+++ src/application/common/uri.ts
@@ -1,19 +1,28 @@
-
-
 export default class URI {
 
-    constructor(private uri: string) {}
-
-    toString() {
-        return this.uri
+    constructor(private uri: string) {
     }
 
     parent(): URI {
-        return new URI(this.uri.substring(0, this.uri.lastIndexOf('/')))
+        return new URI(this.uri.substring(0, this.uri.lastIndexOf('/')));
     }
 
     lastSegment(): string {
         // TODO trim queries and fragments
-        return this.uri.substr(this.uri.lastIndexOf('/') + 1)
+        return this.uri.substr(this.uri.lastIndexOf('/') + 1);
     }
+
+    append(...segments: string[]) {
+        if (!segments || segments.length === 0) {
+            return this;
+        }
+        const copy = segments.slice(0);
+        copy.unshift(this.uri);
+        return new URI(copy.join('/'));
+    }
+
+    toString() {
+        return this.uri;
+    }
+
 }
