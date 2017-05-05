892b609720bfedbe8ff54a2d75eebc3eeb8d7991
Akos Kitta <kittaakos@gmail.com>
2017-05-05 13:13:56 +0200

    GH-62: Reverted `src` module locations to `lib.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- examples/src/web/main.ts
+++ examples/src/web/main.ts
@@ -8,8 +8,8 @@
 import { Container } from "inversify";
 
 import * as main from '../main';
-import { browserClipboardModule } from 'theia/src/application/browser/clipboard/clipboard-module';
-import { browserMenuModule } from "theia/src/application/browser/menu/menu-module";
+import { browserClipboardModule } from 'theia/lib/application/browser/clipboard/clipboard-module';
+import { browserMenuModule } from "theia/lib/application/browser/menu/menu-module";
 
 // Create the browser specific container.
 const container = new Container();
