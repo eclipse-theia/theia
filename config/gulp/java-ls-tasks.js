1beb509d64d048f1b176b68c8cf1eaec5eae1742
Akos Kitta <kittaakos@gmail.com>
2017-05-16 09:10:39 +0200

    GH-10: Fixed gulp tasks. Corrected npm package version.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- config/gulp/java-ls-tasks.js
+++ config/gulp/java-ls-tasks.js
@@ -3,20 +3,20 @@ const gulp = require('gulp');
 const cp = require('child_process');
 const decompress = require('gulp-decompress');
 const download = require('gulp-download');
-const server_dir = '../eclipse.jdt.ls';
-const extension_dir = './lib/java/node/server';
+const serverDir = '../../../eclipse.jdt.ls';
+const extensionDir = '../../lib/java/node/server';
 
 gulp.task('download_java_server', () => {
     download("http://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz")
         .pipe(decompress())
-        .pipe(gulp.dest(extension_dir))
+        .pipe(gulp.dest(extensionDir))
 });
 
 gulp.task('build_java_server', () => {
-    cp.execSync(mvnw() + ' -Pserver-distro clean package', { cwd: server_dir, stdio: [0, 1, 2] });
-    gulp.src(server_dir + '/org.eclipse.jdt.ls.product/distro/*.tar.gz')
+    cp.execSync(mvnw() + ' -Pserver-distro clean package', { cwd: serverDir, stdio: [0, 1, 2] });
+    gulp.src(serverDir + '/org.eclipse.jdt.ls.product/distro/*.tar.gz')
         .pipe(decompress())
-        .pipe(gulp.dest(extension_dir))
+        .pipe(gulp.dest(extensionDir))
 });
 
 gulp.task('dev_java_server', () => {
@@ -31,14 +31,14 @@ gulp.task('dev_java_server', () => {
         command += '-Denvironment.os=win32 -Denvironment.ws=win32 -Denvironment.arch=x86_64';
     }
     console.log('executing ' + command);
-    cp.execSync(command, { cwd: server_dir, stdio: [0, 1, 2] });
-    gulp.src(server_dir + '/org.eclipse.jdt.ls.product/distro/*.tar.gz')
+    cp.execSync(command, { cwd: serverDir, stdio: [0, 1, 2] });
+    gulp.src(serverDir + '/org.eclipse.jdt.ls.product/distro/*.tar.gz')
         .pipe(decompress())
-        .pipe(gulp.dest(extension_dir))
+        .pipe(gulp.dest(extensionDir))
 });
 
 gulp.task('watch_java__server', () => {
-    gulp.watch(server_dir + '/org.eclipse.jdt.ls.core/**/*.java', ['dev_java_server']);
+    gulp.watch(serverDir + '/org.eclipse.jdt.ls.core/**/*.java', ['dev_java_server']);
 });
 
 function isWin() {
