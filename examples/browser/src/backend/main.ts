61a9b381dea187f6faa97db75b6ecfbba842a57e
Brian Ketelsen <bketelsen@gmail.com>
2017-05-18 22:36:23 -0700

    Language Server: add go language server implementation
    Signed-off-by: Brian Ketelsen <bketelsen@gmail.com>

--- examples/browser/src/backend/main.ts
+++ examples/browser/src/backend/main.ts
@@ -15,6 +15,7 @@ import { messagingModule } from "theia-core/lib/messaging/node";
 import { nodeLanguagesModule } from 'theia-core/lib/languages/node';
 import { nodeJavaModule } from 'theia-core/lib/java/node';
 import { nodePythonModule } from 'theia-core/lib/languages/python/node/node-python-module';
+import { nodeGoModule } from 'theia-core/lib/languages/go/node/node-go-module';
 import terminalBackendModule from 'theia-core/lib/terminal/node/terminal-backend-module'
 
 // FIXME introduce default error handler contribution
@@ -42,6 +43,7 @@ container.load(nodeLanguagesModule);
 container.load(terminalBackendModule);
 container.load(nodeJavaModule);
 container.load(nodePythonModule);
+container.load(nodeGoModule);
 container.bind(BackendApplicationContribution).to(StaticServer);
 const application = container.get(BackendApplication);
 application.start();
