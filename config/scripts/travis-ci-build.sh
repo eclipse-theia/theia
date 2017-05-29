53843138f84a058f266215be5425ba5269588377
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-05-29 08:51:48 +0500

    fixed build scripts
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- config/scripts/travis-ci-build.sh
+++ config/scripts/travis-ci-build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 npm install \
-&& npm run build \
 && npm run test \
 && cd config/local-dependency-manager \
 && npm install \
 && cd ../../examples/browser \
-&& npm install \
-&& npm run build \
+&& npm run bootstrap \
 && cd ../electron \
-&& npm install \
-&& npm run build
+&& npm install
