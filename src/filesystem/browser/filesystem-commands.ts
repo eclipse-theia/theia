fc4b20a9bdd51ee3708bc14ecfd923f862832339
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-06-01 16:41:18 +0500

    [fs-commands] turned global functions to protected methods + fixed the rename command
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- src/filesystem/browser/filesystem-commands.ts
+++ src/filesystem/browser/filesystem-commands.ts
@@ -99,19 +99,17 @@ export class FileCommandContribution implements CommandContribution {
 
         registry.registerHandler(
             FileCommands.FILE_RENAME,
-            new FileSystemCommandHandler(this.selectionService, uri => {
-                return this.fileSystem.getFileStat(uri.toString())
-                    .then(stat => {
-                        let dialog = new SingleTextInputDialog('Rename File', {
-                            initialValue: uri.lastSegment,
-                            validate(name) {
-                                return validateFileName(name, stat)
-                            }
-                        })
-                        dialog.open().then(name =>
-                            this.fileSystem.move(uri.toString(), uri.parent.appendPath(name).toString()))
-                    })
-            })
+            new FileSystemCommandHandler(this.selectionService, uri =>
+                this.getParent(uri).then(parent => {
+                    const dialog = new SingleTextInputDialog('Rename File', {
+                        initialValue: uri.lastSegment,
+                        validate: name => this.validateFileName(name, parent)
+                    });
+                    dialog.open().then(name =>
+                        this.fileSystem.move(uri.toString(), uri.parent.appendPath(name).toString())
+                    )
+                })
+            )
         );
 
         registry.registerHandler(
@@ -126,52 +124,46 @@ export class FileCommandContribution implements CommandContribution {
 
         registry.registerHandler(
             FileCommands.FILE_PASTE,
-            new FileSystemCommandHandler(this.selectionService, uri => {
-                let copyPath: URI
-                return getDirectory(uri, this.fileSystem)
-                    .then(stat => {
-                        let data: string = this.clipboardService.getData('text')
-                        copyPath = new URI(data)
-                        let targetUri = uri.appendPath(copyPath.lastSegment)
-                        return this.fileSystem.copy(copyPath.toString(), targetUri.toString())
-                    })
-            }, uri => !this.clipboardService.isEmpty && !!this.clipboardService.getData('text'))
+            new FileSystemCommandHandler(this.selectionService, uri =>
+                this.getDirectory(uri).then(stat => {
+                    const data: string = this.clipboardService.getData('text');
+                    const copyPath = new URI(data);
+                    const targetUri = uri.appendPath(copyPath.lastSegment);
+                    return this.fileSystem.copy(copyPath.toString(), targetUri.toString());
+                }),
+                uri => !this.clipboardService.isEmpty && !!this.clipboardService.getData('text'))
         );
 
         registry.registerHandler(
             FileCommands.NEW_FILE,
-            new FileSystemCommandHandler(this.selectionService, uri => {
-                return getDirectory(uri, this.fileSystem)
-                    .then(stat => {
-                        let freeUri = getFreeChild('Untitled', '.txt', stat)
-                        let dialog = new SingleTextInputDialog(`New File Below '${freeUri.parent.lastSegment}'`, {
-                            initialValue: freeUri.lastSegment,
-                            validate(name) {
-                                return validateFileName(name, stat)
-                            }
-                        })
-                        dialog.open().then(name =>
-                            this.fileSystem.createFile(new URI(stat.uri).appendPath(name).toString()))
+            new FileSystemCommandHandler(this.selectionService, uri =>
+                this.getDirectory(uri).then(parent => {
+                    const freeUri = this.getFreeChild('Untitled', '.txt', parent)
+                    const dialog = new SingleTextInputDialog(`New File Below '${freeUri.parent.lastSegment}'`, {
+                        initialValue: freeUri.lastSegment,
+                        validate: name => this.validateFileName(name, parent)
                     })
-            })
+                    dialog.open().then(name =>
+                        this.fileSystem.createFile(new URI(parent.uri).appendPath(name).toString())
+                    )
+                })
+            )
         );
 
         registry.registerHandler(
             FileCommands.NEW_FOLDER,
-            new FileSystemCommandHandler(this.selectionService, uri => {
-                return getDirectory(uri, this.fileSystem)
-                    .then(stat => {
-                        let freeUri = getFreeChild('Untitled', '', stat)
-                        let dialog = new SingleTextInputDialog(`New Folder Below '${freeUri.parent.lastSegment}'`, {
-                            initialValue: freeUri.lastSegment,
-                            validate(name) {
-                                return validateFileName(name, stat)
-                            }
-                        })
-                        dialog.open().then(name =>
-                            this.fileSystem.createFolder(new URI(stat.uri).appendPath(name).toString()))
-                    })
-            })
+            new FileSystemCommandHandler(this.selectionService, uri =>
+                this.getDirectory(uri).then(stat => {
+                    const freeUri = this.getFreeChild('Untitled', '', stat);
+                    const dialog = new SingleTextInputDialog(`New Folder Below '${freeUri.parent.lastSegment}'`, {
+                        initialValue: freeUri.lastSegment,
+                        validate: name => this.validateFileName(name, stat)
+                    });
+                    dialog.open().then(name =>
+                        this.fileSystem.createFolder(new URI(stat.uri).appendPath(name).toString())
+                    );
+                })
+            )
         )
 
         registry.registerHandler(
@@ -191,51 +183,50 @@ export class FileCommandContribution implements CommandContribution {
             )
         );
     }
-}
 
-/**
- * returns an error message or an empty string if the file name is valid
- * @param name the simple file name to validate
- * @param parent the parent directory's file stat
- */
-function validateFileName(name: string, parent: FileStat): string {
-    if (!name || !name.match(/^[\w\-. ]+$/)) {
-        return "Invalid name, try other"
-    } else {
+    /**
+     * returns an error message or an empty string if the file name is valid
+     * @param name the simple file name to validate
+     * @param parent the parent directory's file stat
+     */
+    protected validateFileName(name: string, parent: FileStat): string {
+        if (!name || !name.match(/^[\w\-. ]+$/)) {
+            return "Invalid name, try other";
+        }
         if (parent.children) {
-            for (let child of parent.children) {
+            for (const child of parent.children) {
                 if (new URI(child.uri).lastSegment === name) {
-                    return 'A file with this name already exists.'
+                    return 'A file with this name already exists.';
                 }
             }
         }
+        return '';
     }
-    return ''
-}
 
-function getDirectory(candidate: URI, fileSystem: FileSystem): Promise<FileStat> {
-    return fileSystem.getFileStat(candidate.toString())
-        .then(stat => {
-            if (!stat || !stat.isDirectory) {
-                // not folder? get parent
-                return fileSystem.getFileStat(new URI(stat.uri).parent.toString())
-            } else {
-                return Promise.resolve(stat)
-            }
-        })
-}
-
-function getFreeChild(prefix: string, suffix: string, fileStat: FileStat): URI {
-    let infixes = ['', ' 1', ' 2', ' 3', ' 4', ' 5']
-    let parentUri = new URI(fileStat.uri)
-    for (let infix of infixes) {
-        let candidate = prefix + infix + suffix
-        let children: FileStat[] = fileStat.children!
-        if (!children.some(stat => new URI(stat.uri).lastSegment === candidate)) {
-            return parentUri.appendPath(candidate)
+    protected async getDirectory(candidate: URI): Promise<FileStat> {
+        const stat = await this.fileSystem.getFileStat(candidate.toString());
+        if (stat.isDirectory) {
+            return stat;
         }
+        return this.getParent(candidate);
     }
-    return parentUri.appendPath(prefix + suffix)
+
+    protected getParent(candidate: URI): Promise<FileStat> {
+        return this.fileSystem.getFileStat(candidate.parent.toString());
+    }
+
+    protected getFreeChild(prefix: string, suffix: string, fileStat: FileStat): URI {
+        const infixes = ['', ' 1', ' 2', ' 3', ' 4', ' 5']
+        const parentUri = new URI(fileStat.uri)
+        for (const infix of infixes) {
+            const candidate = prefix + infix + suffix
+            const children: FileStat[] = fileStat.children!
+            if (!children.some(stat => new URI(stat.uri).lastSegment === candidate)) {
+                return parentUri.appendPath(candidate)
+            }
+        }
+        return parentUri.appendPath(prefix + suffix)
+    }
 }
 
 export class FileSystemCommandHandler implements CommandHandler {
