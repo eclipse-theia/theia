c937ce550721d081ad836c263a0888d03d9cee48
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-06-09 15:20:16 +0500

    added font-awesome
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- config/webpack/rules.js
+++ config/webpack/rules.js
@@ -14,13 +14,17 @@ module.exports = function (dirname) {
             loader: 'style-loader!css-loader'
         },
         {
-            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            test: /\.(ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
             loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
         },
         {
             test: /\.js$/,
             enforce: 'pre',
             loader: 'source-map-loader'
+        },
+        {
+            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
         }
     ];
 };
