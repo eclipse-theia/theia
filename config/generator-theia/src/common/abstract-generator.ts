2798793655c3c32ff807aaa86f79026215aaefa3
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-06-29 15:06:58 +0200

    [generator-theia] configurable copyright
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- config/generator-theia/src/common/abstract-generator.ts
+++ config/generator-theia/src/common/abstract-generator.ts
@@ -56,13 +56,8 @@ export abstract class AbstractGenerator {
     }
 
     protected compileCopyright(): string {
-        return `/*
- * Copyright (C) 2017 TypeFox and others.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- */
-`;
+        const copyright = this.model.config.copyright;
+        return copyright ? copyright + os.EOL : '';
     }
 
     protected commonScripts(target: 'electron' | 'web'): { [name: string]: string } {
