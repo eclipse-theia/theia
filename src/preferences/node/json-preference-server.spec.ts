9524fb7448260290ccc6588a3aa2ef1cf1f0602f
Patrick Guilbert <patrick.pollo.guilbert@ericsson.com>
2017-06-30 15:58:04 -0400

    Add test for CompoundPreferenceServer and refactor test fixtures
    Signed-off-by: Patrick-Jeffrey Pollo Guilbert <patrick.pollo.guilbert@ericsson.com>

--- src/preferences/node/json-preference-server.spec.ts
+++ src/preferences/node/json-preference-server.spec.ts
@@ -6,37 +6,20 @@
  */
 
 import * as chai from 'chai';
+import * as chaiAsPromised from "chai-as-promised";
 import * as fs from 'fs-extra';
 import * as temp from 'temp';
-import URI from '../../application/common/uri';
-import { FileUri } from '../../application/node/file-uri';
-import { FileSystemNode } from "../../filesystem/node/node-filesystem"
-import { FileSystemWatcher, FileSystemWatcherClientListener } from '../../filesystem/common/filesystem-watcher'
-import { ChokidarFileSystemWatcherServer } from '../../filesystem/node/chokidar-filesystem-watcher'
 import { JsonPreferenceServer } from './json-preference-server'
-import { Logger } from '../../application/common/logger'
-import * as chaiAsPromised from "chai-as-promised";
-
+import { FileUri } from '../../application/node/file-uri';
+import { JsonPrefHelper } from '../node/test/preference-stubs'
+import URI from '../../application/common/uri';
 
 const expect = chai.expect;
-const track = temp.track();
 const preferencePath = '.theia/prefs.json';
-
-let prefServer: JsonPreferenceServer;
-let fileWatcher: FileSystemWatcher;
-let fileSystem: FileSystemNode;
-let rootUri: URI;
 let preferenceFileUri: URI;
-const logger = new Proxy<Logger>({} as any, {
-    get: (target, name) => () => {
-        if (name.toString().startsWith('is')) {
-            return Promise.resolve(false);
-        }
-        if (name.toString().startsWith('if')) {
-            return new Promise(resolve => { });
-        }
-    }
-});
+let prefServer: JsonPreferenceServer;
+let helper = new JsonPrefHelper();
+const track = temp.track();
 
 before(() => {
     chai.should();
@@ -44,27 +27,20 @@ before(() => {
     chai.config.showDiff = true;
     chai.config.includeStack = true;
 
-    rootUri = FileUri.create(track.mkdirSync());
+    const rootUri = FileUri.create(track.mkdirSync());
     preferenceFileUri = rootUri.resolve(preferencePath);
     fs.mkdirSync(FileUri.fsPath(rootUri.resolve('.theia')));
-
     fs.writeFileSync(FileUri.fsPath(preferenceFileUri), '{ "showLineNumbers": false }');
 
-    fileSystem = new FileSystemNode();
-    fileWatcher = createFileSystemWatcher();
-    prefServer = new JsonPreferenceServer(fileSystem, fileWatcher, logger, Promise.resolve(preferenceFileUri));
+    prefServer = helper.createJsonPrefServer(preferenceFileUri);
 });
 
 after(() => {
     prefServer.dispose();
-})
+    track.cleanupSync();
+});
 
 describe('json-preference-server', () => {
-
-    after(() => {
-        track.cleanupSync();
-    });
-
     describe('01 #has preference', () => {
 
         it('should return true for the has preference', async () => {
@@ -126,45 +102,40 @@ describe('json-preference-server', () => {
             // Modify the content.
             fs.writeFileSync(FileUri.fsPath(preferenceFileUri), fileContent);
 
-            let { content } = await fileSystem.resolveContent(FileUri.fsPath(preferenceFileUri));
+            let { content } = await helper.getFS().resolveContent(FileUri.fsPath(preferenceFileUri));
             expect(content).to.be.equal(fileContent);
 
             return promise;
 
         }).timeout(20000)
 
-        describe('03 #write invalid json pref file', () => {
 
-            it('should log an error and have undefined prefs', async () => {
+    });
+    describe('03 #write invalid json pref file', () => {
 
-                // Register a simple client
-                let promise: Promise<boolean> = new Promise<boolean>((done) => {
-                    prefServer.setClient({
-                        onDidChangePreference(event) {
-                            expect(event.newValue).to.be.equal(undefined);
-                            done();
-                        }
-                    })
+        it('should log an error and have undefined prefs', async () => {
+
+            // Register a simple client
+            let promise: Promise<boolean> = new Promise<boolean>((done) => {
+                prefServer.setClient({
+                    onDidChangePreference(event) {
+                        expect(event.newValue).to.be.equal(undefined);
+                        done();
+                    }
                 })
+            })
 
-                const fileContent = '{showLineNumbers":tue'; // Invalid json
+            const fileContent = '{showLineNumbers":tue'; // Invalid json
 
-                // Modify the content.
-                fs.writeFileSync(FileUri.fsPath(preferenceFileUri), fileContent);
+            // Modify the content.
+            fs.writeFileSync(FileUri.fsPath(preferenceFileUri), fileContent);
 
-                let { content } = await fileSystem.resolveContent(FileUri.fsPath(preferenceFileUri));
-                expect(content).to.be.equal(fileContent);
+            let { content } = await helper.getFS().resolveContent(FileUri.fsPath(preferenceFileUri));
+            expect(content).to.be.equal(fileContent);
 
-                return promise;
+            return promise;
 
-            }).timeout(20000)
-        });
+        }).timeout(20000)
     });
 });
 
-function createFileSystemWatcher(): FileSystemWatcher {
-    const listener = new FileSystemWatcherClientListener();
-    const server = new ChokidarFileSystemWatcherServer(logger);
-    server.setClient(listener);
-    return new FileSystemWatcher(server, listener);
-}
