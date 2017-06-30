bb24d7f5b16cc786e598e950f99fbc2911027e44
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-06-30 22:21:47 +0300

    [fs] added fs preferences
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- src/filesystem/common/index.ts
+++ src/filesystem/common/index.ts
@@ -5,6 +5,7 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
+export * from './filesystem-preferences';
 export * from './filesystem';
 export * from './filesystem-selection';
 export * from './filesystem-watcher';
