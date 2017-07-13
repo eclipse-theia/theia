43ccca769a9de1c5256436f72d4ff2abb3c36091
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-07-13 17:52:55 +0500

    [restructuring] added filesystem extension
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

---
+++ extensions/filesystem/src/browser/file-dialog/file-dialog-model.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2017 TypeFox and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import { injectable, inject } from "inversify";
+import { Emitter, Event } from "@theia/core/lib/common";
+import { ITreeNode } from "@theia/core/lib/browser";
+import { DirNode, FileNode, FileTreeModel, FileTree, FileTreeServices } from '../file-tree';
+
+@injectable()
+export class FileDialogModel extends FileTreeModel {
+
+    protected readonly onDidOpenFileEmitter = new Emitter<void>();
+
+    constructor(
+        @inject(FileTree) protected readonly tree: FileTree,
+        @inject(FileTreeServices) services: FileTreeServices
+    ) {
+        super(tree, services);
+        this.toDispose.push(this.onDidOpenFileEmitter);
+    }
+
+    get onDidOpenFile(): Event<void> {
+        return this.onDidOpenFileEmitter.event;
+    }
+
+    protected doOpenNode(node: ITreeNode): void {
+        if (FileNode.is(node)) {
+            this.onDidOpenFileEmitter.fire(undefined);
+        } else if (DirNode.is(node)) {
+            this.navigateTo(node);
+        } else {
+            super.doOpenNode(node);
+        }
+    }
+
+}
---
+++ extensions/filesystem/src/browser/file-dialog/file-dialog-widget.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2017 TypeFox and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import { injectable, inject } from "inversify";
+import { ContextMenuRenderer, TreeProps } from "@theia/core/lib/browser";
+import { FileTreeWidget } from "../file-tree";
+import { FileDialogModel } from "./file-dialog-model";
+
+export const FILE_DIALOG_CLASS = 'theia-FileDialog';
+
+@injectable()
+export class FileDialogWidget extends FileTreeWidget {
+
+    constructor(
+        @inject(TreeProps) readonly props: TreeProps,
+        @inject(FileDialogModel) readonly model: FileDialogModel,
+        @inject(ContextMenuRenderer) contextMenuRenderer: ContextMenuRenderer
+    ) {
+        super(props, model, contextMenuRenderer);
+        this.addClass(FILE_DIALOG_CLASS);
+    }
+
+}
