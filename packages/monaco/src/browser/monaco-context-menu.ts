016192fd5c3c339934ae6b28b34453414cd3991c
Sven Efftinge <sven.efftinge@typefox.io>
2017-07-21 15:11:38 +0200

    [monaco] Enabled support for quick fixes
    Signed-off-by: Sven Efftinge <sven.efftinge@typefox.io>

--- packages/monaco/src/browser/monaco-context-menu.ts
+++ packages/monaco/src/browser/monaco-context-menu.ts
@@ -10,6 +10,8 @@ import { EDITOR_CONTEXT_MENU_ID } from "@theia/editor/lib/browser";
 import { ContextMenuRenderer, toAnchor } from "@theia/core/lib/browser";
 import IContextMenuService = monaco.editor.IContextMenuService;
 import IContextMenuDelegate = monaco.editor.IContextMenuDelegate;
+import {Menu} from "@phosphor/widgets"
+import {CommandRegistry} from "@phosphor/commands"
 
 @injectable()
 export class MonacoContextMenuService implements IContextMenuService {
@@ -18,7 +20,35 @@ export class MonacoContextMenuService implements IContextMenuService {
     }
 
     showContextMenu(delegate: IContextMenuDelegate): void {
-        this.contextMenuRenderer.render(EDITOR_CONTEXT_MENU_ID, toAnchor(delegate.getAnchor()));
+        let anchor = toAnchor(delegate.getAnchor());
+        // If it is the general context menu, we want to delegate to our menu registry entirely and ignore the actually passed actions.
+        // Unfortunately checking the existence of certain properties seems to be the best way to tell, what kind of contect menu is requested.
+        if (delegate.hasOwnProperty("getKeyBinding")) {
+            this.contextMenuRenderer.render(EDITOR_CONTEXT_MENU_ID, anchor);
+        } else {
+            delegate.getActions().then( actions => {
+                let commands = new CommandRegistry()
+                const menu = new Menu({
+                    commands
+                })
+
+                for (const action of actions) {
+                    const commandId = 'quickfix_' + actions.indexOf(action)
+                    commands.addCommand(commandId, {
+                        label : action.label,
+                        className : action.class,
+                        isToggled : () => action.checked,
+                        isEnabled : () => action.enabled,
+                        execute : () => action.run()
+                    })
+                    menu.addItem({
+                        type: 'command',
+                        command : commandId
+                    })
+                }
+                menu.open(anchor.x, anchor.y);
+            })
+        }
     }
 
 }
