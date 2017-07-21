faac0e0c4bdc988a71646c2a40f6438b0071caa4
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-07-21 08:29:36 +0500

    [languages] removed a dependency from monaco
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- packages/languages/src/common/console-window.ts
+++ packages/languages/src/common/console-window.ts
@@ -5,9 +5,45 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import { injectable, decorate } from 'inversify';
-import { ConsoleWindow } from 'monaco-languageclient/lib/console-window';
-decorate(injectable(), ConsoleWindow);
-export {
-    ConsoleWindow
-};
+import { injectable } from 'inversify';
+import { MessageActionItem, MessageType } from 'vscode-base-languageclient/lib/protocol';
+import { Window, OutputChannel } from 'vscode-base-languageclient/lib/services';
+
+@injectable()
+export class ConsoleWindow implements Window {
+    protected readonly channels = new Map<string, OutputChannel>();
+    showMessage<T extends MessageActionItem>(type: MessageType, message: string, ...actions: T[]): Thenable<T | undefined> {
+        if (type === MessageType.Error) {
+            console.error(message);
+        }
+        if (type === MessageType.Warning) {
+            console.warn(message);
+        }
+        if (type === MessageType.Info) {
+            console.info(message);
+        }
+        if (type === MessageType.Log) {
+            console.log(message);
+        }
+        return Promise.resolve(undefined);
+    }
+    createOutputChannel(name: string): OutputChannel {
+        const existing = this.channels.get(name);
+        if (existing) {
+            return existing;
+        }
+        const channel: OutputChannel = {
+            append(value: string): void {
+                console.log(name + ': ' + value);
+            },
+            appendLine(line: string): void {
+                console.log(name + ': ' + line);
+            },
+            show(): void {
+                // no-op
+            }
+        }
+        this.channels.set(name, channel)
+        return channel;
+    }
+}
