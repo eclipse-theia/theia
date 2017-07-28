5196c04992568570a720f41d6979a4e9f4ecf833
Akos Kitta <kittaakos@gmail.com>
2017-07-28 10:16:26 +0200

    Checked in generated source for now.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

---
+++ examples/electron/src-gen/frontend/electron-main.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2017 TypeFox and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+// @ts-check
+// Workaround for https://github.com/electron/electron/issues/9225. Chrome has an issue where
+// in certain locales (e.g. PL), image metrics are wrongly computed. We explicitly set the
+// LC_NUMERIC to prevent this from happening (selects the numeric formatting category of the
+// C locale, http://en.cppreference.com/w/cpp/locale/LC_categories).
+if (process.env.LC_ALL) {
+    process.env.LC_ALL = 'C';
+}
+process.env.LC_NUMERIC = 'C';
+
+const electron = require('electron');
+const path = require('path');
+
+electron.app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        electron.app.quit();
+    }
+});
+
+electron.app.on('ready', function () {
+    const mainWindow = new electron.BrowserWindow({ width: 1024, height: 728 });
+    require("../backend/main").then(server => {
+        mainWindow.loadURL(`file://${path.join(__dirname, '../../lib/index.html')}?port=${server.address().port}`);
+        mainWindow.webContents.openDevTools();
+    }).catch(() => {
+        electron.app.exit(1);
+    });
+    mainWindow.on('closed', function () {
+        electron.app.exit(0);
+    });
+});
---
+++ examples/electron/src-gen/frontend/index.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2017 TypeFox and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+// @ts-check
+const { Container } = require('inversify');
+const { FrontendApplication } = require('@theia/core/lib/browser');
+const { frontendApplicationModule } = require('@theia/core/lib/browser/frontend-application-module');
+const { messagingFrontendModule } = require('@theia/core/lib/browser/messaging/messaging-frontend-module');
+const { loggerFrontendModule } = require('@theia/core/lib/browser/logger-frontend-module');
+
+const container = new Container();
+container.load(frontendApplicationModule);
+container.load(messagingFrontendModule);
+container.load(loggerFrontendModule);
+
+function load(raw) {
+    return Promise.resolve(raw.default).then(module =>
+        container.load(module)
+    )
+}
+
+function start() {
+    const application = container.get(FrontendApplication);
+    application.start();
+}
+
+module.exports = Promise.resolve()
+    .then(function () { return Promise.resolve(require('@theia/core/lib/electron-browser/menu/electron-menu-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/core/lib/electron-browser/clipboard/electron-clipboard-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/filesystem/lib/browser/filesystem-frontend-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/workspace/lib/browser/workspace-frontend-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/preferences/lib/browser/preference-frontend-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/navigator/lib/browser/navigator-frontend-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/terminal/lib/browser/terminal-frontend-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/editor/lib/browser/editor-frontend-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/languages/lib/browser/languages-frontend-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/monaco/lib/electron-browser/monaco-electron-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/java/lib/browser/java-frontend-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/python/lib/browser/python-frontend-module')).then(load) })
+    .then(function () { return Promise.resolve(require('@theia/cpp/lib/browser/cpp-frontend-module')).then(load) })
+    .then(start).catch(reason => {
+        console.error('Failed to start the frontend application.');
+        if (reason) {
+            console.error(reason);
+        }
+    });
