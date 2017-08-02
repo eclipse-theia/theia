6bd4c5c019dbea9ce1ef118d1415728ffacedb75
Jan Bicker <jan.bicker@typefox.io>
2017-08-02 16:20:09 +0200

    Connected to server and edited styles
    Signed-off-by: Jan Bicker <jan.bicker@typefox.io>

--- packages/extension-manager/src/browser/extension-frontend-module.ts
+++ packages/extension-manager/src/browser/extension-frontend-module.ts
@@ -11,8 +11,10 @@ import { ExtensionServer, extensionPath } from '../common/extension-protocol';
 import { ExtensionManager } from '../common';
 import { ExtensionContribution } from './extension-contribution';
 import { ExtensionWidget } from './extension-widget';
+// import { ExtensionServerMock } from './extension-server-mock';
 
 export default new ContainerModule(bind => {
+    // bind(ExtensionServer).to(ExtensionServerMock).inSingletonScope();
     bind(ExtensionServer).toDynamicValue(ctx => {
         const provider = ctx.container.get(WebSocketConnectionProvider);
         return provider.createProxy<ExtensionServer>(extensionPath);
