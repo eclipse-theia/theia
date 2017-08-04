377c1dbc39552957931788834ba4f4425979c3ad
Jan Bicker <jan.bicker@typefox.io>
2017-08-04 13:26:37 +0200

    Implemented extension detail service and widget.
    Signed-off-by: Jan Bicker <jan.bicker@typefox.io>

--- packages/extension-manager/src/browser/extension-detail-widget.ts
+++ packages/extension-manager/src/browser/extension-detail-widget.ts
@@ -5,14 +5,86 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import { Widget } from "@theia/core/lib/browser";
-import { ResolvedRawExtension } from "../common/extension-protocol"
+import { Extension, ResolvedExtension } from '../common/extension-manager';
+import { Message } from "@phosphor/messaging/lib";
+import { VirtualWidget, VirtualRenderer } from "@theia/core/lib/browser";
+import { h } from "@phosphor/virtualdom/lib";
 
-export class ExtensionDetailWidget extends Widget {
+export class ExtensionDetailWidget extends VirtualWidget {
 
-    constructor(id: string, resolvedRawExtension: ResolvedRawExtension) {
+    constructor(id: string, protected resolvedExtension: ResolvedExtension) {
         super();
         this.id = id;
+        this.addClass('theia-extension-detail');
+        this.title.closable = true;
+        this.title.label = resolvedExtension.name;
+    }
+
+    protected onActivateRequest(msg: Message): void {
+        super.onActivateRequest(msg);
+        // this.node.focus();
+        this.update();
+    }
+
+    protected onCloseRequest(msg: Message): void {
+        super.onCloseRequest(msg);
+        this.dispose();
+    }
+
+    protected render(): h.Child {
+        const d = h.div;
+        const c = (cn: string) => ({ className: cn });
+        const r = this.resolvedExtension;
+
+        const name = h.h2(c('extensionName'), r.name);
+        const extversion = d(c('extensionVersion'), r.version);
+        const author = d(c('extensionAuthor'), r.author);
+        const titleInfo = d(c('extensionSubtitle flexcontainer'), author, extversion);
+        const titleContainer = d(c('extensionTitleContainer flexcontainer'),
+            name, titleInfo);
+
+        const description = d(c('extensionDescription'), r.description);
+
+        const buttonContainer = d(c('extensionButtonContainer flexcontainer'),
+            VirtualRenderer.flatten(this.createButtons(this.resolvedExtension)));
+
+        const headerContainer = d(c('extensionHeaderContainer flexcontainer'),
+            titleContainer, description, buttonContainer);
+
+        const documentation = d(c('extensionDocumentation'), r.documentation);
+        const docContainer = d(c('extensionDocContainer flexcontainer'), documentation);
+
+        return [headerContainer, docContainer];
+    }
+
+    protected createButtons(extension: Extension): h.Child[] {
+        const buttonArr = [];
+        let btnLabel = 'Install';
+        if (extension.installed) {
+            btnLabel = 'Uninstall';
+        }
+
+        buttonArr.push(h.div({
+            className: 'extensionButton' + (extension.installed ? ' installed' : ''),
+            onclick: event => {
+                if (extension.installed) {
+                    extension.uninstall();
+                } else {
+                    extension.install();
+                }
+            }
+        }, btnLabel));
+
+        if (extension.outdated) {
+            buttonArr.push(h.div({
+                className: 'extensionButton outdated',
+                onclick: event => {
+                    extension.update();
+                }
+            }, 'Update'));
+        }
+
+        return buttonArr;
     }
 
 }
