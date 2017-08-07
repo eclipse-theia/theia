3b79638745992045ed4a1c774a79e342efdd61c5
Akos Kitta <kittaakos@gmail.com>
2017-08-07 18:25:36 +0200

    GH-147: Removed ports and standalone selenium from install phase.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- examples/electron/wdio.conf.js
+++ examples/electron/wdio.conf.js
@@ -107,9 +107,10 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: ['selenium-standalone'],
-
     seleniumArgs: {
-        seleniumArgs: ["-port", "4444", "-Djna.nosys=true"]
+        seleniumArgs: [
+            '-Djna.nosys=true'
+        ]
     },
 
     //
