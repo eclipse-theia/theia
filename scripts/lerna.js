7b23d1a569bf5e787dd706b14a0b9060604b394a
Akos Kitta <kittaakos@gmail.com>
2017-08-07 10:10:48 +0200

    Adapted main package.json to use lerna runner.
    Instead of directly invoking it from the npm task.
    
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- scripts/lerna.js
+++ scripts/lerna.js
@@ -5,7 +5,6 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 // @ts-check
-const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
 
