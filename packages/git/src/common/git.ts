2d9e3025a170e1762e9eb665ab9983be62837d13
Akos Kitta <kittaakos@gmail.com>
2017-08-08 17:42:05 +0200

    GH-165: Added support for getting `git status`.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- packages/git/src/common/git.ts
+++ packages/git/src/common/git.ts
@@ -1,3 +1,45 @@
+/*
+ * Copyright (C) 2017 TypeFox and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import { WorkingDirectoryStatus } from './model';
+
 export interface Git {
+
+    /**
+     * Returns with the working directory status of the given Git repository.
+     */
+    status(repository: Repository): Promise<WorkingDirectoryStatus>;
+
+}
+
+/**
+ * Bare minimum representation of a local Git clone.
+ */
+export interface Repository {
+
+    /**
+     * The remote URL of the local clone.
+     */
+    readonly remoteUrl?: string;
+
+    /**
+     * The FS URI of the local clone.
+     */
+    readonly localUri: string;
+
+}
+
+export namespace Repository {
+
+    /**
+     * `true` if the argument is a type of a [Repository](#Repository), otherwise `false`.
+     */
+    export function is(repository: any | undefined): repository is Repository {
+        return repository && typeof (<Repository>repository).localUri === 'string';
+    }
 
 }
--- packages/git/src/node/dugite-git.ts
+++ packages/git/src/node/dugite-git.ts
@@ -1,5 +1,50 @@
-import { Git } from '../common/git';
+/*
+ * Copyright (C) 2017 TypeFox and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
 
+import URI from '@theia/core/lib/common/uri';
+import { status } from 'dugite-extra/lib/command';
+import { FileUri } from '@theia/core/lib/node/file-uri';
+import { Git, Repository, WorkingDirectoryStatus, FileChange, FileStatus } from '../common';
+import { IStatusResult, IAheadBehind, WorkingDirectoryStatus as DugiteStatus, FileChange as DugiteFileChange } from 'dugite-extra/lib/model';
+
+/**
+ * `dugite-extra` based Git implementation.
+ */
 export class DugiteGit implements Git {
 
+    status(repository: Repository): Promise<WorkingDirectoryStatus> {
+        const path = FileUri.fsPath(new URI(repository.localUri));
+        return status(path).then(result => mapStatus(result));
+    }
+
+}
+
+function mapStatus(toMap: IStatusResult): WorkingDirectoryStatus {
+    return {
+        exists: toMap.exists,
+        branch: toMap.currentBranch,
+        upstreamBranch: toMap.currentUpstreamBranch,
+        aheadBehind: mapAheadBehind(toMap.branchAheadBehind),
+        changes: mapFileChanges(toMap.workingDirectory)
+    };
+}
+
+function mapAheadBehind(toMap: IAheadBehind | undefined): { ahead: number, behind: number } | undefined {
+    return toMap ? { ...toMap } : undefined;
+}
+
+function mapFileChanges(toMap: DugiteStatus): FileChange[] {
+    return toMap.files.map(mapFileChange);
+}
+
+function mapFileChange(toMap: DugiteFileChange): FileChange {
+    return {
+        uri: FileUri.create(toMap.path),
+        status: FileStatus.Conflicted,
+        oldUri: toMap.oldPath ? FileUri.create(toMap.oldPath) : undefined
+    };
 }
