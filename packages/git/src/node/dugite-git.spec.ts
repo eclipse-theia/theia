c1c26f2837581f7cf3f8df08f606d2ed95caf1fe
Akos Kitta <kittaakos@gmail.com>
2017-08-10 18:52:42 +0200

    GH-165: Added first status tests.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- packages/git/src/node/dugite-git.spec.ts
+++ packages/git/src/node/dugite-git.spec.ts
@@ -5,29 +5,75 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
+import * as fs from 'fs';
 import * as chai from 'chai';
+import * as path from 'path';
+import * as temp from 'temp';
+import { FileStatus } from '../common';
 import { DugiteGit } from './dugite-git';
+import { setupRepository } from './test/fixture-helper';
+import { FileUri } from '@theia/core/lib/node/file-uri';
 
 const expect = chai.expect;
+const track = temp.track();
+
 const git = new DugiteGit();
 
 describe('dugite-git', async () => {
 
-    it('status 01 - missing', async () => {
-        try {
-            await git.status({ localUri: 'missing' });
-        } catch (error) {
-            expect(error.message).to.be.equal('Unable to find path to repository on disk.');
-        }
+
+    after(async () => {
+        track.cleanupSync();
     });
 
-    it('status 02 - untouched', async () => {
-        try {
-            const result = await git.status({ localUri: '' });
-            console.log(result);
-        } catch (error) {
-            expect(error.message).to.be.equal('Unable to find path to repository on disk.');
-        }
+    describe('status', async () => {
+
+        let repositoryLocation: string;
+
+        beforeEach(async () => {
+            repositoryLocation = setupRepository('git_repo_01', track.mkdirSync());
+        });
+
+        it('missing', async () => {
+            try {
+                await git.status({ localUri: 'missing' });
+                throw new Error('Expected an error when getting the status of an absent Git clone.');
+            } catch (error) {
+                expect(error.message).to.be.equal('Unable to find path to repository on disk.');
+            }
+        });
+
+        it('clean', async () => {
+            const result = await git.status({ localUri: repositoryLocation });
+            expect(result).to.be.not.undefined;
+            expect(result.changes).to.be.empty;
+        });
+
+        it('added', async () => {
+            const newFilePath = path.join(repositoryLocation, 'foo.txt');
+            fs.writeFileSync(newFilePath, 'foo');
+            expect(fs.existsSync(newFilePath)).to.be.true;
+
+            const result = await git.status({ localUri: repositoryLocation });
+            expect(result).to.be.not.undefined;
+            expect(result.changes).to.be.not.empty;
+            expect(result.changes[0].status).to.be.equal(FileStatus.New);
+            expect(FileUri.fsPath(result.changes[0].uri)).to.be.equal(newFilePath);
+        });
+
+        it('deleted', async () => {
+            const deletedFilePath = path.join(repositoryLocation, 'A.txt');
+            fs.unlinkSync(deletedFilePath);
+            expect(fs.existsSync(deletedFilePath)).to.be.false;
+
+            const result = await git.status({ localUri: repositoryLocation });
+            expect(result).to.be.not.undefined;
+            expect(result.changes).to.be.not.empty;
+            expect(result.changes[0].status).to.be.equal(FileStatus.Deleted);
+            expect(FileUri.fsPath(result.changes[0].uri)).to.be.equal(deletedFilePath);
+        });
+
     });
+
 
 });
--- packages/git/src/node/dugite-git.ts
+++ packages/git/src/node/dugite-git.ts
@@ -5,10 +5,11 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
+import * as path from 'path';
 import URI from '@theia/core/lib/common/uri';
 import { status } from 'dugite-extra/lib/command';
 import { FileUri } from '@theia/core/lib/node/file-uri';
-import { Git, Repository, WorkingDirectoryStatus, FileChange, FileStatus } from '../common';
+import { Git, Repository, WorkingDirectoryStatus, FileChange } from '../common';
 import { IStatusResult, IAheadBehind, WorkingDirectoryStatus as DugiteStatus, FileChange as DugiteFileChange } from 'dugite-extra/lib/model';
 
 /**
@@ -18,18 +19,18 @@ export class DugiteGit implements Git {
 
     status(repository: Repository): Promise<WorkingDirectoryStatus> {
         const path = FileUri.fsPath(new URI(repository.localUri));
-        return status(path).then(result => mapStatus(result));
+        return status(path).then(result => mapStatus(result, repository));
     }
 
 }
 
-function mapStatus(toMap: IStatusResult): WorkingDirectoryStatus {
+function mapStatus(toMap: IStatusResult, repository: Repository): WorkingDirectoryStatus {
     return {
         exists: toMap.exists,
         branch: toMap.currentBranch,
         upstreamBranch: toMap.currentUpstreamBranch,
         aheadBehind: mapAheadBehind(toMap.branchAheadBehind),
-        changes: mapFileChanges(toMap.workingDirectory)
+        changes: mapFileChanges(toMap.workingDirectory, repository)
     };
 }
 
@@ -37,14 +38,14 @@ function mapAheadBehind(toMap: IAheadBehind | undefined): { ahead: number, behin
     return toMap ? { ...toMap } : undefined;
 }
 
-function mapFileChanges(toMap: DugiteStatus): FileChange[] {
-    return toMap.files.map(mapFileChange);
+function mapFileChanges(toMap: DugiteStatus, repository: Repository): FileChange[] {
+    return toMap.files.map(file => mapFileChange(file, repository));
 }
 
-function mapFileChange(toMap: DugiteFileChange): FileChange {
+function mapFileChange(toMap: DugiteFileChange, repository: Repository): FileChange {
     return {
-        uri: FileUri.create(toMap.path),
-        status: FileStatus.Conflicted,
+        uri: FileUri.create(path.join(repository.localUri, toMap.path)),
+        status: toMap.status,
         oldUri: toMap.oldPath ? FileUri.create(toMap.oldPath) : undefined
     };
 }
