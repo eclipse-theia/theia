79d07c4ad6735a7be5301b6343b4a5f3723511be
Patrick-Jeffrey Pollo Guilbert <patrick.pollo.guilbert@ericsson.com>
2017-08-18 15:07:03 -0400

    [Electron] Adds shortcut to open/close electron dev tools
    Fixes #87
    
    Signed-off-by: Patrick-Jeffrey Pollo Guilbert <patrick.pollo.guilbert@ericsson.com>

---
+++ packages/core/src/electron-browser/menu/electron-commands.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017 Ericsson and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import { injectable } from "inversify"
+import { CommandContribution, CommandRegistry } from "../../common"
+import * as electron from "electron"
+
+@injectable()
+export class ElectronCommandHandlers implements CommandContribution {
+
+    constructor(
+    ) { }
+
+    registerCommands(registry: CommandRegistry): void {
+        registry.registerCommand({
+            id: 'theia.electron.open.dev.tools',
+            label: 'Open Electron Developer Tools'
+        });
+        registry.registerHandler('theia.electron.open.dev.tools', {
+            execute: (): any => {
+                const webContent = electron.remote.getCurrentWebContents();
+                if (!webContent.isDevToolsOpened()) {
+                    webContent.openDevTools();
+                } else {
+                    webContent.closeDevTools();
+                }
+                return null;
+            }
+        });
+
+    }
+}
