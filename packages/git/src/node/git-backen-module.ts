8fe15a13043370c1c98330f5bdb0cd0e28c6586d
Akos Kitta <kittaakos@gmail.com>
2017-08-23 11:47:40 +0200

    GH-165: Configured bindings.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- packages/git/src/node/git-backen-module.ts
+++ packages/git/src/node/git-backen-module.ts
@@ -5,13 +5,28 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import { Git } from '../common/git';
+import { Git, GitPath } from '../common/git';
+import { GitWatcher, GitWatcherClient, GitWatcherServer } from '../common/git-watcher';
 import { DugiteGit } from './dugite-git';
+import { DugiteGitWatcherServer } from './dugite-git-watcher';
 import { ContainerModule } from 'inversify';
 import { bindGitPreferences } from '../common/git-preferences';
+import { ConnectionHandler, JsonRpcConnectionHandler } from "@theia/core/lib/common";
 
 export default new ContainerModule(bind => {
     bindGitPreferences(bind);
     bind(DugiteGit).toSelf().inSingletonScope();
     bind(Git).toDynamicValue(ctx => ctx.container.get(DugiteGit)).inSingletonScope();
+    bind(ConnectionHandler).toDynamicValue(context => new JsonRpcConnectionHandler(GitPath, () => context.container.get(Git))).inSingletonScope();
+    bind(DugiteGitWatcherServer);
+    bind(GitWatcherServer).toDynamicValue(context => context.container.get(DugiteGitWatcherServer));
+    bind(GitWatcher).toSelf();
+    bind(ConnectionHandler).toDynamicValue(context =>
+        new JsonRpcConnectionHandler<GitWatcherClient>(GitPath, client => {
+            const server = context.container.get<GitWatcherServer>(GitWatcherServer);
+            server.setClient(client);
+            client.onDidCloseConnection(() => server.dispose());
+            return server;
+        })
+    ).inSingletonScope();
 });
