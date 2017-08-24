54f73d1f939883c59c0a8acf5875659cc51b8884
Jan Bicker <jan.bicker@typefox.io>
2017-08-24 18:16:52 +0200

    GH-165: Made the linter happy again.
    Signed-off-by: Jan Bicker <jan.bicker@typefox.io>

--- packages/git/src/browser/git-widget.ts
+++ packages/git/src/browser/git-widget.ts
@@ -17,12 +17,12 @@ import { FileChange, FileStatus, Repository } from '../common/model';
 export class GitWidget extends VirtualWidget {
 
     protected localUri = new URI('/Users/jbicker/github/theia').toString();
-    protected repository: Repository = {localUri: this.localUri};
+    protected repository: Repository = { localUri: this.localUri };
 
     protected stagedChanges: FileChange[] = [];
     protected unstagedChanges: FileChange[] = [];
 
-    constructor(@inject(Git) private git: Git) {
+    constructor( @inject(Git) private git: Git) {
         super();
         this.id = 'gitContainer';
         this.title.label = 'Git';
@@ -33,18 +33,19 @@ export class GitWidget extends VirtualWidget {
         this.createChangeLists();
     }
 
-    async createChangeLists(): void {
+    protected createChangeLists(): void {
         this.stagedChanges = [];
         this.unstagedChanges = [];
-        const status = await this.git.status(this.repository);
-        status.changes.forEach(change => {
-            if (change.staged) {
-                this.stagedChanges.push(change);
-            } else {
-                this.unstagedChanges.push(change);
-            }
+        this.git.status(this.repository).then(status => {
+            status.changes.forEach(change => {
+                if (change.staged) {
+                    this.stagedChanges.push(change);
+                } else {
+                    this.unstagedChanges.push(change);
+                }
+            });
+            this.update();
         });
-        this.update();
     }
 
     protected render(): h.Child {
@@ -56,59 +57,59 @@ export class GitWidget extends VirtualWidget {
         const stagedChanges = this.renderStagedChanges();
         const unstagedChanges = this.renderUnstagedChanges();
 
-        return h.div({id: 'gitContainer'}, repoList, commandBar, messageInput, stagedChanges, unstagedChanges);
+        return h.div({ id: 'gitContainer' }, repoList, commandBar, messageInput, stagedChanges, unstagedChanges);
     }
 
     protected renderRepoList(): h.Child {
-        return h.div({id: 'repositories'}, 'Repo. Nothing here yet');
+        return h.div({ id: 'repositories' }, 'Repo. Nothing here yet');
     }
 
     protected renderCommandBar(): h.Child {
 
-        return h.div({id: 'commandBar'}, 'Commands. Nothing here yet');
+        return h.div({ id: 'commandBar' }, 'Commands. Nothing here yet');
     }
 
     protected renderMessageInput(): h.Child {
-        const input = h.input({placeholder: 'Commit message'});
-        return h.div({id: 'messageInput', className: 'flexcontainer'}, input);
+        const input = h.input({ placeholder: 'Commit message' });
+        return h.div({ id: 'messageInput', className: 'flexcontainer' }, input);
     }
 
     protected renderGitItemButtons(change: FileChange): h.Child {
-        let btns: h.Child[] = [];
+        const btns: h.Child[] = [];
         if (change.staged) {
             btns.push(h.div({
                 className: 'button', onclick: event => {
                     this.git.rm(this.repository, change.uri);
                 }
-            }, h.i({className: 'fa fa-minus'})));
+            }, h.i({ className: 'fa fa-minus' })));
         } else {
             btns.push(h.div({
                 className: 'button', onclick: event => {
                 }
-            }, h.i({className: 'fa fa-undo'})));
+            }, h.i({ className: 'fa fa-undo' })));
             btns.push(h.div({
                 className: 'button', onclick: event => {
                     this.git.add(this.repository, change.uri)
                 }
-            }, h.i({className: 'fa fa-plus'})));
+            }, h.i({ className: 'fa fa-plus' })));
         }
-        return h.div({className: 'buttons'}, btns);
+        return h.div({ className: 'buttons' }, VirtualRenderer.flatten(btns));
     }
 
-    protected renderGitItem(change): h.Child {
+    protected renderGitItem(change: FileChange): h.Child {
         const uri: URI = new URI(change.uri);
-        const nameSpan = h.span({className: 'name'}, uri.displayName + ' ');
-        const pathSpan = h.span({className: 'path'}, uri.path.dir.toString());
-        const nameAndPathDiv = h.div({className: 'noWrapInfo'}, nameSpan, pathSpan);
+        const nameSpan = h.span({ className: 'name' }, uri.displayName + ' ');
+        const pathSpan = h.span({ className: 'path' }, uri.path.dir.toString());
+        const nameAndPathDiv = h.div({ className: 'noWrapInfo' }, nameSpan, pathSpan);
         const buttonsDiv = this.renderGitItemButtons(change);
         const staged = change.staged ? 'staged ' : '';
-        const statusDiv = h.div({className: 'status ' + staged + FileStatus[change.status].toLowerCase()}, FileStatus[change.status].charAt(0));
-        const itemBtnsAndStatusDiv = h.div({className: 'itemButtonsContainer'}, buttonsDiv, statusDiv);
-        return h.div({className: 'gitItem'}, nameAndPathDiv, itemBtnsAndStatusDiv);
+        const statusDiv = h.div({ className: 'status ' + staged + FileStatus[change.status].toLowerCase() }, FileStatus[change.status].charAt(0));
+        const itemBtnsAndStatusDiv = h.div({ className: 'itemButtonsContainer' }, buttonsDiv, statusDiv);
+        return h.div({ className: 'gitItem' }, nameAndPathDiv, itemBtnsAndStatusDiv);
     }
 
-    protected renderChangesHeader(title: string) {
-        const stagedChangesHeaderDiv = h.div({className: 'changesHeader'}, title);
+    protected renderChangesHeader(title: string): h.Child {
+        const stagedChangesHeaderDiv = h.div({ className: 'changesHeader' }, title);
         return stagedChangesHeaderDiv;
     }
 
