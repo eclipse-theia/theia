c7ba7a686fa4df8bf0dd6431d73426d4502cbd96
Remy Suen <remy.suen@gmail.com>
2017-08-28 04:31:02 +0900

    Fix #412 Correct typos in the placeholder test cases
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

--- packages/editor/src/package.spec.ts
+++ packages/editor/src/package.spec.ts
@@ -7,8 +7,8 @@
 
 /* note: this bogus test file is required so that
    we are able to run mocha unit tests on this
-   package, wihout having any actual unit tests in it.
-   This way a coverage report will be genarated,
+   package, without having any actual unit tests in it.
+   This way a coverage report will be generated,
    showing 0% coverage, instead of no report.
    This file can be removed once we have real unit
    tests in place. */
