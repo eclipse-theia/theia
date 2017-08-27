2092670a02dc496d53a492996ed12882efb895ad
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-08-27 12:28:10 +0200

    [preferences-api] remove usages of ajv
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- packages/preferences-api/src/common/preference-contribution.ts
+++ packages/preferences-api/src/common/preference-contribution.ts
@@ -7,8 +7,6 @@
 
 import { inject, injectable, named } from "inversify";
 import { ContributionProvider, ILogger } from '@theia/core/lib/common';
-import * as ajv from 'ajv';
-
 
 export const PreferenceContribution = Symbol("PreferenceContribution");
 export interface PreferenceContribution {
@@ -31,17 +29,9 @@ export class PreferenceSchemaProvider {
     constructor(
         @inject(ILogger) protected readonly logger: ILogger,
         @inject(ContributionProvider) @named(PreferenceContribution)
-        protected readonly preferenceContributions: ContributionProvider<PreferenceContribution>,
+        protected readonly preferenceContributions: ContributionProvider<PreferenceContribution>
     ) {
-
         this.preferenceContributions.getContributions().forEach(contrib => {
-
-            try {
-                ajv().compile(contrib.schema);
-            } catch (error) {
-                this.logger.error("Invalid json schemas: ", error);
-            }
-
             for (const property in contrib.schema) {
                 if (this.combinedSchema.properties[property]) {
                     this.logger.error("Preference name collision detected in the schema for property: " + property);
