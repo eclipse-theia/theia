1a73d90df0a763c71194ca4fc42aaeaaf00ee744
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2017-08-28 16:21:21 -0400

    Language Server: add go language server integration
    This adds support for "go-langserver". At this moment, it is possible
    to "Go to definition".
    
    For best results, GOPATH needs to be set, see:
    https://github.com/sourcegraph/go-langserver/issues/185
    
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

---
+++ packages/go/src/browser/go-client-contribution.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2017 Ericsson and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import { injectable, inject } from "inversify";
+import { BaseLanguageClientContribution, Workspace, Languages, LanguageClientFactory } from '@theia/languages/lib/browser';
+import { GO_LANGUAGE_ID, GO_LANGUAGE_NAME } from '../common';
+
+@injectable()
+export class GoClientContribution extends BaseLanguageClientContribution {
+
+    readonly id = GO_LANGUAGE_ID;
+    readonly name = GO_LANGUAGE_NAME;
+
+    constructor(
+        @inject(Workspace) protected readonly workspace: Workspace,
+        @inject(Languages) protected readonly languages: Languages,
+        @inject(LanguageClientFactory) protected readonly languageClientFactory: LanguageClientFactory
+    ) {
+        super(workspace, languages, languageClientFactory)
+    }
+
+    protected get documentSelector() {
+        return [
+            'go'
+        ];
+    }
+
+    protected get globPatterns() {
+        return [
+            '**/*.go'
+        ];
+    }
+
+
+}
