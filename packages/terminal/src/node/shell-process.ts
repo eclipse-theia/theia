0d9772cfd5ceb4a75c913528b6f074c79967c827
Antoine Tremblay <antoine.tremblay@ericsson.com>
2017-08-28 14:51:37 -0400

    Add ShellProcess class
    This class manages the creation of a ShellProcess, meaning this manages
    creation of a shell like bash or cmd.exe on windows.
    
    It uses the SHELL env var by default on linux to find the shell to execute
    or cmd.exe on windows.
    
    Signed-off-by: Antoine Tremblay <antoine.tremblay@ericsson.com>

---
+++ packages/terminal/src/node/shell-process.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2017 Ericsson and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import { injectable, inject } from 'inversify';
+import * as process from 'process';
+import { ILogger } from '@theia/core/lib/common/logger';
+import { TerminalProcess, TerminalProcessOptions } from '@theia/core/lib/node/process/terminal-process';
+import { isWindows } from "@theia/core/lib/common";
+
+export const ShellProcessFactory = Symbol("ShellProcessFactory");
+export type ShellProcessFactory = (options: ShellProcessOptions) => ShellProcess;
+
+export const ShellProcessOptions = Symbol("ShellProcessOptions");
+export interface ShellProcessOptions {
+    shell?: string,
+    cols?: number,
+    rows?: number
+}
+
+@injectable()
+export class ShellProcess extends TerminalProcess {
+
+    protected static defaultCols = 80;
+    protected static defaultRows = 24;
+
+    constructor(
+        @inject(ShellProcessOptions) options: ShellProcessOptions,
+        @inject(ILogger) logger: ILogger
+    ) {
+        super(<TerminalProcessOptions>{
+            command: options.shell || ShellProcess.getShellExecutablePath(),
+            args: [],
+            options: {
+                name: 'xterm-color',
+                cols: options.cols || ShellProcess.defaultCols,
+                rows: options.rows || ShellProcess.defaultRows,
+                cwd: process.cwd(),
+                env: process.env as any
+            }
+        }, logger);
+    }
+
+    protected static getShellExecutablePath(): string {
+        if (isWindows) {
+            return 'cmd.exe';
+        } else {
+            return process.env.SHELL!;
+        }
+    }
+}
