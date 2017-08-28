4b15e5d0a209439a7bdd807267bf66e825b88d66
Antoine Tremblay <antoine.tremblay@ericsson.com>
2017-08-28 14:51:37 -0400

    Add ShellProcess class
    This class manages the creation of a ShellProcess, meaning this manages
    creation of a shell like bash or cmd.exe on windows.
    
    It uses the SHELL env var by default on linux to find the shell to execute
    or cmd.exe on windows.
    
    Signed-off-by: Antoine Tremblay <antoine.tremblay@ericsson.com>

--- packages/terminal/src/node/terminal-backend-module.ts
+++ packages/terminal/src/node/terminal-backend-module.ts
@@ -5,10 +5,25 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import { ContainerModule } from 'inversify';
+import { ContainerModule, Container } from 'inversify';
 import { BackendApplicationContribution } from '@theia/core/lib/node';
 import { TerminalBackendContribution } from "./terminal-backend-contribution";
+import { ShellProcess, ShellProcessFactory, ShellProcessOptions } from './shell-process';
+import { ILogger } from '@theia/core/lib/common/logger';
 
 export default new ContainerModule(bind => {
-    bind(BackendApplicationContribution).to(TerminalBackendContribution)
+    bind(BackendApplicationContribution).to(TerminalBackendContribution);
+    bind(ShellProcess).toSelf().inTransientScope();
+    bind(ShellProcessFactory).toFactory(ctx =>
+        (options: ShellProcessOptions) => {
+            const child = new Container({ defaultScope: 'Singleton' });
+            child.parent = ctx.container;
+
+            const logger = ctx.container.get<ILogger>(ILogger);
+            const loggerChild = logger.child({ 'module': 'terminal-backend' });
+            child.bind(ShellProcessOptions).toConstantValue({});
+            child.bind(ILogger).toConstantValue(loggerChild);
+            return child.get(ShellProcess);
+        }
+    );
 });
