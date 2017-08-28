a94c35ecb2ec00e57db4817382001f433a70f7a2
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-08-28 10:10:44 +0200

    [quickopen] added typings for the monaco quick open widget
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- packages/monaco/src/typings/monaco/index.d.ts
+++ packages/monaco/src/typings/monaco/index.d.ts
@@ -200,6 +200,56 @@ declare module monaco.keybindings {
         export function toString(key: any): string;
     }
 
+    export class ResolvedKeybindingPart {
+        readonly ctrlKey: boolean;
+        readonly shiftKey: boolean;
+        readonly altKey: boolean;
+        readonly metaKey: boolean;
+    
+        readonly keyLabel: string;
+        readonly keyAriaLabel: string;
+
+        constructor(ctrlKey: boolean, shiftKey: boolean, altKey: boolean, metaKey: boolean, kbLabel: string, kbAriaLabel: string);
+    }
+    export abstract class ResolvedKeybinding {
+        /**
+         * This prints the binding in a format suitable for displaying in the UI.
+         */
+        public abstract getLabel(): string;
+        /**
+         * This prints the binding in a format suitable for ARIA.
+         */
+        public abstract getAriaLabel(): string;
+        /**
+         * This prints the binding in a format suitable for electron's accelerators.
+         * See https://github.com/electron/electron/blob/master/docs/api/accelerator.md
+         */
+        public abstract getElectronAccelerator(): string;
+        /**
+         * This prints the binding in a format suitable for user settings.
+         */
+        public abstract getUserSettingsLabel(): string;
+        /**
+         * Is the user settings label reflecting the label?
+         */
+        public abstract isWYSIWYG(): boolean;
+    
+        /**
+         * Is the binding a chord?
+         */
+        public abstract isChord(): boolean;
+    
+        /**
+         * Returns the firstPart, chordPart that should be used for dispatching.
+         */
+        public abstract getDispatchParts(): [string, string];
+        /**
+         * Returns the firstPart, chordPart of the keybinding.
+         * For simple keybindings, the second element will be null.
+         */
+        public abstract getParts(): [ResolvedKeybindingPart, ResolvedKeybindingPart];
+    }
+
 }
 
 declare module monaco.services {
@@ -209,4 +259,182 @@ declare module monaco.services {
         executeCommand<T>(commandId: string, ...args: any[]): monaco.Promise<T>;
         executeCommand(commandId: string, ...args: any[]): monaco.Promise<any>;
     }
+}
+
+declare module monaco.quickOpen {
+
+    export class QuickOpenWidget implements IDisposable {
+        constructor(container: HTMLElement, callbacks: IQuickOpenCallbacks, options: IQuickOpenOptions, usageLogger?: IQuickOpenUsageLogger);
+        dispose(): void;
+        create(): HTMLElement;
+        setInput(input: IModel<any>, autoFocus: IAutoFocus, ariaLabel?: string): void;
+        layout(dimension: monaco.editor.IDimension): void;
+        show(prefix: string, options?: IShowOptions): void;
+        hide(reason?: HideReason): void;
+    }
+
+    export enum HideReason {
+        ELEMENT_SELECTED,
+        FOCUS_LOST,
+        CANCELED
+    }
+    export interface IQuickOpenCallbacks {
+        onOk: () => void;
+        onCancel: () => void;
+        onType: (lookFor: string) => void;
+        onShow?: () => void;
+        onHide?: (reason: HideReason) => void;
+        onFocusLost?: () => boolean /* veto close */;
+    }
+    export interface IQuickOpenOptions /* extends IQuickOpenStyles */ {
+        minItemsToShow?: number;
+        maxItemsToShow?: number;
+        inputPlaceHolder?: string;
+        inputAriaLabel?: string;
+        // actionProvider?: IActionProvider;
+        keyboardSupport?: boolean;
+    }
+    export interface IQuickOpenUsageLogger {
+        publicLog(eventName: string, data?: any): void;
+    }
+
+    export interface IShowOptions {
+        quickNavigateConfiguration?: IQuickNavigateConfiguration;
+        autoFocus?: IAutoFocus;
+        inputSelection?: IRange;
+    }
+
+    export interface IQuickNavigateConfiguration {
+        keybindings: monaco.keybindings.ResolvedKeybinding[];
+    }
+    export interface IAutoFocus {
+
+        /**
+         * The index of the element to focus in the result list.
+         */
+        autoFocusIndex?: number;
+
+        /**
+         * If set to true, will automatically select the first entry from the result list.
+         */
+        autoFocusFirstEntry?: boolean;
+
+        /**
+         * If set to true, will automatically select the second entry from the result list.
+         */
+        autoFocusSecondEntry?: boolean;
+
+        /**
+         * If set to true, will automatically select the last entry from the result list.
+         */
+        autoFocusLastEntry?: boolean;
+
+        /**
+         * If set to true, will automatically select any entry whose label starts with the search
+         * value. Since some entries to the top might match the query but not on the prefix, this
+         * allows to select the most accurate match (matching the prefix) while still showing other
+         * elements.
+         */
+        autoFocusPrefixMatch?: string;
+    }
+    export enum Mode {
+        PREVIEW,
+        OPEN,
+        OPEN_IN_BACKGROUND
+    }
+    export interface IEntryRunContext {
+        event: any;
+        keymods: number[];
+        quickNavigateConfiguration: IQuickNavigateConfiguration;
+    }
+    export interface IDataSource<T> {
+        getId(entry: T): string;
+        getLabel(entry: T): string;
+    }
+    /**
+     * See vs/base/parts/tree/browser/tree.ts - IRenderer
+     */
+    export interface IRenderer<T> {
+        getHeight(entry: T): number;
+        getTemplateId(entry: T): string;
+        renderTemplate(templateId: string, container: HTMLElement, styles: any): any;
+        renderElement(entry: T, templateId: string, templateData: any, styles: any): void;
+        disposeTemplate(templateId: string, templateData: any): void;
+    }
+    export interface IFilter<T> {
+        isVisible(entry: T): boolean;
+    }
+    export interface IAccessiblityProvider<T> {
+        getAriaLabel(entry: T): string;
+    }
+    export interface IRunner<T> {
+        run(entry: T, mode: Mode, context: IEntryRunContext): boolean;
+    }
+    export interface IModel<T> {
+        entries: T[];
+        dataSource: IDataSource<T>;
+        renderer: IRenderer<T>;
+        runner: IRunner<T>;
+        filter?: IFilter<T>;
+        accessibilityProvider?: IAccessiblityProvider<T>;
+    }
+
+    export interface IHighlight {
+        start: number;
+        end: number;
+    }
+    export interface IIconLabelOptions {
+        title?: string;
+        extraClasses?: string[];
+        italic?: boolean;
+        matches?: monaco.filters.IMatch[];
+    }    
+    export class QuickOpenEntry {
+        constructor(highlights?: IHighlight[]);
+        getLabel(): string;
+        getLabelOptions(): IIconLabelOptions;
+        getAriaLabel(): string;
+        getDetail(): string;
+        getIcon(): string;
+        getDescription(): string;
+        getKeybinding(): monaco.keybindings.ResolvedKeybinding;
+        getResource(): Uri;
+        isHidden(): boolean;
+        setHidden(hidden: boolean): void;
+        setHighlights(labelHighlights: IHighlight[], descriptionHighlights?: IHighlight[], detailHighlights?: IHighlight[]): void;
+        getHighlights(): [IHighlight[] /* Label */, IHighlight[] /* Description */, IHighlight[] /* Detail */];
+        run(mode: Mode, context: IEntryRunContext): boolean;
+        static compare(elementA: QuickOpenEntry, elementB: QuickOpenEntry, lookFor: string): number;
+        static highlight(entry: QuickOpenEntry, lookFor: string, fuzzyHighlight?: boolean): { labelHighlights: IHighlight[], descriptionHighlights: IHighlight[] };
+    }
+    export class QuickOpenEntryGroup extends QuickOpenEntry {
+        constructor(entry?: QuickOpenEntry, groupLabel?: string, withBorder?: boolean);
+        getGroupLabel(): string;
+        setGroupLabel(groupLabel: string): void;
+        showBorder(): boolean;
+        setShowBorder(showBorder: boolean): void;
+    }
+    export class QuickOpenModel implements IModel<QuickOpenEntry>, IDataSource<QuickOpenEntry>, IFilter<QuickOpenEntry>, IRunner<QuickOpenEntry> {
+        constructor(entries?: QuickOpenEntry[] /*, actionProvider?: IActionProvider */);
+        addEntries(entries: QuickOpenEntry[]): void;
+        entries: QuickOpenEntry[];
+        dataSource: IDataSource<QuickOpenEntry>;
+        renderer: IRenderer<QuickOpenEntry>;
+        runner: IRunner<QuickOpenEntry>;
+        filter?: IFilter<QuickOpenEntry>;
+        accessibilityProvider?: IAccessiblityProvider<QuickOpenEntry>;
+        getId(entry: QuickOpenEntry): string;
+        getLabel(entry: QuickOpenEntry): string;
+        isVisible(entry: QuickOpenEntry): boolean;
+        run(entry: QuickOpenEntry, mode: Mode, context: IEntryRunContext): boolean;
+    }
+
+}
+
+declare module monaco.filters {
+    export interface IMatch {
+        start: number;
+        end: number;
+    }    
+    export function matchesFuzzy(word: string, wordToMatchAgainst: string, enableSeparateSubstringMatching?: boolean): IMatch[] | undefined;
 }
