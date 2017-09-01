0086ae1e6438dbc05804769ffc5bf0c330ca5083
Antoine Tremblay <antoine.tremblay@ericsson.com>
2017-09-01 13:56:33 -0400

    Refactor the terminal widget to use new services.
    This patch refactors the terminal widget so that it uses the shell service
    to create and resize the terminal and uses the terminal backend contribution at
    /services/terminals to connect to an active terminal.
    
    Signed-off-by: Antoine Tremblay <antoine.tremblay@ericsson.com>

--- packages/terminal/src/node/terminal-backend-contribution.ts
+++ packages/terminal/src/node/terminal-backend-contribution.ts
@@ -6,67 +6,21 @@
  */
 
 import * as http from 'http';
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
 import * as stream from 'stream';
 import { injectable, inject } from 'inversify';
 import URI from "@theia/core/lib/common/uri";
 import { ILogger } from "@theia/core/lib/common";
+import { TerminalProcess } from "@theia/core/lib/node/process/terminal-process";
+import { ProcessManager } from "@theia/core/lib/node/process/process-manager";
 import { BackendApplicationContribution } from '@theia/core/lib/node';
 import { openSocket } from '@theia/core/lib/node';
-import { ShellProcess, ShellProcessFactory } from './shell-process'
 
 @injectable()
 export class TerminalBackendContribution implements BackendApplicationContribution {
-    private terminals: Map<number, ShellProcess> = new Map()
 
-    constructor( @inject(ShellProcessFactory) protected readonly shellFactory: ShellProcessFactory,
+    constructor(
+        @inject(ProcessManager) protected readonly processManager: ProcessManager,
         @inject(ILogger) protected readonly logger: ILogger) {
-    }
-
-    configure(app: express.Application): void {
-        app.post('/services/terminals', bodyParser.json({ type: 'application/json' }), (req, res) => {
-            const cols = parseInt(req.query.cols, 10);
-            const rows = parseInt(req.query.rows, 10);
-            let term: ShellProcess;
-            try {
-                term = this.shellFactory({ 'cols': cols, 'rows': rows });
-
-                term.onError(error => {
-                    this.logger.error(`Terminal pid: ${term.pid} error: ${error}, closing it.`);
-                    this.closeTerminal(term);
-                });
-
-                const root: { uri?: string } | undefined = req.body;
-                if (root && root.uri) {
-                    const uri = new URI(root.uri);
-                    term.write(`cd ${uri.path} && `);
-                    term.write("source ~/.profile\n");
-                }
-
-                const pid = term.pid;
-                this.terminals.set(pid, term);
-                res.send(pid.toString());
-                res.end();
-            } catch (error) {
-                this.logger.error(`Error while creating terminal: ${error}`);
-                res.send(-1);
-                res.end();
-            }
-        });
-
-        app.post('/services/terminals/:pid/size', (req, res) => {
-            let pid = parseInt(req.params.pid, 10),
-                cols = parseInt(req.query.cols, 10),
-                rows = parseInt(req.query.rows, 10),
-                term = this.terminals.get(pid)
-            if (term) {
-                term.resize(cols, rows);
-            } else {
-                console.error("Couldn't resize terminal " + pid + ", because it doesn't exist.")
-            }
-            res.end();
-        });
     }
 
     onStart(server: http.Server): void {
@@ -78,8 +32,8 @@ export class TerminalBackendContribution implements BackendApplicationContributi
             }
         }, (ws, request) => {
             const uri = new URI(request.url!)
-            const pid = parseInt(uri.path.base, 10)
-            const term = this.terminals.get(pid)
+            const id = parseInt(uri.path.base, 10)
+            const term = this.processManager.get(id);
             if (!term) {
                 return;
             }
@@ -97,16 +51,13 @@ export class TerminalBackendContribution implements BackendApplicationContributi
             term.output.pipe(termStream);
 
             ws.on('message', (msg: any) => {
-                term.write(msg)
-            })
+                if (term instanceof TerminalProcess) {
+                    term.write(msg)
+                }
+            });
             ws.on('close', (msg: any) => {
-                this.closeTerminal(term);
-            })
-        })
-    }
-
-    protected closeTerminal(term: ShellProcess) {
-        this.terminals.delete(term.pid);
-        term.dispose();
+                term.dispose();
+            });
+        });
     }
 }
