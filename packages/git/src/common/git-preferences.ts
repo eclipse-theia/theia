dc3686c350d9f395317b0154bda1fa93786023c0
Akos Kitta <kittaakos@gmail.com>
2017-09-08 17:42:24 +0200

    GH-165: Adjusted Git preferences to the current preferences API.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- packages/git/src/common/git-preferences.ts
+++ packages/git/src/common/git-preferences.ts
@@ -1,12 +1,13 @@
 /*
  * Copyright (C) 2017 TypeFox and others.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
 import { interfaces } from 'inversify';
 import { createPreferenceProxy, PreferenceProxy, PreferenceService } from '@theia/preferences-api';
+import { PreferenceSchema } from '@theia/preferences-api/lib/common/';
 
 export interface GitConfiguration {
 
@@ -21,11 +22,22 @@ const DefaultGitConfiguration: GitConfiguration = {
     'git.pollInterval': 1000
 };
 
+export const GitPreferenceSchema: PreferenceSchema = {
+    'type': 'object',
+    'properties': {
+        'git.pollInterval': {
+            'type': 'number',
+            'minimum': 100,
+            'description': 'The time-interval (in milliseconds) to poll for the status changes in the local working directory.'
+        }
+    }
+};
+
 export const GitPreferences = Symbol('GitPreferences');
 export type GitPreferences = PreferenceProxy<GitConfiguration>;
 
 function createGitPreferences(preferences: PreferenceService): GitPreferences {
-    return createPreferenceProxy(preferences, DefaultGitConfiguration);
+    return createPreferenceProxy(preferences, DefaultGitConfiguration, GitPreferenceSchema);
 }
 
 export function bindGitPreferences(bind: interfaces.Bind): void {
