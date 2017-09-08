2c8c17bba8fac8e450aaaef344809cdb2b89dcf7
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-09-08 06:56:32 +0200

    [shell] use the JLab copyright
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- packages/core/src/browser/shell.ts
+++ packages/core/src/browser/shell.ts
@@ -1,9 +1,6 @@
-/*
- * Copyright (C) 2017 TypeFox and others.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- */
+
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
 
 import { ArrayExt, each, find, toArray } from "@phosphor/algorithm";
 import { ISignal, Signal } from "@phosphor/signaling";
