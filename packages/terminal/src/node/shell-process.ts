5ed6ae785f5d4b5e516938d3de3199e960060780
Antoine Tremblay <antoine.tremblay@ericsson.com>
2017-09-14 15:14:26 -0400

    Make child loggers for the terminal singletons
    This makes it so that class injected with a child logger doesn't need to be
    disposable.
    
    Since a child logger is created for the whole package I think this is
    acceptable.
    
    Signed-off-by: Antoine Tremblay <antoine.tremblay@ericsson.com>

--- packages/terminal/src/node/shell-process.ts
+++ packages/terminal/src/node/shell-process.ts
@@ -5,7 +5,7 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import { injectable, inject } from 'inversify';
+import { injectable, inject, named } from 'inversify';
 import * as process from 'process';
 import { ILogger } from '@theia/core/lib/common/logger';
 import { TerminalProcess, TerminalProcessOptions, ProcessManager } from '@theia/process/lib/node';
@@ -41,7 +41,7 @@ export class ShellProcess extends TerminalProcess {
     constructor(
         @inject(ShellProcessOptions) options: ShellProcessOptions,
         @inject(ProcessManager) processManager: ProcessManager,
-        @inject(ILogger) logger: ILogger
+        @inject(ILogger) @named("terminal") logger: ILogger
     ) {
         super(<TerminalProcessOptions>{
             command: options.shell || ShellProcess.getShellExecutablePath(),
