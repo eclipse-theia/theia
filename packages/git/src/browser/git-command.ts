2bb922e855537433a4449ff303e5a8a54fdd93f7
Jan Bicker <jan.bicker@typefox.io>
2017-09-26 09:59:42 +0200

    GH-165: Command menu implemented
    Signed-off-by: Jan Bicker <jan.bicker@typefox.io>

--- packages/git/src/browser/git-command.ts
+++ packages/git/src/browser/git-command.ts
@@ -1,11 +1,14 @@
+import { Repository } from '../../lib/common';
 /*
- * Copyright (C) 2017 TypeFox and others.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- */
+* Copyright (C) 2017 TypeFox and others.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+*/
 
 import { injectable, inject } from "inversify";
+import { Git } from '../common';
+import { GitRepositoryProvider } from './git-repository-provider';
 import { CommandContribution, CommandRegistry, ILogger } from "@theia/core/lib/common";
 
 export namespace GIT_COMMANDS {
@@ -31,6 +34,8 @@ export namespace GIT_COMMANDS {
 export class GitCommandHandlers implements CommandContribution {
 
     constructor(
+        @inject(Git) protected readonly git: Git,
+        @inject(GitRepositoryProvider) protected readonly gitRepositoryProvider: GitRepositoryProvider,
         @inject(ILogger) protected readonly logger: ILogger
     ) { }
 
@@ -38,7 +43,9 @@ export class GitCommandHandlers implements CommandContribution {
         registry.registerCommand(GIT_COMMANDS.FETCH);
         registry.registerHandler(GIT_COMMANDS.FETCH.id, {
             execute: (): any => {
-                this.logger.info('FETCH');
+                this.gitRepositoryProvider.getSelected().then(repo => {
+                    this.git.fetch(repo);
+                });
             },
             isEnabled: () => true
         });
@@ -46,7 +53,9 @@ export class GitCommandHandlers implements CommandContribution {
         registry.registerCommand(GIT_COMMANDS.PULL);
         registry.registerHandler(GIT_COMMANDS.PULL.id, {
             execute: (): any => {
-                this.logger.info('PULL');
+                this.gitRepositoryProvider.getSelected().then(repo => {
+                    this.git.pull(repo);
+                });
             },
             isEnabled: () => true
         });
@@ -54,7 +63,17 @@ export class GitCommandHandlers implements CommandContribution {
         registry.registerCommand(GIT_COMMANDS.MERGE);
         registry.registerHandler(GIT_COMMANDS.MERGE.id, {
             execute: (): any => {
-                this.logger.info('MERGE');
+                let repo: Repository;
+                this.gitRepositoryProvider.getSelected()
+                    .then(r => {
+                        repo = r;
+                        return this.git.branch(repo, { type: 'current' });
+                    })
+                    .then(branch => {
+                        if (branch && typeof branch === 'string') {
+                            this.git.merge(repo, branch);
+                        }
+                    });
             },
             isEnabled: () => true
         });
@@ -62,7 +81,9 @@ export class GitCommandHandlers implements CommandContribution {
         registry.registerCommand(GIT_COMMANDS.PUSH);
         registry.registerHandler(GIT_COMMANDS.PUSH.id, {
             execute: (): any => {
-                this.logger.info('PUSH');
+                this.gitRepositoryProvider.getSelected().then(repo => {
+                    this.git.push(repo);
+                });
             },
             isEnabled: () => true
         });
