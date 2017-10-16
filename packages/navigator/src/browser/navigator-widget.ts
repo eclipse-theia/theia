e693f1d99827f27aafcd86470f4941c5d1680096
Sven Efftinge <sven.efftinge@typefox.io>
2017-10-16 16:14:46 +0200

    [navigator] Preserve tree between sessions (see #534)
    Signed-off-by: Sven Efftinge <sven.efftinge@typefox.io>

--- packages/navigator/src/browser/navigator-widget.ts
+++ packages/navigator/src/browser/navigator-widget.ts
@@ -9,12 +9,14 @@ import { injectable, inject } from "inversify";
 import { Message } from "@phosphor/messaging";
 import URI from "@theia/core/lib/common/uri";
 import { CommandService } from '@theia/core/lib/common/command';
-import { ContextMenuRenderer, TreeProps, ITreeModel } from "@theia/core/lib/browser";
+import { ContextMenuRenderer, TreeProps, ITreeModel, StatefulWidget, ITreeNode } from '@theia/core/lib/browser';
 import { FileTreeWidget } from "@theia/filesystem/lib/browser";
 import { FileNavigatorModel } from "./navigator-model";
 import { FileIconProvider } from '@theia/filesystem/lib/browser/icons/file-icons';
 import { h } from "@phosphor/virtualdom/lib";
 import { WorkspaceCommands } from '@theia/workspace/lib/browser/workspace-frontend-contribution';
+import { FileNavigatorTree } from './navigator-tree';
+import { ICompositeTreeNode } from '../../../core/lib/browser/tree/tree';
 
 export const FILE_STAT_NODE_CLASS = 'theia-FileStatNode';
 export const DIR_NODE_CLASS = 'theia-DirNode';
@@ -25,11 +27,54 @@ export const LABEL = 'Files';
 export const CLASS = 'theia-Files';
 
 @injectable()
-export class FileNavigatorWidget extends FileTreeWidget {
+export class FileNavigatorWidget extends FileTreeWidget implements StatefulWidget {
+
+    private deflateForStorage(node: ITreeNode): object {
+        const copy = Object.assign({}, node) as any;
+        if (copy.uri) {
+            copy.uri = copy.uri.toString();
+        }
+        if (copy.parent) {
+            delete copy.parent;
+        }
+        if (ICompositeTreeNode.is(node)) {
+            copy.children = []
+            for (const child of node.children) {
+                copy.children.push(this.deflateForStorage(child));
+            }
+        }
+        return copy;
+    }
+
+    private inflateFromStorage(node: any, parent?: ITreeNode): ITreeNode {
+        if (parent) {
+            node.parent = parent;
+        }
+        if (node.uri) {
+            node.uri = new URI(node.uri);
+        }
+        if (node.selected) {
+            node.selected = false;
+        }
+        if (Array.isArray(node.children)) {
+            for (const child of node.children as ITreeNode[]) {
+                this.inflateFromStorage(child, node);
+            }
+        }
+        return node;
+    }
+
+    storeState(): object {
+        return this.deflateForStorage(this.tree.root!);
+    }
+    restoreState(oldState: object): void {
+        this.tree.root = this.inflateFromStorage(oldState);
+    }
 
     constructor(
         @inject(TreeProps) readonly props: TreeProps,
         @inject(FileNavigatorModel) readonly model: FileNavigatorModel,
+        @inject(FileNavigatorTree) readonly tree: FileNavigatorTree,
         @inject(ContextMenuRenderer) contextMenuRenderer: ContextMenuRenderer,
         @inject(FileIconProvider) iconProvider: FileIconProvider,
         @inject(CommandService) protected readonly commandService: CommandService
