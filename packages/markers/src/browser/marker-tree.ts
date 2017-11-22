84ddef8cd44a6593e3d2ee118bb321277bc57643
Sven Efftinge <sven.efftinge@typefox.io>
2017-11-22 19:02:33 +0100

    removed unused symbol import
    Signed-off-by: Sven Efftinge <sven.efftinge@typefox.io>

--- packages/markers/src/browser/marker-tree.ts
+++ packages/markers/src/browser/marker-tree.ts
@@ -5,7 +5,7 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import { injectable, inject } from "inversify";
+import { injectable } from "inversify";
 import { Tree, ICompositeTreeNode, ITreeNode, ISelectableTreeNode, IExpandableTreeNode } from "@theia/core/lib/browser";
 import { MarkerManager } from './marker-manager';
 import { Marker } from '../common/marker';
