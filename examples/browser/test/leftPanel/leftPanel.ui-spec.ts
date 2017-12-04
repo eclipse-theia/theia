f1fdc2fe48ffd713d0f6b51604da726333878105
jdhananjaya <jahnavi.dhananjaya@gmail.com>
2017-12-04 13:31:38 -0500

    Browser UI test cases updated.
    Signed-off-by: jdhananjaya <jahnavi.dhananjaya@gmail.com>

---
+++ examples/browser/test/leftPanel/leftPanel.ui-spec.ts
@@ -0,0 +1,14 @@
+import { LeftPanelMain } from "./leftPanelMain"
+
+describe('theia left panel elements loading', () => {
+
+    let leftPanelMain: LeftPanelMain;
+
+    before(() => {
+
+        leftPanelMain = new LeftPanelMain();
+    });
+
+
+
+}); 
