e3814508d79964ce637797b495a29b6119077ce7
Simon Marchi <simon.marchi@ericsson.com>
2018-01-11 13:24:50 -0500

    Make raw-process handle non-executable paths gracefully
    When Node's child_process.spawn tries to execute a non-executable path,
    it throws an error, rather than calling the error handler registered
    with on('error').  We don't handle this currently.
    
    This patch tries to normalize the error handling by catching errors
    thrown by child_process.spawn and calling the error handler with it.
    
    I added a test for that.
    
    Signed-off-by: Simon Marchi <simon.marchi@ericsson.com>

--- packages/process/src/node/raw-process.ts
+++ packages/process/src/node/raw-process.ts
@@ -24,6 +24,21 @@ export interface RawProcessFactory {
     (options: RawProcessOptions): RawProcess;
 }
 
+/* A Node stream like /dev/null.
+
+   Writing goes to a black hole, reading returns EOF.  */
+class DevNullStream extends stream.Duplex {
+    // tslint:disable-next-line:no-any
+    _write(chunk: any, encoding: string, callback: (err?: Error) => void): void {
+        callback();
+    }
+
+    _read(size: number): void {
+        // tslint:disable-next-line:no-null-keyword
+        this.push(null);
+    }
+}
+
 @injectable()
 export class RawProcess extends Process {
 
@@ -42,17 +57,34 @@ export class RawProcess extends Process {
             + ` with args : ${options.args}, `
             + ` options ${JSON.stringify(options.options)}`);
 
-        this.process = spawn(
-            options.command,
-            options.args,
-            options.options);
+        /* spawn can throw exceptions, for example if the file is not
+           executable, it throws an error with EACCES.  Here, we try to
+           normalize the error handling by calling the error handler
+           instead.  */
+        try {
+            this.process = spawn(
+                options.command,
+                options.args,
+                options.options);
 
-        this.process.on('error', this.emitOnError.bind(this));
-        this.process.on('exit', this.emitOnExit.bind(this));
+            this.process.on('error', this.emitOnError.bind(this));
+            this.process.on('exit', this.emitOnExit.bind(this));
 
-        this.output = this.process.stdout;
-        this.input = this.process.stdin;
-        this.errorOutput = this.process.stderr;
+            this.output = this.process.stdout;
+            this.input = this.process.stdin;
+            this.errorOutput = this.process.stderr;
+        } catch (error) {
+            /* When an error is thrown, set up some fake streams, so the client
+               code doesn't break because these field are undefined.  */
+            this.output = new DevNullStream();
+            this.input = new DevNullStream();
+            this.errorOutput = new DevNullStream();
+
+            /* Call the client error handler, but first give them a chance to register it.  */
+            process.nextTick(() => {
+                this.errorEmitter.fire(error);
+            });
+        }
     }
 
     get pid() {
