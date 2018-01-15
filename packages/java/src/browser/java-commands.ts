aad46f8a200cb473b6c61f25b553ea308fdbe806
Sven Efftinge <sven.efftinge@typefox.io>
2018-01-15 11:31:14 +0100

    [java] Added java ls organize imports command
    Signed-off-by: Sven Efftinge <sven.efftinge@typefox.io>

--- packages/java/src/browser/java-commands.ts
+++ packages/java/src/browser/java-commands.ts
@@ -6,9 +6,14 @@
  */
 
 import { inject, injectable } from "inversify";
-import { CommandContribution, CommandRegistry, Command } from '@theia/core/lib/common';
-import { EditorCommands } from "@theia/editor/lib/browser";
+import {
+    CommandContribution, CommandRegistry, Command, MenuContribution, MenuModelRegistry, KeybindingContext,
+    Keybinding, KeybindingContextRegistry, KeybindingContribution, KeybindingRegistry, KeyCode, Key, Modifier
+} from '@theia/core/lib/common';
+import { EditorCommands, EditorManager, EDITOR_CONTEXT_MENU } from "@theia/editor/lib/browser";
 import { WorkspaceEdit, Workspace } from "@theia/languages/lib/common";
+import { JavaClientContribution } from "./java-client-contribution";
+import { ExecuteCommandRequest } from "monaco-languageclient/lib";
 
 /**
  * Show Java references
@@ -24,11 +29,36 @@ export const APPLY_WORKSPACE_EDIT: Command = {
     id: 'java.apply.workspaceEdit'
 };
 
+/**
+ * Organize Imports
+ */
+export const JAVA_ORGANIZE_IMPORTS: Command = {
+    label: 'Java: Organize Imports',
+    id: 'java.edit.organizeImports'
+};
+
+const CONTEXT_ID = 'java.editor.context';
+
 @injectable()
-export class JavaCommandContribution implements CommandContribution {
+export class JavaEditorContext implements KeybindingContext {
+    readonly id = CONTEXT_ID;
+
+    constructor( @inject(EditorManager) protected readonly editorService: EditorManager) { }
+
+    isEnabled(arg?: Keybinding) {
+        return this.editorService && !!this.editorService.currentEditor && (this.editorService.currentEditor.editor.document.uri.endsWith(".java"));
+    }
+}
+
+@injectable()
+export class JavaCommandContribution implements CommandContribution, MenuContribution, KeybindingContribution {
 
     constructor(
-        @inject(Workspace) protected readonly workspace: Workspace
+        @inject(Workspace) protected readonly workspace: Workspace,
+        @inject(JavaEditorContext) protected readonly editorContext: JavaEditorContext,
+        @inject(KeybindingContextRegistry) protected readonly keybindingContextRegistry: KeybindingContextRegistry,
+        @inject(JavaClientContribution) protected readonly javaClientContribution: JavaClientContribution,
+        @inject(EditorManager) protected readonly editorService: EditorManager
     ) { }
 
     registerCommands(commands: CommandRegistry): void {
@@ -40,6 +70,52 @@ export class JavaCommandContribution implements CommandContribution {
             execute: (changes: WorkspaceEdit) =>
                 !!this.workspace.applyEdit && this.workspace.applyEdit(changes)
         });
+        commands.registerCommand(JAVA_ORGANIZE_IMPORTS, {
+            execute: async (changes: WorkspaceEdit) => {
+                const editor = this.editorService.activeEditor;
+                if (!editor) {
+                    return false;
+                }
+                const uri = editor.editor.uri.toString();
+                const client = await this.javaClientContribution.languageClient;
+                const result = await client.sendRequest(ExecuteCommandRequest.type, {
+                    command: JAVA_ORGANIZE_IMPORTS.id,
+                    arguments: [
+                        uri
+                    ]
+                });
+                if (isWorkspaceEdit(result) && this.workspace.applyEdit) {
+                    return await this.workspace.applyEdit(result);
+                } else {
+                    return false;
+                }
+            },
+            isVisible: () => {
+                const result = this.editorContext.isEnabled();
+                return result;
+            },
+            isEnabled: () => this.editorContext.isEnabled()
+        });
     }
 
+    registerMenus(menus: MenuModelRegistry): void {
+        menus.registerMenuAction([...EDITOR_CONTEXT_MENU, '1_modification'], {
+            commandId: JAVA_ORGANIZE_IMPORTS.id,
+            label: 'Organize Imports'
+        });
+    }
+
+    registerKeybindings(keybindings: KeybindingRegistry): void {
+        this.keybindingContextRegistry.registerContext(this.editorContext);
+        keybindings.registerKeybinding({
+            commandId: JAVA_ORGANIZE_IMPORTS.id,
+            contextId: CONTEXT_ID,
+            accelerator: ['Accel Shift O'],
+            keyCode: KeyCode.createKeyCode({ first: Key.KEY_O, modifiers: [Modifier.M1, Modifier.M2] })
+        });
+    }
+}
+
+function isWorkspaceEdit(edit?: object): edit is WorkspaceEdit {
+    return !!edit && ('changes' in edit || 'documentchanges' in edit);
 }
