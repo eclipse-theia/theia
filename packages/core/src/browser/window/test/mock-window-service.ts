3ed261d73a398cc50b35a7025421f8f6fa2bdc02
Patrick-Jeffrey Pollo Guilbert <patrick.pollo.guilbert@ericsson.com>
2018-01-22 11:36:20 -0500

    Add mocks and test skeleton to frontend preference-service
    Signed-off-by: Patrick-Jeffrey Pollo Guilbert <patrick.pollo.guilbert@ericsson.com>

---
+++ packages/core/src/browser/window/test/mock-window-service.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2017 Ericsson and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+import { injectable } from 'inversify';
+import { WindowService } from '../window-service';
+
+@injectable()
+export class MockWindowService implements WindowService {
+
+    openNewWindow(url: string): void { }
+}
