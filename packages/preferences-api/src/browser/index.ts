epatpol/theia-1

Move preferences to frontend
This also adds basic workspace/user scopes

Signed-off-by: Patrick-Jeffrey Pollo Guilbert <patrick.pollo.guilbert@ericsson.com>

packages/preferences-api/src/browser/index.ts
@@ -1,8 +1,11 @@
 /*
- * Copyright (C) 2017 Ericsson and others.
+ * Copyright (C) 2017 TypeFox and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-export * from '@theia/preferences-api';
+export * from './preference-protocol';
+export * from './preference-service';
+export * from './preference-proxy';
+export * from './preference-contribution';

eclipse-theia/theia

packages/preferences-api/src/browser/index.ts
@@ -1,8 +1,11 @@
 /*
- * Copyright (C) 2017 Ericsson and others.
+ * Copyright (C) 2017 TypeFox and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-export * from '@theia/preferences-api';
+export * from './preference-service';
+export * from './preference-proxy';
+export * from './preference-contribution';
+export * from './preference-provider';