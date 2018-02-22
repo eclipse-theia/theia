e31ab923c6e3d2763ba38fd11ecfdc5d61829c34
Patrick-Jeffrey Pollo Guilbert <patrick.pollo.guilbert@ericsson.com>
2018-02-21 21:50:04 -0500

    Add extension manager metrics
    Signed-off-by: Patrick-Jeffrey Pollo Guilbert <patrick.pollo.guilbert@ericsson.com>

--- packages/metrics/src/node/index.ts
+++ packages/metrics/src/node/index.ts
@@ -8,3 +8,4 @@
 export * from './metrics-backend-application-contribution';
 export * from './metrics-contribution';
 export * from './node-metrics-contribution';
+export * from './extensions-metrics-contribution';
--- packages/metrics/src/node/metrics-backend-module.ts
+++ packages/metrics/src/node/metrics-backend-module.ts
@@ -8,12 +8,14 @@
 import { ContainerModule } from 'inversify';
 import { BackendApplicationContribution } from '@theia/core/lib/node';
 import { bindContributionProvider } from '@theia/core/lib/common';
-import { MetricsContribution, NodeMetricsContribution, MetricsBackendApplicationContribution } from './';
+import { MetricsContribution, NodeMetricsContribution, MetricsBackendApplicationContribution, ExtensionMetricsContribution } from './';
 
 export default new ContainerModule(bind => {
 
     bindContributionProvider(bind, MetricsContribution);
     bind(MetricsContribution).to(NodeMetricsContribution);
+    bind(MetricsContribution).to(ExtensionMetricsContribution);
 
     bind(BackendApplicationContribution).to(MetricsBackendApplicationContribution);
+
 });
