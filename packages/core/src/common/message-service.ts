8c32c86ea36e5482fc3125587afb37e9d0e81a97
Jacques Bouthillier <jacques.bouthillier@ericsson.com>
2018-02-22 11:16:25 -0500

    # This is a combination of 2 commits. # This is the 1st commit message:
    GH-1081 Auto-dismiss notification.
    Add notifications preferences to allow auto-dismiss
    Auto-Dismiss can be used/disabled for each notification since it is optional
    When the user has more than one action available,
    the auto-Dismiss timeout will not be applied
    
    Signed-off-by: Jacques Bouthillier <jacques.bouthillier@ericsson.com>
    
    # This is the commit message #2:
    
    fix issue with `timeout=0`
    
    Signed-off-by: Alex Tugarev <alex.tugarev@typefox.io>

--- packages/core/src/common/message-service.ts
+++ packages/core/src/common/message-service.ts
@@ -6,7 +6,7 @@
  */
 
 import { injectable, inject } from "inversify";
-import { MessageClient, MessageType } from "./message-service-protocol";
+import { MessageClient, MessageType, MessageOptions } from "./message-service-protocol";
 
 @injectable()
 export class MessageService {
@@ -15,20 +15,45 @@ export class MessageService {
         @inject(MessageClient) protected readonly client: MessageClient
     ) { }
 
-    log(message: string, ...actions: string[]): Promise<string | undefined> {
-        return this.client.showMessage({ type: MessageType.Log, text: message, actions: actions || [] });
+    log(message: string, ...actions: string[]): Promise<string | undefined>;
+    log(message: string, options?: MessageOptions, ...actions: string[]): Promise<string | undefined>;
+    // tslint:disable-next-line:no-any
+    log(message: string, ...args: any[]): Promise<string | undefined> {
+        return this.processMessage(MessageType.Log, message, args);
     }
 
-    info(message: string, ...actions: string[]): Promise<string | undefined> {
-        return this.client.showMessage({ type: MessageType.Info, text: message, actions: actions || [] });
+    info(message: string, ...actions: string[]): Promise<string | undefined>;
+    info(message: string, options?: MessageOptions, ...actions: string[]): Promise<string | undefined>;
+    // tslint:disable-next-line:no-any
+    info(message: string, ...args: any[]): Promise<string | undefined> {
+        return this.processMessage(MessageType.Info, message, args);
     }
 
-    warn(message: string, ...actions: string[]): Promise<string | undefined> {
-        return this.client.showMessage({ type: MessageType.Warning, text: message, actions: actions || [] });
+    warn(message: string, ...actions: string[]): Promise<string | undefined>;
+    warn(message: string, options?: MessageOptions, ...actions: string[]): Promise<string | undefined>;
+    // tslint:disable-next-line:no-any
+    warn(message: string, ...args: any[]): Promise<string | undefined> {
+        return this.processMessage(MessageType.Warning, message, args);
     }
 
-    error(message: string, ...actions: string[]): Promise<string | undefined> {
-        return this.client.showMessage({ type: MessageType.Error, text: message, actions: actions || [] });
+    error(message: string, ...actions: string[]): Promise<string | undefined>;
+    error(message: string, options?: MessageOptions, ...actions: string[]): Promise<string | undefined>;
+    // tslint:disable-next-line:no-any
+    error(message: string, ...args: any[]): Promise<string | undefined> {
+        return this.processMessage(MessageType.Error, message, args);
+    }
+
+    // tslint:disable-next-line:no-any
+    protected processMessage(type: MessageType, text: string, args?: any[]): Promise<string | undefined> {
+        if (!!args && args.length > 0) {
+            const first = args[0];
+            const actions: string[] = args.filter(a => typeof a === 'string');
+            const options = (typeof first === 'object' && !Array.isArray(first))
+                ? <MessageOptions>first
+                : undefined;
+            return this.client.showMessage({ type, options, text, actions });
+        }
+        return this.client.showMessage({ type, text });
     }
 
 }
