caaf6774f574de9f234e101fd6a7d64309d80bcb
Anatoliy Bazko <abazko@redhat.com>
2018-03-06 14:20:14 +0200

    Discoverting debug configuration provider and creation debug session

--- packages/debug/src/common/debug-model.ts
+++ packages/debug/src/common/debug-model.ts
@@ -9,27 +9,29 @@
  *   Red Hat, Inc. - initial API and implementation
  */
 
-import {Disposable} from '@theia/core';
-// import {DebugProtocol} from 'vscode-debugprotocol';
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+// Some entities copied and modified from https://github.com/Microsoft/vscode/blob/master/src/vs/vscode.d.ts
 
 /**
- * The WS endpoint path to the Debug service.
+ * Configuration for a debug session.
  */
-export const DebugPath = '/services/debug';
+export interface DebugConfiguration {
+    /**
+     * The type of the debug session.
+     */
+    type: string;
 
-/**
- * Debug symbol for DI.
- */
-export const Debug = Symbol('Debug');
+    /**
+     * The name of the debug session.
+     */
+    name: string;
 
-// export namespace Debug {
-//     export interface InitializeEvent extends DebugProtocol.InitializedEvent {
-//     }
-// }
-
-/**
- * Provides functionality for Debug.
- */
-export interface Debug extends Disposable {
-    doSomething(param: string): void;
+    /**
+     * Additional debug type specific properties.
+     */
+    [key: string]: any;
 }
