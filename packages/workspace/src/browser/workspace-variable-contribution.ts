26f1e0c3f11068d503663b1f120c0e7f06747c09
Artem Zatsarynnyi <azatsarynnyy@codenvy.com>
2018-03-30 20:29:28 +0300

    Add 'workspaceFolder' and 'workspaceFolderBasename' variables
    Signed-off-by: Artem Zatsarynnyi <azatsarynnyy@codenvy.com>

---
+++ packages/workspace/src/browser/workspace-variable-contribution.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018 Red Hat, Inc. and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import { injectable, inject } from 'inversify';
+import { VariableContribution, VariableRegistry } from '@theia/variable-resolver/lib/browser';
+import { WorkspaceService } from './workspace-service';
+import URI from '@theia/core/lib/common/uri';
+
+@injectable()
+export class WorkspaceVariableContribution implements VariableContribution {
+
+    constructor(
+        @inject(WorkspaceService) protected readonly workspaceService: WorkspaceService
+    ) { }
+
+    registerVariables(variables: VariableRegistry): void {
+        variables.registerVariable({
+            name: 'workspaceFolder',
+            description: 'The path of the workspace root folder',
+            resolve: async () => {
+                const uri = await this.getWorkspaceRootUri();
+                return uri ? uri.path.toString() : undefined;
+            }
+        });
+        variables.registerVariable({
+            name: 'workspaceFolderBasename',
+            description: 'The name of the workspace root folder',
+            resolve: async () => {
+                const uri = await this.getWorkspaceRootUri();
+                return uri ? uri.displayName : undefined;
+            }
+        });
+    }
+
+    protected async getWorkspaceRootUri(): Promise<URI | undefined> {
+        const wsRoot = await this.workspaceService.root;
+        if (wsRoot) {
+            return new URI(wsRoot.uri);
+        }
+        return undefined;
+    }
+}
