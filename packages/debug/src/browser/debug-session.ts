c3d39c786c0dfc16bad6a7d92f67e57f44c482c0
Anatoliy Bazko <abazko@redhat.com>
2018-05-04 14:12:17 +0300

    Refactoring

--- packages/debug/src/browser/debug-session.ts
+++ packages/debug/src/browser/debug-session.ts
@@ -15,48 +15,34 @@ import { DebugAdapterPath, DebugConfiguration } from "../common/debug-model";
 import { DebugProtocol } from "vscode-debugprotocol";
 import { Disposable } from "vscode-jsonrpc";
 import { Deferred } from "@theia/core/lib/common/promise-util";
-import { Emitter, Event } from "@theia/core";
-import * as ee from 'events';
+import { Emitter, Event, DisposableCollection } from "@theia/core";
 
 export interface DebugSession extends Disposable {
     sessionId: string;
     configuration: DebugConfiguration;
 
-    connect(): Promise<void>;
-    sendRequest(command: string, args?: any): Promise<void>;
+    getServerCapabilities(): DebugProtocol.Capabilities | undefined;
+    initialize(): Promise<DebugProtocol.InitializeResponse>;
+    configurationDone(): Promise<DebugProtocol.ConfigurationDoneResponse>;
+    disconnect(): Promise<DebugProtocol.InitializeResponse>;
 }
 
-export class DebugSessionImpl extends ee.EventEmitter implements DebugSession {
+export class DebugSessionImpl implements DebugSession {
+    private sequence: number;
+
+    protected readonly toDispose = new DisposableCollection();
+    protected readonly callbacks = new Map<number, (response: DebugProtocol.Response) => void>();
+
     protected websocket: Promise<WebSocket>;
+    protected capabilities: DebugProtocol.Capabilities = {};
 
     constructor(
         public readonly sessionId: string,
         public readonly configuration: DebugConfiguration,
         protected readonly connectionProvider: WebSocketConnectionProvider) {
-        super();
-    }
 
-    connect(): Promise<void> {
         this.websocket = this.initWebSocket();
-        return this.websocket.then(() => { });
-    }
-
-    sendRequest(command: string, args?: any): Promise<void> {
-        const request = {
-            type: "request",
-            command: command,
-            arguments: args
-        };
-
-        return this.websocket
-            .then(websocket => websocket.send(JSON.stringify(request)))
-            .catch(error => console.log(error && error.message));
-    }
-
-    dispose() {
-        this.websocket
-            .then(websocket => websocket.close)
-            .catch(error => console.error(error));
+        this.sequence = 1;
     }
 
     private initWebSocket(): Promise<WebSocket> {
@@ -78,13 +64,83 @@ export class DebugSessionImpl extends ee.EventEmitter implements DebugSession {
         return initialized.promise;
     }
 
-    private handleMessage(event: MessageEvent) {
+    initialize(): Promise<DebugProtocol.InitializeResponse> {
+        return this.proceedRequest("initialize", {
+            clientID: "Theia",
+            clientName: "Theia",
+            adapterID: this.configuration.type,
+            locale: "",
+            linesStartAt1: true,
+            columnsStartAt1: true,
+            pathFormat: "path",
+            supportsVariableType: false,
+            supportsVariablePaging: false,
+            supportsRunInTerminalRequest: false
+        }).then(response => {
+            this.capabilities = response.body || {};
+            return response;
+        });
+    }
+
+    configurationDone(): Promise<DebugProtocol.ConfigurationDoneResponse> {
+        return this.proceedRequest("configurationDone");
+    }
+
+    getServerCapabilities(): DebugProtocol.Capabilities {
+        return this.capabilities;
+    }
+
+    disconnect(): Promise<DebugProtocol.DisconnectResponse> {
+        return this.proceedRequest("disconnect", { terminateDebuggee: true });
+    }
+
+    protected handleMessage(event: MessageEvent) {
         const message: DebugProtocol.ProtocolMessage = JSON.parse(event.data);
         if (message.type === 'response') {
-            // TODO
+            this.proceedResponse(message as DebugProtocol.Response);
         } else if (message.type === 'event') {
-            // TODO
+            this.proceedEvent(message as DebugProtocol.Event);
         }
+    }
+
+    protected proceedRequest(command: string, args?: any): Promise<DebugProtocol.Response> {
+        const result = new Deferred<DebugProtocol.Response>();
+
+        const request: DebugProtocol.Request = {
+            seq: this.sequence++,
+            type: "request",
+            command: command,
+            arguments: args
+        };
+
+        this.callbacks.set(request.seq, (response) => {
+            result.resolve(response);
+        });
+
+        return this.websocket
+            .then(websocket => websocket.send(JSON.stringify(request)))
+            .then(() => result.promise);
+    }
+
+    protected proceedResponse(response: DebugProtocol.Response): void {
+        console.log(response);
+
+        const callback = this.callbacks.get(response.request_seq);
+        if (callback) {
+            this.callbacks.delete(response.request_seq);
+            callback(response);
+        }
+    }
+
+    protected proceedEvent(event: DebugProtocol.Event): void {
+        console.log(event);
+    }
+
+    dispose() {
+        this.callbacks.clear();
+        this.websocket
+            .then(websocket => websocket.close())
+            .catch(error => console.error(error));
     }
 }
 
@@ -105,39 +161,39 @@ export class DebugSessionFactory {
 export class DebugSessionManager implements Disposable {
     private activeDebugSessionId: string | undefined;
 
-    protected readonly clients = new Map<string, DebugSession>();
+    protected readonly sessions = new Map<string, DebugSession>();
     protected readonly onDidChangeActiveDebugSessionEmitter = new Emitter<DebugSession | undefined>();
     protected readonly onDidStartDebugSessionEmitter = new Emitter<DebugSession>();
     protected readonly onDidTerminateDebugSessionEmitter = new Emitter<DebugSession>();
 
     constructor(
         @inject(DebugSessionFactory)
-        protected readonly debugClientFactory: DebugSessionFactory
+        protected readonly debugSessionFactory: DebugSessionFactory
     ) { }
 
     /**
-     * Creates a new [debug client](#DebugClient).
+     * Creates a new [debug session](#DebugSession).
      * @param sessionId The session identifier
      * @param configuration The debug configuration
-     * @returns The debug client
+     * @returns The debug session
      */
     create(sessionId: string, debugConfiguration: DebugConfiguration): DebugSession {
-        const client = this.debugClientFactory.get(sessionId, debugConfiguration);
-        this.clients.set(sessionId, client);
-        this.onDidStartDebugSessionEmitter.fire(client);
-        return client;
+        const session = this.debugSessionFactory.get(sessionId, debugConfiguration);
+        this.sessions.set(sessionId, session);
+        this.onDidStartDebugSessionEmitter.fire(session);
+        return session;
     }
 
     /**
-     * Removes the [debug client](#DebugClient).
+     * Removes the [debug session](#DebugSession).
      * @param sessionId The session identifier
      */
     remove(sessionId: string): void {
-        this.clients.delete(sessionId);
+        this.sessions.delete(sessionId);
         if (this.activeDebugSessionId) {
             if (this.activeDebugSessionId === sessionId) {
-                if (this.clients.size !== 0) {
-                    this.setActiveDebugSession(this.clients.keys().next().value);
+                if (this.sessions.size !== 0) {
+                    this.setActiveDebugSession(this.sessions.keys().next().value);
                 } else {
                     this.setActiveDebugSession(undefined);
                 }
@@ -147,15 +203,15 @@ export class DebugSessionManager implements Disposable {
     }
 
     /**
-     * Finds all instantiated debug clients.
-     * @returns An array of debug clients
+     * Finds all instantiated debug sessions.
+     * @returns An array of debug sessions
      */
     findAll(): DebugSession[] {
-        return Array.from(this.clients.values());
+        return Array.from(this.sessions.values());
     }
 
     /**
-     * Sets the active debug client.
+     * Sets the active debug session.
      * @param sessionId The session identifier
      */
     setActiveDebugSession(sessionId: string | undefined) {
@@ -166,34 +222,34 @@ export class DebugSessionManager implements Disposable {
     }
 
     /**
-     * Returns the active debug client.
-     * @returns the [debug client](#DebugClient)
+     * Returns the active debug session.
+     * @returns the [debug session](#DebugSession)
      */
     getActiveDebugSession(): DebugSession | undefined {
         if (this.activeDebugSessionId) {
-            return this.clients.get(this.activeDebugSessionId);
+            return this.sessions.get(this.activeDebugSessionId);
         }
     }
 
     /**
-     * Disposes the debug client.
+     * Disposes the debug session.
      * @param sessionId The session identifier
      */
     dispose(sessionId?: string): void {
         if (sessionId) {
-            const debugClient = this.clients.get(sessionId);
-            if (debugClient) {
-                this.doDispose(debugClient);
+            const session = this.sessions.get(sessionId);
+            if (session) {
+                this.doDispose(session);
             }
         } else {
-            this.clients.forEach(debugClient => this.doDispose(debugClient));
+            this.sessions.forEach(session => this.doDispose(session));
         }
     }
 
-    private doDispose(debugClient: DebugSession): void {
-        debugClient.dispose();
-        this.remove(debugClient.sessionId);
-        this.onDidTerminateDebugSessionEmitter.fire(debugClient);
+    private doDispose(session: DebugSession): void {
+        session.dispose();
+        this.remove(session.sessionId);
+        this.onDidTerminateDebugSessionEmitter.fire(session);
     }
 
     get onDidChangeActiveDebugSession(): Event<DebugSession | undefined> {
