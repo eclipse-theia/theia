483e0b788ddb08b517164dc3bd792f8995499621
Mykola Morhun <mmorhun@redhat.com>
2018-05-08 17:37:41 +0300

    Make plugin-ext npm module (#12) Signed-off-by: Florent BENOIT <fbenoit@redhat.com>

---
+++ packages/plugin-ext/src/common/index.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2018 Red Hat, Inc. and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+// Exports contribution point for uri postprocessor of hosted plugin manager.
+// This could be used to alter hosted instance uri, for example, change port.
+export * from '../hosted/node/hosted-plugin-uri-postprocessor';
