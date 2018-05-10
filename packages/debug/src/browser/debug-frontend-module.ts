666c4c2574bc15a22992b5f9680126e97a6cc7ad
Anatoliy Bazko <abazko@redhat.com>
2018-05-10 12:23:00 +0300

    Show threads of the debug target

--- packages/debug/src/browser/debug-frontend-module.ts
+++ packages/debug/src/browser/debug-frontend-module.ts
@@ -10,47 +10,30 @@
  */
 
 import { ContainerModule, interfaces } from 'inversify';
-import { DebugCommandHandlers, DEBUG_SESSION_CONTEXT_MENU } from "./debug-command";
+import { DebugCommandHandlers } from "./debug-command";
 import { DebugConfigurationManager } from './debug-configuration';
 import {
     DebugViewContribution,
-    DebugTreeWidgetFactory,
-    DebugTreeWidget,
-    DebugWidgetFactory,
+    DebugWidget,
+    DEBUG_FACTORY_ID,
 } from './view/debug-view-contribution';
 import { DebugPath, DebugService } from "../common/debug-model";
 import { MenuContribution } from "@theia/core/lib/common/menu";
 import { CommandContribution } from "@theia/core/lib/common/command";
 import { WebSocketConnectionProvider } from "@theia/core/lib/browser/messaging/connection";
 import {
-    FrontendApplicationContribution,
-    createTreeContainer,
-    TreeWidget,
     WidgetFactory,
-    KeybindingContribution,
-    TreeProps,
-    defaultTreeProps
+    bindViewContribution
 } from '@theia/core/lib/browser';
 import { DebugSessionManager, DebugSessionFactory } from './debug-session';
 
-export const DEBUG_SESSION_TREE_PROPS = <TreeProps>{
-    ...defaultTreeProps,
-    contextMenuPath: DEBUG_SESSION_CONTEXT_MENU,
-    multiSelect: false
-};
-
 export default new ContainerModule((bind: interfaces.Bind, unbind: interfaces.Unbind, isBound: interfaces.IsBound, rebind: interfaces.Rebind) => {
-    bind(DebugViewContribution).toSelf().inSingletonScope();
-    bind(FrontendApplicationContribution).toDynamicValue(c => c.container.get(DebugViewContribution));
-    bind(MenuContribution).toDynamicValue(c => c.container.get(DebugViewContribution));
-    bind(CommandContribution).toDynamicValue(c => c.container.get(DebugViewContribution));
-    bind(KeybindingContribution).toDynamicValue(c => c.container.get(DebugViewContribution));
-    bind(DebugWidgetFactory).toSelf().inSingletonScope();
-    bind(WidgetFactory).toDynamicValue(context => context.container.get(DebugWidgetFactory));
-
-    bind(DebugTreeWidgetFactory).toFactory(ctx =>
-        () => createDebugTreeWidget(ctx.container)
-    );
+    bind(DebugWidget).toSelf();
+    bind(WidgetFactory).toDynamicValue(context => ({
+        id: DEBUG_FACTORY_ID,
+        createWidget: () => context.container.get<DebugWidget>(DebugWidget)
+    })).inSingletonScope();
+    bindViewContribution(bind, DebugViewContribution);
 
     bind(DebugSessionFactory).toSelf().inSingletonScope();
     bind(DebugSessionManager).toSelf().inSingletonScope();
@@ -59,11 +42,3 @@ export default new ContainerModule((bind: interfaces.Bind, unbind: interfaces.Un
     bind(DebugConfigurationManager).toSelf().inSingletonScope();
     bind(DebugService).toDynamicValue(context => WebSocketConnectionProvider.createProxy(context.container, DebugPath)).inSingletonScope();
 });
-
-function createDebugTreeWidget(parent: interfaces.Container): DebugTreeWidget {
-    const child = createTreeContainer(parent);
-    child.unbind(TreeWidget);
-    child.bind(DebugTreeWidget).toSelf();
-    child.rebind(TreeProps).toConstantValue(DEBUG_SESSION_TREE_PROPS);
-    return child.get(DebugTreeWidget);
-}
