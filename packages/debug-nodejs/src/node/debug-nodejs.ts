08da6c893e29da43f2f0ce5eadef98da1cedea99
Anatoliy Bazko <abazko@redhat.com>
2018-05-23 16:08:34 +0300

    Showing threads for a debug target

--- packages/debug-nodejs/src/node/debug-nodejs.ts
+++ packages/debug-nodejs/src/node/debug-nodejs.ts
@@ -9,6 +9,10 @@
  *   Red Hat, Inc. - initial API and implementation
  */
 
+const path = require('path');
+const packageJson = require('../../package.json');
+const debugAdapterDir = packageJson['debugAdapter']['dir'];
+
 import { injectable } from "inversify";
 import {
     DebugConfiguration,
@@ -24,20 +28,34 @@ export class NodeJsDebugAdapterContribution implements DebugAdapterContribution 
         return [{
             "type": this.debugType,
             "request": "attach",
-            "name": "Attach by Process ID",
+            "name": "Attach by PID",
             "processId": ""
         }];
     }
 
     resolveDebugConfiguration(config: DebugConfiguration): DebugConfiguration {
+        if (!config.request) {
+            throw new Error("Debug request type isn't provided.");
+        }
+
+        switch (config.request) {
+            case "attach": this.validateAttachConfig(config);
+        }
+
         return config;
     }
 
     provideDebugAdapterExecutable(config: DebugConfiguration): DebugAdapterExecutable {
+        const program = path.join(__dirname, `../../${debugAdapterDir}/out/src/nodeDebug.js`);
         return {
             runtime: "node",
-            program: "./packages/debug-nodejs/lib/node/adapter/out/src/nodeDebug.js",
-            args: []
+            program
         };
+    }
+
+    private validateAttachConfig(config: DebugConfiguration) {
+        if (!config.processId) {
+            throw new Error("PID isn't provided.");
+        }
     }
 }
