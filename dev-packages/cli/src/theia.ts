e3c1363aeee7fe6d0bf5211b464976059ecb9e7b
Simon Marchi <simon.marchi@ericsson.com>
2018-06-01 11:39:44 -0400

    theia.ts: Move code from the global scope to an IFFE
    To get rid of these warnings:
    
    WARNING: (no-shadowed-variable) /home/emaisin/src/theia/dev-packages/cli/src/theia.ts[29, 25]: Shadowed name: 'command'
    WARNING: (no-shadowed-variable) /home/emaisin/src/theia/dev-packages/cli/src/theia.ts[29, 42]: Shadowed name: 'target'
    
    We can move the code in the global scope to an immediatly invoked
    function.  This also helps avoid contaminating the global scope with
    variables.
    
    Signed-off-by: Simon Marchi <simon.marchi@ericsson.com>

--- dev-packages/cli/src/theia.ts
+++ dev-packages/cli/src/theia.ts
@@ -42,81 +42,83 @@ function rebuildCommand(command: string, target: ApplicationPackageTarget): yarg
     };
 }
 
-const projectPath = process.cwd();
-const manager = new ApplicationPackageManager({ projectPath });
-const target = manager.pck.target;
+(function() {
+    const projectPath = process.cwd();
+    const manager = new ApplicationPackageManager({ projectPath });
+    const target = manager.pck.target;
 
-yargs
-    .command({
-        command: 'start',
-        describe: 'start the ' + manager.pck.target + ' backend',
-        handler: async () => {
-            try {
-                await manager.start(commandArgs('start'));
-            } catch (err) {
-                console.error(err);
-                process.exit(1);
+    yargs
+        .command({
+            command: 'start',
+            describe: 'start the ' + manager.pck.target + ' backend',
+            handler: async () => {
+                try {
+                    await manager.start(commandArgs('start'));
+                } catch (err) {
+                    console.error(err);
+                    process.exit(1);
+                }
             }
-        }
-    })
-    .command({
-        command: 'clean',
-        describe: 'clean for the ' + target + ' target',
-        handler: () => {
-            try {
-                manager.clean();
-            } catch (err) {
-                console.error(err);
-                process.exit(1);
+        })
+        .command({
+            command: 'clean',
+            describe: 'clean for the ' + target + ' target',
+            handler: () => {
+                try {
+                    manager.clean();
+                } catch (err) {
+                    console.error(err);
+                    process.exit(1);
+                }
             }
-        }
-    })
-    .command({
-        command: 'copy',
-        handler: () => {
-            try {
-                manager.copy();
-            } catch (err) {
-                console.error(err);
-                process.exit(1);
+        })
+        .command({
+            command: 'copy',
+            handler: () => {
+                try {
+                    manager.copy();
+                } catch (err) {
+                    console.error(err);
+                    process.exit(1);
+                }
             }
-        }
-    })
-    .command({
-        command: 'generate',
-        handler: () => {
-            try {
-                manager.generate();
-            } catch (err) {
-                console.error(err);
-                process.exit(1);
+        })
+        .command({
+            command: 'generate',
+            handler: () => {
+                try {
+                    manager.generate();
+                } catch (err) {
+                    console.error(err);
+                    process.exit(1);
+                }
             }
-        }
-    })
-    .command({
-        command: 'build',
-        describe: 'webpack the ' + target + ' frontend',
-        handler: async () => {
-            try {
-                await manager.build(commandArgs('build'));
-            } catch (err) {
-                console.error(err);
-                process.exit(1);
+        })
+        .command({
+            command: 'build',
+            describe: 'webpack the ' + target + ' frontend',
+            handler: async () => {
+                try {
+                    await manager.build(commandArgs('build'));
+                } catch (err) {
+                    console.error(err);
+                    process.exit(1);
+                }
             }
-        }
-    })
-    .command(rebuildCommand('rebuild', target))
-    .command(rebuildCommand('rebuild:browser', 'browser'))
-    .command(rebuildCommand('rebuild:electron', 'electron'));
+        })
+        .command(rebuildCommand('rebuild', target))
+        .command(rebuildCommand('rebuild:browser', 'browser'))
+        .command(rebuildCommand('rebuild:electron', 'electron'));
 
-// see https://github.com/yargs/yargs/issues/287#issuecomment-314463783
-const commands = (yargs as any).getCommandInstance().getCommands();
-const argv = yargs.demandCommand(1).argv;
-const command = argv._[0];
-if (!command || commands.indexOf(command) === -1) {
-    console.log("non-existing or no command specified");
-    yargs.showHelp();
-    process.exit(1);
-} else {
-    yargs.help(false);
-}
+    // see https://github.com/yargs/yargs/issues/287#issuecomment-314463783
+    const commands = (yargs as any).getCommandInstance().getCommands();
+    const argv = yargs.demandCommand(1).argv;
+    const command = argv._[0];
+    if (!command || commands.indexOf(command) === -1) {
+        console.log("non-existing or no command specified");
+        yargs.showHelp();
+        process.exit(1);
+    } else {
+        yargs.help(false);
+    }
+})();
