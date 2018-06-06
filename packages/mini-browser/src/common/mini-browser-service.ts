5a02377dc708061184cd8dc1cb25e25f2886ddf2
Akos Kitta <kittaakos@gmail.com>
2018-06-06 20:33:46 +0200

    GH-2051: Open the HTML files in the Code Editor by default.
    It is still possible to open them in the Mini Browser.
    
    Closes #2051.
    
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- packages/mini-browser/src/common/mini-browser-service.ts
+++ packages/mini-browser/src/common/mini-browser-service.ts
@@ -10,11 +10,11 @@ export const MiniBrowserService = Symbol('MiniBrowserService');
 export interface MiniBrowserService {
 
     /**
-     * Resolves to an array of file extensions supported by the `Mini Browser`.
+     * Resolves to an array of file extensions - priority pairs supported by the `Mini Browser`.
      *
-     * The extensions start with the leading dot (`.`) and should be treated in a case-insensitive way. This means,
-     * if the `Mini Browser` supports `['.jpg']`, then it can open the `MyPicture.JPG` file.
+     * The file extensions start without the leading dot (`.`) and should be treated in a case-insensitive way. This means,
+     * if the `Mini Browser` supports `['jpg']`, then it can open the `MyPicture.JPG` file.
      */
-    supportedFileExtensions(): Promise<string[]>;
+    supportedFileExtensions(): Promise<Readonly<{ extension: string, priority: number }>[]>;
 
 }
