122da7028064cfe8841560df9b02cc4b923fd23c
Sven Efftinge <sven.efftinge@typefox.io>
2018-07-03 09:51:55 +0000

    [textmate] added html, less, markdown, shell, xml, yaml
    Signed-off-by: Sven Efftinge <sven.efftinge@typefox.io>

---
+++ packages/textmate-grammars/src/browser/less.ts
@@ -0,0 +1,77 @@
+/********************************************************************************
+ * Copyright (C) 2018 TypeFox and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import { LanguageGrammarDefinitionContribution, TextmateRegistry } from "@theia/monaco/lib/browser/textmate";
+import { injectable } from "inversify";
+
+@injectable()
+export class LessContribution implements LanguageGrammarDefinitionContribution {
+
+    readonly id = 'less';
+    readonly scopeName = 'source.less';
+
+    registerTextmateLanguage(registry: TextmateRegistry) {
+        monaco.languages.register({
+            id: this.id,
+            extensions: ['.less'],
+            aliases: ['Less', 'less'],
+            mimetypes: ['text/x-less', 'text/less'],
+        });
+        monaco.languages.setLanguageConfiguration(this.id, {
+            wordPattern: /(#?-?\d*\.\d\w*%?)|([@#!.:]?[\w-?]+%?)|[@#!.]/g,
+            comments: {
+                blockComment: ['/*', '*/'],
+                lineComment: '//'
+            },
+            brackets: [
+                ['{', '}'],
+                ['[', ']'],
+                ['(', ')'],
+            ],
+            autoClosingPairs: [
+                { open: '{', close: '}', notIn: ['string', 'comment'] },
+                { open: '[', close: ']', notIn: ['string', 'comment'] },
+                { open: '(', close: ')', notIn: ['string', 'comment'] },
+                { open: '"', close: '"', notIn: ['string', 'comment'] },
+                { open: '\'', close: '\'', notIn: ['string', 'comment'] },
+            ],
+            surroundingPairs: [
+                { open: '{', close: '}' },
+                { open: '[', close: ']' },
+                { open: '(', close: ')' },
+                { open: '"', close: '"' },
+                { open: '\'', close: '\'' },
+            ],
+            folding: {
+                markers: {
+                    start: new RegExp("^\\s*\\/\\*\\s*#region\\b\\s*(.*?)\\s*\\*\\/"),
+                    end: new RegExp("^\\s*\\/\\*\\s*#endregion\\b.*\\*\\/")
+                }
+            }
+        });
+
+        const grammar = require('../../data/less.tmLanguage.json');
+        registry.registerTextMateGrammarScope(this.scopeName, {
+            async getGrammarDefinition() {
+                return {
+                    format: 'json',
+                    content: grammar
+                };
+            }
+        });
+        registry.mapLanguageIdToTextmateGrammar(this.id, this.scopeName);
+    }
+}
