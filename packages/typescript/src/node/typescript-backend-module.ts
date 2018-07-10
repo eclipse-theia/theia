99ac80251faba9bf8608257b3359faddccca4e15
Sven Efftinge <sven.efftinge@typefox.io>
2018-07-10 12:06:59 +0000

    [typescript] Added coloring support for TSX & JSX
    Fixes theia-ide/theia#2310
    
    Signed-off-by: Sven Efftinge <sven.efftinge@typefox.io>

--- packages/typescript/src/node/typescript-backend-module.ts
+++ packages/typescript/src/node/typescript-backend-module.ts
@@ -16,9 +16,11 @@
 
 import { ContainerModule } from "inversify";
 import { LanguageServerContribution } from "@theia/languages/lib/node";
-import { JavaScriptContribution, TypeScriptContribution } from './typescript-contribution';
+import { JavaScriptContribution, TypeScriptContribution, TypeScriptReactContribution, JavaScriptReactContribution } from './typescript-contribution';
 
 export default new ContainerModule(bind => {
     bind(LanguageServerContribution).to(TypeScriptContribution).inSingletonScope();
+    bind(LanguageServerContribution).to(TypeScriptReactContribution).inSingletonScope();
     bind(LanguageServerContribution).to(JavaScriptContribution).inSingletonScope();
+    bind(LanguageServerContribution).to(JavaScriptReactContribution).inSingletonScope();
 });
