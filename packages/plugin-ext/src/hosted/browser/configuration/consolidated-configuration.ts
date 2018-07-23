7defe973b8ff95f7c7654dbd1f398348b511d909
Oleksandr Andriienko <oandriie@redhat.com>
2018-07-23 13:11:36 +0300

    Work on implementation consolidated configuration
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- packages/plugin-ext/src/hosted/browser/configuration/consolidated-configuration.ts
+++ packages/plugin-ext/src/hosted/browser/configuration/consolidated-configuration.ts
@@ -14,17 +14,23 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 import { injectable, inject } from "inversify";
-import {
-    PreferenceService,
-    //  PreferenceSchemaProvider
-} from "@theia/core/lib/browser";
+import { PreferenceServiceImpl } from "@theia/core/lib/browser";
+import { Emitter, DisposableCollection } from "@theia/core/lib/common";
+import { ConfigurationChange, ConfigurationModel } from "../../../api/plugin-api";
 // import { ConsolidatedPluginConfigurationProvider } from "../consolidated-plugin-configuration";
 
+// create default configuration the same like in the vscode....? in the separated file
 @injectable()
 export class ConsolidatedConfigurationRegistry {
 
+    protected readonly toDispose = new DisposableCollection();
+    protected readonly onConfigurationChangedEmitter = new Emitter<ConfigurationChange>();
+    readonly onConfigurationChanged = this.onConfigurationChangedEmitter.event;
+
+    private readonly consolidatedConf: ConfigurationModel = {properties: {}}; // todo inject ?
+
     constructor(
-        @inject(PreferenceService) private prefService: PreferenceService,
+        @inject(PreferenceServiceImpl) private prefService: PreferenceServiceImpl, // it's bad to use implementation
         // @inject(PreferenceSchemaProvider) private readonly prefChemaProvider: PreferenceSchemaProvider,
         // @inject(ConsolidatedPluginConfigurationProvider) private readonly pluginConfProvider: ConsolidatedPluginConfigurationProvider
     ) {
@@ -33,15 +39,34 @@ export class ConsolidatedConfigurationRegistry {
         // todo apply handler for add new plugin
         this.prefService.onPreferenceChanged(e => {
             console.log('pref changed!!!');
-            // todo get change from event !!!!
-        //     this.proxy.$acceptConfigurationChanged(prefService.getPreferences(), e);
+            this.prefService.getPreferences();
+
+            this.consolidatedConf.properties = {
+                ...this.consolidatedConf.properties,
+                ...this.prefService.getPreferences()
+            };
+            const confChange: ConfigurationChange = {section: e.preferenceName, value: e.newValue};
+            this.onConfigurationChangedEmitter.fire(confChange);
         });
+
+        // todo fire configuration changed event for another cases...
+
+        this.toDispose.push(this.onConfigurationChangedEmitter);
     }
+
+    // private applyPreferencesToConfiguration(prefs: {[key: string]: any}) {
+
+    // }
+
+    // todo in memory configs to change to change properties for plugins
 
     // getValue(section: string) {
     // }
 
-    // private getConsolidatedConfig() {
+    // updateOption(scope: ) {}
+    // removeOption(scope: ) {}
 
-    // }
+    getConsolidatedConfig(): ConfigurationModel {
+        return this.consolidatedConf;
+    }
 }
