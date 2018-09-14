bfba2e58ef1971e5de1e05db1227abbf7f536e9c
Anton Kosyakov <anton.kosyakov@typefox.io>
2018-09-14 08:17:11 +0000

    fix #1693: tslint extension
    Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

--- packages/typescript/src/node/typescript-contribution.ts
+++ packages/typescript/src/node/typescript-contribution.ts
@@ -15,14 +15,40 @@
  ********************************************************************************/
 
 import { injectable } from 'inversify';
+import { ApplicationPackageManager } from '@theia/application-manager';
 import { BaseLanguageServerContribution, IConnection } from '@theia/languages/lib/node';
 import { TYPESCRIPT_LANGUAGE_ID, TYPESCRIPT_LANGUAGE_NAME } from '../common';
+import { TypeScriptPlugin, TypeScriptInitializeParams, TypeScriptInitializationOptions } from 'typescript-language-server/lib/ts-protocol';
+import { isRequestMessage, Message } from 'vscode-ws-jsonrpc';
+import { InitializeRequest } from 'vscode-languageserver-protocol';
 
 @injectable()
 export class TypeScriptContribution extends BaseLanguageServerContribution {
 
     readonly id = TYPESCRIPT_LANGUAGE_ID;
     readonly name = TYPESCRIPT_LANGUAGE_NAME;
+
+    protected readonly plugins: TypeScriptPlugin[] = [];
+
+    constructor() {
+        super();
+        // TODO: ApplicationPackageManager should be injected
+        const manager = new ApplicationPackageManager({
+            // TODO: configure project path via CLI
+            projectPath: process.cwd()
+        });
+        for (const extension of manager.pck.extensionPackages) {
+            const { contributes, installed } = extension.raw;
+            if (installed && contributes && contributes.typescriptServerPlugins && Array.isArray(contributes.typescriptServerPlugins)) {
+                for (const plugin of contributes.typescriptServerPlugins) {
+                    this.plugins.push({
+                        name: plugin.name,
+                        location: installed.packagePath
+                    });
+                }
+            }
+        }
+    }
 
     start(clientConnection: IConnection): void {
         const command = 'node';
@@ -33,4 +59,22 @@ export class TypeScriptContribution extends BaseLanguageServerContribution {
         const serverConnection = this.createProcessStreamConnection(command, args);
         this.forward(clientConnection, serverConnection);
     }
+
+    protected map(message: Message): Message {
+        if (isRequestMessage(message)) {
+            if (message.method === InitializeRequest.type.method) {
+                const initializeParams = message.params as TypeScriptInitializeParams;
+                if (this.plugins.length) {
+                    const options: TypeScriptInitializationOptions = {
+                        plugins: [],
+                        ...initializeParams.initializationOptions
+                    };
+                    options.plugins.push(...this.plugins);
+                    initializeParams.initializationOptions = options;
+                }
+            }
+        }
+        return super.map(message);
+    }
+
 }
