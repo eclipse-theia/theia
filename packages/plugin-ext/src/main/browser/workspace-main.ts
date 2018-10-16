66bc6d86366cd9d6d093575a5b0932e74eebfee5
Vitalii Parfonov <vparfonov@redhat.com>
2018-10-16 11:24:27 +0300

    Initial commit for support vscode.workspace.findFiles feature
    Signed-off-by: Vitalii Parfonov <vparfonov@redhat.com>
    Signed-off-by: Bogdan Stolojan <petre.stolojan@arm.com>

--- packages/plugin-ext/src/main/browser/workspace-main.ts
+++ packages/plugin-ext/src/main/browser/workspace-main.ts
@@ -14,16 +14,18 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
+import * as theia from '@theia/plugin';
 import { interfaces } from 'inversify';
 import { WorkspaceExt, MAIN_RPC_CONTEXT, WorkspaceMain, WorkspaceFolderPickOptionsMain } from '../../api/plugin-api';
 import { RPCProtocol } from '../../api/rpc-protocol';
 import { WorkspaceService } from '@theia/workspace/lib/browser';
 import Uri from 'vscode-uri';
-import { WorkspaceFoldersChangeEvent, WorkspaceFolder } from '@theia/plugin';
+import { UriComponents } from '../../common/uri-components';
 import { Path } from '@theia/core/lib/common/path';
 import { QuickOpenModel, QuickOpenItem, QuickOpenMode } from '@theia/core/lib/browser/quick-open/quick-open-model';
 import { MonacoQuickOpenService } from '@theia/monaco/lib/browser/monaco-quick-open-service';
 import { FileStat } from '@theia/filesystem/lib/common';
+import { FileSearchService } from '@theia/file-search/lib/common/file-search-service';
 
 export class WorkspaceMainImpl implements WorkspaceMain {
 
@@ -31,12 +33,15 @@ export class WorkspaceMainImpl implements WorkspaceMain {
 
     private quickOpenService: MonacoQuickOpenService;
 
+    private fileSearchService: FileSearchService;
+
     private roots: FileStat[];
 
     constructor(rpc: RPCProtocol, container: interfaces.Container) {
         this.proxy = rpc.getProxy(MAIN_RPC_CONTEXT.WORKSPACE_EXT);
         this.quickOpenService = container.get(MonacoQuickOpenService);
         const workspaceService = container.get(WorkspaceService);
+        this.fileSearchService = container.get(FileSearchService);
 
         workspaceService.roots.then(roots => {
             this.roots = roots;
@@ -53,22 +58,22 @@ export class WorkspaceMainImpl implements WorkspaceMain {
                     uri: uri,
                     name: path.base,
                     index: 0
-                } as WorkspaceFolder;
+                } as theia.WorkspaceFolder;
             });
 
             this.proxy.$onWorkspaceFoldersChanged({
                 added: folders,
                 removed: []
-            } as WorkspaceFoldersChangeEvent);
+            } as theia.WorkspaceFoldersChangeEvent);
         } else {
             this.proxy.$onWorkspaceFoldersChanged({
                 added: [],
                 removed: []
-            } as WorkspaceFoldersChangeEvent);
+            } as theia.WorkspaceFoldersChangeEvent);
         }
     }
 
-    $pickWorkspaceFolder(options: WorkspaceFolderPickOptionsMain): Promise<WorkspaceFolder | undefined> {
+    $pickWorkspaceFolder(options: WorkspaceFolderPickOptionsMain): Promise<theia.WorkspaceFolder | undefined> {
         return new Promise((resolve, reject) => {
             // Return undefined if workspace root is not set
             if (!this.roots || !this.roots.length) {
@@ -80,7 +85,7 @@ export class WorkspaceMainImpl implements WorkspaceMain {
             const activeElement: HTMLElement | undefined = window.document.activeElement as HTMLElement;
 
             // WorkspaceFolder to be returned
-            let returnValue: WorkspaceFolder | undefined;
+            let returnValue: theia.WorkspaceFolder | undefined;
 
             const items = this.roots.map(root => {
                 const rootUri = Uri.parse(root.uri);
@@ -94,7 +99,7 @@ export class WorkspaceMainImpl implements WorkspaceMain {
                                 uri: rootUri,
                                 name: rootPathName,
                                 index: 0
-                            } as WorkspaceFolder;
+                            } as theia.WorkspaceFolder;
                         }
                         return true;
                     }
@@ -125,4 +130,29 @@ export class WorkspaceMainImpl implements WorkspaceMain {
         });
     }
 
+    $startFileSearch(includePattern: string, excludePatternOrDisregardExcludes?: string | false,
+                     maxResults?: number, token?: theia.CancellationToken): Promise<UriComponents[]> {
+        const uris: UriComponents[] = new Array();
+        let j = 0;
+        const promises: Promise<any>[] = new Array();
+        for (const root of this.roots) {
+            promises[j++] = this.fileSearchService.find(includePattern, {rootUri: root.uri}).then(value => {
+                const paths: string[] = new Array();
+                let i = 0;
+                value.forEach( item => {
+                    let path: string;
+                    path = root.uri.endsWith('/') ? root.uri + item : root.uri + '/' + item;
+                    paths[i++] = path;
+                });
+                return Promise.resolve(paths);
+            });
+        }
+        return Promise.all(promises).then(value => {
+            let i = 0;
+            value.forEach(path => {
+                uris[i++] = Uri.parse(path);
+            });
+            return Promise.resolve(uris);
+            });
+    }
 }
