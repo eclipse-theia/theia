0f8b550cf8575466090961048ca62c4b59c62d48
Akos Kitta <kittaakos@typefox.io>
2018-10-31 11:25:54 +0100

    GH-3333: Added toolbar support for the tab-bars.
     - It is supported only in the `main` and `bottom` panels.
     - From now on, `Preview` open in the `Code Editor` by default.
     - Removed the redundant `Open Preview` command.
    
    Closes: #3333.
    
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- packages/core/src/browser/widget-open-handler.ts
+++ packages/core/src/browser/widget-open-handler.ts
@@ -14,8 +14,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-// tslint:disable:no-any
-
 import { inject, postConstruct, injectable } from 'inversify';
 import { Widget, FocusTracker } from '@phosphor/widgets';
 import URI from '../common/uri';
