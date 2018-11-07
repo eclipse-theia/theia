826d45f0bb454bb7514b75a7139e91f47fac1a52
Vitaliy Gulyy <vgulyy@redhat.com>
2018-11-07 14:57:40 +0200

    Register Text Document Content Provider plugin API
    Signed-off-by: Vitaliy Gulyy <vgulyy@redhat.com>

--- packages/plugin-ext/src/plugin/plugin-context.ts
+++ packages/plugin-ext/src/plugin/plugin-context.ts
@@ -103,7 +103,7 @@ export function createAPIFactory(
     const editorsAndDocuments = rpc.set(MAIN_RPC_CONTEXT.EDITORS_AND_DOCUMENTS_EXT, new EditorsAndDocumentsExtImpl(rpc));
     const editors = rpc.set(MAIN_RPC_CONTEXT.TEXT_EDITORS_EXT, new TextEditorsExtImpl(rpc, editorsAndDocuments));
     const documents = rpc.set(MAIN_RPC_CONTEXT.DOCUMENTS_EXT, new DocumentsExtImpl(rpc, editorsAndDocuments));
-    const workspaceExt = rpc.set(MAIN_RPC_CONTEXT.WORKSPACE_EXT, new WorkspaceExtImpl(rpc));
+    const workspaceExt = rpc.set(MAIN_RPC_CONTEXT.WORKSPACE_EXT, new WorkspaceExtImpl(rpc, editorsAndDocuments));
     const statusBarMessageRegistryExt = new StatusBarMessageRegistryExt(rpc);
     const terminalExt = rpc.set(MAIN_RPC_CONTEXT.TERMINAL_EXT, new TerminalServiceExtImpl(rpc));
     const outputChannelRegistryExt = new OutputChannelRegistryExt(rpc);
@@ -269,29 +269,25 @@ export function createAPIFactory(
             onDidChangeConfiguration(listener, thisArgs?, disposables?): theia.Disposable {
                 return preferenceRegistryExt.onDidChangeConfiguration(listener, thisArgs, disposables);
             },
-            openTextDocument(uriOrFileNameOrOptions?: theia.Uri | string | { language?: string; content?: string; }) {
-                let uriPromise: Promise<Uri>;
-
+            async openTextDocument(uriOrFileNameOrOptions?: theia.Uri | string | { language?: string; content?: string; }): Promise<theia.TextDocument | undefined> {
                 const options = uriOrFileNameOrOptions as { language?: string; content?: string; };
+
+                let uri: Uri;
                 if (typeof uriOrFileNameOrOptions === 'string') {
-                    uriPromise = Promise.resolve(Uri.file(uriOrFileNameOrOptions));
+                    uri = Uri.file(uriOrFileNameOrOptions);
+
                 } else if (uriOrFileNameOrOptions instanceof Uri) {
-                    uriPromise = Promise.resolve(uriOrFileNameOrOptions);
+                    uri = uriOrFileNameOrOptions;
+
                 } else if (!options || typeof options === 'object') {
-                    uriPromise = documents.createDocumentData(options);
+                    uri = await documents.createDocumentData(options);
+
                 } else {
-                    throw new Error('illegal argument - uriOrFileNameOrOptions');
+                    return Promise.reject('illegal argument - uriOrFileNameOrOptions');
                 }
 
-                return uriPromise.then(uri =>
-                    documents.ensureDocumentData(uri).then(() => {
-                        const data = documents.getDocumentData(uri);
-                        return data && data.document;
-                    }));
-            },
-            registerTextDocumentContentProvider(scheme: string, provider: theia.TextDocumentContentProvider) {
-                // FIXME: to implement
-                return new Disposable(() => { });
+                const data = await documents.openDocument(uri);
+                return data && data.document;
             },
             createFileSystemWatcher(globPattern: theia.GlobPattern,
                 ignoreCreateEvents?: boolean,
@@ -301,6 +297,9 @@ export function createAPIFactory(
             },
             findFiles(include: theia.GlobPattern, exclude?: theia.GlobPattern | undefined, maxResults?: number, token?: CancellationToken): PromiseLike<Uri[]> {
                 return workspaceExt.findFiles(include, undefined, maxResults, token);
+            },
+            registerTextDocumentContentProvider(scheme: string, provider: theia.TextDocumentContentProvider): theia.Disposable {
+                return workspaceExt.registerTextDocumentContentProvider(scheme, provider);
             },
             registerFileSystemProvider(scheme: string, provider: theia.FileSystemProvider, options?: { isCaseSensitive?: boolean, isReadonly?: boolean }): theia.Disposable {
                 // FIXME: to implement
--- packages/plugin-ext/src/plugin/workspace.ts
+++ packages/plugin-ext/src/plugin/workspace.ts
@@ -14,12 +14,26 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { WorkspaceFolder, WorkspaceFoldersChangeEvent, WorkspaceFolderPickOptions, GlobPattern, FileSystemWatcher } from '@theia/plugin';
+import {
+    WorkspaceFolder,
+    WorkspaceFoldersChangeEvent,
+    WorkspaceFolderPickOptions,
+    GlobPattern,
+    FileSystemWatcher,
+    TextDocumentContentProvider,
+    Disposable
+} from '@theia/plugin';
 import { Event, Emitter } from '@theia/core/lib/common/event';
 import { CancellationToken } from '@theia/core/lib/common/cancellation';
-import { WorkspaceExt, WorkspaceFolderPickOptionsMain, WorkspaceMain, PLUGIN_RPC_CONTEXT as Ext } from '../api/plugin-api';
+import {
+    WorkspaceExt,
+    WorkspaceFolderPickOptionsMain,
+    WorkspaceMain,
+    PLUGIN_RPC_CONTEXT as Ext
+} from '../api/plugin-api';
 import { Path } from '@theia/core/lib/common/path';
 import { RPCProtocol } from '../api/rpc-protocol';
+import { EditorsAndDocumentsExtImpl } from './editors-and-documents';
 import URI from 'vscode-uri';
 
 export class WorkspaceExtImpl implements WorkspaceExt {
@@ -31,7 +45,9 @@ export class WorkspaceExtImpl implements WorkspaceExt {
 
     private folders: WorkspaceFolder[] | undefined;
 
-    constructor(rpc: RPCProtocol) {
+    private documentContentProviders = new Map<string, TextDocumentContentProvider>();
+
+    constructor(rpc: RPCProtocol, private editorsAndDocuments: EditorsAndDocumentsExtImpl) {
         this.proxy = rpc.getProxy(Ext.WORKSPACE_MAIN);
     }
 
@@ -66,7 +82,7 @@ export class WorkspaceExtImpl implements WorkspaceExt {
     }
 
     findFiles(include: GlobPattern, exclude?: GlobPattern | undefined, maxResults?: number,
-              token: CancellationToken = CancellationToken.None): PromiseLike<URI[]> {
+        token: CancellationToken = CancellationToken.None): PromiseLike<URI[]> {
         let includePattern: string;
         if (include) {
             if (typeof include === 'string') {
@@ -102,6 +118,50 @@ export class WorkspaceExtImpl implements WorkspaceExt {
     createFileSystemWatcher(globPattern: GlobPattern, ignoreCreateEvents?: boolean, ignoreChangeEvents?: boolean, ignoreDeleteEvents?: boolean): FileSystemWatcher {
         // FIXME: to implement
         return new Proxy(<FileSystemWatcher>{}, {});
+    }
+
+    registerTextDocumentContentProvider(scheme: string, provider: TextDocumentContentProvider): Disposable {
+        if (scheme === 'file' || scheme === 'untitled' || this.documentContentProviders.has(scheme)) {
+            throw new Error(`Text Content Document Provider for scheme '${scheme}' is already registered`);
+        }
+
+        this.documentContentProviders.set(scheme, provider);
+        this.proxy.$registerTextDocumentContentProvider(scheme);
+
+        let onDidChangeSubscription: Disposable;
+        if (typeof provider.onDidChange === 'function') {
+            onDidChangeSubscription = provider.onDidChange(async uri => {
+                if (uri.scheme === scheme && this.editorsAndDocuments.getDocument(uri.toString())) {
+                    const content = await this.$provideTextDocumentContent(uri.toString());
+                    if (content) {
+                        this.proxy.$onTextDocumentContentChange(uri.toString(), content);
+                    }
+                }
+            });
+        }
+
+        const instance = this;
+        return {
+            dispose() {
+                if (instance.documentContentProviders.delete(scheme)) {
+                    instance.proxy.$unregisterTextDocumentContentProvider(scheme);
+                }
+
+                if (onDidChangeSubscription) {
+                    onDidChangeSubscription.dispose();
+                }
+            }
+        };
+    }
+
+    async $provideTextDocumentContent(documentURI: string): Promise<string | undefined> {
+        const uri = URI.parse(documentURI);
+        const provider = this.documentContentProviders.get(uri.scheme);
+        if (provider) {
+            return provider.provideTextDocumentContent(uri, CancellationToken.None);
+        }
+
+        return undefined;
     }
 
 }
