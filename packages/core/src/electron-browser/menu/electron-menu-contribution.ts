6340b339929b5f2706c97b17864a7a5746363a7b
Paul Maréchal <paul.marechal@ericsson.com>
2018-11-14 14:11:49 -0500

    [electron] OSX: Replace menus when changing window
    OSX uses one title menu bar, shared accross all windows. This commit recreates
    the menus when we switch windows, so that the actions are correctly bound to
    the right window.
    
    Signed-off-by: Paul Maréchal <paul.marechal@ericsson.com>

--- packages/core/src/electron-browser/menu/electron-menu-contribution.ts
+++ packages/core/src/electron-browser/menu/electron-menu-contribution.ts
@@ -77,11 +77,21 @@ export class ElectronMenuContribution implements FrontendApplicationContribution
             }
         }
 
+        const currentWindow = electron.remote.getCurrentWindow();
         const createdMenuBar = this.factory.createMenuBar();
+
         if (isOSX) {
             electron.remote.Menu.setApplicationMenu(createdMenuBar);
+            currentWindow.on('focus', () =>
+                // OSX: Recreate the menus when changing windows.
+                // OSX only has one menu bar for all windows, so we need to swap
+                // between them as the user switch windows.
+                electron.remote.Menu.setApplicationMenu(this.factory.createMenuBar())
+            );
+
         } else {
-            electron.remote.getCurrentWindow().setMenu(createdMenuBar);
+            // Unix/Windows: Set the per-window menus
+            currentWindow.setMenu(createdMenuBar);
         }
     }
 
