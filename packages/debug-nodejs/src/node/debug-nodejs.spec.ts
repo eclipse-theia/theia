9fe8aa63bb2b2568abbd5f7e49b36b57c49e2a49
Anton Kosyakov <anton.kosyakov@typefox.io>
2018-12-03 12:04:48 +0000

    added missing spec tests to enable coverage stats
    Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

--- packages/debug-nodejs/src/node/debug-nodejs.spec.ts
+++ packages/debug-nodejs/src/node/debug-nodejs.spec.ts
@@ -13,3 +13,8 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
+
+describe('node-debug package', () => {
+
+    it('support code coverage statistics', () => true);
+});
