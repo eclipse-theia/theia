a37b81d0643fd930f4e520a78ff4b7a4115e5e23
Rob Moran <rob.moran@arm.com>
2018-12-07 10:53:16 +0000

    Upgrade electron to 0.2.14
    Signed-off-by: Rob Moran <rob.moran@arm.com>

--- packages/core/src/electron-browser/menu/electron-context-menu-renderer.ts
+++ packages/core/src/electron-browser/menu/electron-context-menu-renderer.ts
@@ -22,12 +22,12 @@ import { ElectronMainMenuFactory } from './electron-main-menu-factory';
 @injectable()
 export class ElectronContextMenuRenderer implements ContextMenuRenderer {
 
-    constructor( @inject(ElectronMainMenuFactory) private menuFactory: ElectronMainMenuFactory) {
+    constructor(@inject(ElectronMainMenuFactory) private menuFactory: ElectronMainMenuFactory) {
     }
 
     render(menuPath: MenuPath, anchor: Anchor, onHide?: () => void): void {
         const menu = this.menuFactory.createContextMenu(menuPath);
-        menu.popup();
+        menu.popup({});
         if (onHide) {
             onHide();
         }
