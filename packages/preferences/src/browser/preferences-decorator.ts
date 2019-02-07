eclipse-theia/theia

commit 57b8add5db31a2da5d8b501cdd0ed76609dbcb24

escape and unescape \n and \r\n
Signed-off-by: Uni Sayo <unibtc@gmail.com>

move escape/unescape functions to core common strings.

Signed-off-by: Uni Sayo <unibtc@gmail.com>

use explicit string type

Signed-off-by: Uni Sayo <unibtc@gmail.com>

rename prefValue to value

Signed-off-by: Uni Sayo <unibtc@gmail.com>

packages/preferences/src/browser/preferences-decorator.ts
@@ -17,6 +17,7 @@
 import { inject, injectable } from 'inversify';
 import { Tree, TreeDecorator, TreeDecoration, PreferenceProperty, PreferenceService } from '@theia/core/lib/browser';
 import { Emitter, Event, MaybePromise } from '@theia/core';
+import { escapeInvisibleChars } from '@theia/core/lib/common/strings';
 
 @injectable()
 export class PreferencesDecorator implements TreeDecorator {
@@ -50,7 +51,8 @@ export class PreferencesDecorator implements TreeDecorator {
                 tooltip: preferenceValue.description,
                 captionSuffixes: [
                     {
-                        data: storedValue !== undefined ? ': ' + storedValue : preferenceValue.default !== undefined ? ': ' + preferenceValue.default : undefined,
+                        data: storedValue !== undefined && typeof storedValue === 'string' ? ': ' + escapeInvisibleChars(storedValue) :
+                            preferenceValue.default !== undefined ? ': ' + preferenceValue.default : undefined,
                     },
                     {
                         data: ' ' + preferenceValue.description,
@@ -64,4 +66,5 @@ export class PreferencesDecorator implements TreeDecorator {
     decorations(tree: Tree): MaybePromise<Map<string, TreeDecoration.Data>> {
         return this.preferencesDecorations;
     }
+
 }

packages/core/src/common/strings.ts
@@ -26,3 +26,11 @@ export function* split(s: string, splitter: string): IterableIterator<string> {
         start = end + splitter.length;
     }
 }
+
+export function escapeInvisibleChars(value: string ): string  {
+    return value.replace(/\n/g, '\\n').replace(/\r/g, '\\r');
+}
+
+export function unescapeInvisibleChars(value: string): string {
+    return value.replace(/\\n/g, '\n').replace(/\\r/g, '\r');
+}

packages/preferences/src/browser/preferences-menu-factory.ts
@@ -18,6 +18,7 @@ import { injectable } from 'inversify';
 import { Menu } from '@phosphor/widgets';
 import { CommandRegistry } from '@phosphor/commands';
 import { PreferenceProperty } from '@theia/core/lib/browser';
+import { escapeInvisibleChars, unescapeInvisibleChars } from '@theia/core/lib/common/strings';
 
 @injectable()
 export class PreferencesMenuFactory {
@@ -29,13 +30,14 @@ export class PreferencesMenuFactory {
         if (property) {
             const enumConst = property.enum;
             if (enumConst) {
-                enumConst.forEach(enumValue => {
+                enumConst.map(escapeInvisibleChars)
+                .forEach(enumValue => {
                     const commandId = id + '-' + enumValue;
                     if (!commands.hasCommand(commandId)) {
                         commands.addCommand(commandId, {
                             label: enumValue,
-                            iconClass: savedPreference === enumValue || !savedPreference && property.default === enumValue ? 'fa fa-check' : '',
-                            execute: () => execute(id, enumValue)
+                            iconClass: escapeInvisibleChars(savedPreference) === enumValue || !savedPreference && property.default === enumValue ? 'fa fa-check' : '',
+                            execute: () => execute(id, unescapeInvisibleChars(enumValue))
                         });
                         menu.addItem({
                             type: 'command',

commit 63a04ef0288e21a9aa5da4936dfb1c2173e3d479

fix displayed values in the preference editor
- preference values displayed in the tree of preference editor are broken by the commit https://github.com/theia-ide/theia/commit/57b8add5db31a2da5d8b501cdd0ed76609dbcb24. This change fixes the bug where preference values (if boolean or number) don't get updated after being edited.

Signed-off-by: elaihau <liang.huang@ericsson.com>

packages/preferences/src/browser/preferences-decorator.ts
@@ -53,8 +53,7 @@ export class PreferencesDecorator implements TreeDecorator {
                 tooltip: preferenceValue.description,
                 captionSuffixes: [
                     {
-                        data: storedValue !== undefined && typeof storedValue === 'string' ? ': ' + escapeInvisibleChars(storedValue) :
-                            preferenceValue.default !== undefined ? ': ' + preferenceValue.default : undefined,
+                        data: `: ${this.getPreferenceDisplayValue(storedValue, preferenceValue.default)}`
                     },
                     {
                         data: ' ' + preferenceValue.description,
@@ -72,5 +71,16 @@ export class PreferencesDecorator implements TreeDecorator {
     setActiveFolder(folder: string) {
         this.activeFolderUri = folder;
         this.fireDidChangeDecorations(this.preferences);
+    }
+
+    // tslint:disable-next-line:no-any
+    private getPreferenceDisplayValue(storedValue: any, defaultValue: any): any {
+        if (storedValue !== undefined) {
+            if (typeof storedValue === 'string') {
+                return escapeInvisibleChars(storedValue);
+            }
+            return storedValue;
+        }
+        return defaultValue;
     }
 }