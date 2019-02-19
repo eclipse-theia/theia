335f6600f503d4c59a37d21efa98f1c838af035a
Sven Efftinge <sven.efftinge@typefox.io>
2019-02-19 12:55:53 +0000

    [terminal] introduced font preferences
    Signed-off-by: Sven Efftinge <sven.efftinge@typefox.io>

--- packages/terminal/src/browser/terminal-preferences.ts
+++ packages/terminal/src/browser/terminal-preferences.ts
@@ -16,6 +16,7 @@
 
 import { interfaces } from 'inversify';
 import { createPreferenceProxy, PreferenceProxy, PreferenceService, PreferenceContribution, PreferenceSchema } from '@theia/core/lib/browser';
+import { EDITOR_FONT_DEFAULTS } from '@theia/editor/lib/browser';
 
 export const TerminalConfigSchema: PreferenceSchema = {
     type: 'object',
@@ -29,14 +30,54 @@ export const TerminalConfigSchema: PreferenceSchema = {
             type: 'boolean',
             description: 'Enable ctrl-v (cmd-v on macOS) to paste from clipboard',
             default: true
-        }
+        },
+        'terminal.integrated.fontFamily': {
+            type: 'string',
+            description: 'Controls the font family of the terminal.',
+            default: EDITOR_FONT_DEFAULTS.fontFamily
+        },
+        'terminal.integrated.fontSize': {
+            type: 'number',
+            description: 'Controls the font size in pixels of the terminal.',
+            default: EDITOR_FONT_DEFAULTS.fontSize
+        },
+        'terminal.integrated.fontWeight': {
+            type: 'string',
+            enum: ['normal', 'bold', '100', '200', '300', '400', '500', '600', '700', '800', '900'],
+            description: 'The font weight to use within the terminal for non-bold text.',
+            default: 'normal'
+        },
+        'terminal.integrated.fontWeightBold': {
+            type: 'string',
+            enum: ['normal', 'bold', '100', '200', '300', '400', '500', '600', '700', '800', '900'],
+            description: 'The font weight to use within the terminal for bold text.',
+            default: 'bold'
+        },
+        'terminal.integrated.letterSpacing': {
+            description: 'Controls the letter spacing of the terminal, this is an integer value which represents the amount of additional pixels to add between characters.',
+            type: 'number',
+            default: 1
+        },
+        'terminal.integrated.lineHeight': {
+            description: 'Controls the line height of the terminal, this number is multiplied by the terminal font size to get the actual line-height in pixels.',
+            type: 'number',
+            default: 1
+        },
     }
 };
 
 export interface TerminalConfiguration {
     'terminal.enableCopy': boolean
     'terminal.enablePaste': boolean
+    'terminal.integrated.fontFamily': string
+    'terminal.integrated.fontSize': number
+    'terminal.integrated.fontWeight': FontWeigth
+    'terminal.integrated.fontWeightBold': FontWeigth
+    'terminal.integrated.letterSpacing': number
+    'terminal.integrated.lineHeight': number
 }
+
+type FontWeigth = 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
 
 export const TerminalPreferences = Symbol('TerminalPreferences');
 export type TerminalPreferences = PreferenceProxy<TerminalConfiguration>;
