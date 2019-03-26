73f89c9d9605a4ad083c4d5b96087878658376ff
Oleksii Kurinnyi <okurinny@redhat.com>
2019-03-26 13:59:12 +0200

    Handle changing of 'launch' property in order to re-build preference schema
    `UserLaunchProvider`, `WorkspaceLaunchProvider` and `FoldersLaunchProvider`
    handle changing of 'launch' section of preferences without paying
    attention whether configuration is valid or not. Once it happens a new
    preference schema for launch configurations builds.
    
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>

---
+++ packages/debug/src/browser/abstract-launch-preference-provider.ts
@@ -0,0 +1,157 @@
+/********************************************************************************
+ * Copyright (C) 2019 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import { injectable, postConstruct } from 'inversify';
+import { PreferenceScope, PreferenceProvider } from '@theia/core/lib/browser';
+import { Emitter, Event } from '@theia/core';
+import { Deferred } from '@theia/core/lib/common/promise-util';
+import { DisposableCollection } from '@theia/core';
+import { Disposable } from '@theia/core';
+
+export interface GlobalLaunchConfig {
+    version: string;
+    compounds?: LaunchCompound[];
+    configurations: LaunchConfig[];
+}
+
+export namespace GlobalLaunchConfig {
+    /* tslint:disable-next-line:no-any */
+    export function is(data: any): data is GlobalLaunchConfig {
+        return !data || (!!data.version && (!data.compounds || Array.isArray(data.compounds)) && Array.isArray(data.configurations));
+    }
+}
+
+export interface LaunchConfig {
+    type: string;
+    request: string;
+    name: string;
+
+    /* tslint:disable-next-line:no-any */
+    [field: string]: any;
+}
+
+export interface LaunchCompound {
+    name: string;
+    configurations: (string | { name: string, folder: string })[];
+}
+
+export const LaunchPreferenceProvider = Symbol('LaunchConfigurationProvider');
+export interface LaunchPreferenceProvider {
+
+    readonly onDidLaunchChanged: Event<void>;
+
+    ready: Promise<void>;
+
+    getConfigurationNames(withCompounds: boolean, resourceUri?: string): string[];
+
+}
+
+export const FolderLaunchProviderOptions = Symbol('FolderLaunchProviderOptions');
+export interface FolderLaunchProviderOptions {
+    folderUri: string;
+}
+
+export const LaunchProviderProvider = Symbol('LaunchProviderProvider');
+export type LaunchProviderProvider = (scope: PreferenceScope) => LaunchPreferenceProvider;
+
+@injectable()
+export abstract class AbstractLaunchPreferenceProvider implements LaunchPreferenceProvider, Disposable {
+
+    protected readonly onDidLaunchChangedEmitter = new Emitter<void>();
+    readonly onDidLaunchChanged: Event<void> = this.onDidLaunchChangedEmitter.event;
+
+    protected preferences: GlobalLaunchConfig | undefined;
+
+    protected _ready: Deferred<void> = new Deferred<void>();
+
+    protected readonly toDispose = new DisposableCollection();
+
+    protected readonly preferenceProvider: PreferenceProvider;
+
+    @postConstruct()
+    protected init(): void {
+        this.preferenceProvider.ready
+            .then(() => this._ready.resolve())
+            .catch(() => this._ready.resolve());
+
+        this.updatePreferences();
+        if (this.preferences !== undefined) {
+            this.emitLaunchChangedEvent();
+        }
+
+        this.toDispose.push(this.onDidLaunchChangedEmitter);
+        this.toDispose.push(
+            this.preferenceProvider.onDidNotValidPreferencesRead(prefs => {
+                if (!prefs || !GlobalLaunchConfig.is(prefs.launch)) {
+                    return;
+                }
+                if (!prefs.launch && !this.preferences) {
+                    return;
+                }
+                this.preferences = prefs.launch;
+                this.emitLaunchChangedEvent();
+            })
+        );
+        this.toDispose.push(
+            this.preferenceProvider.onDidPreferencesChanged(prefs => {
+                if (!prefs || !prefs.launch) {
+                    return;
+                }
+                this.updatePreferences();
+                this.emitLaunchChangedEvent();
+            })
+        );
+    }
+
+    protected updatePreferences(): void {
+        const prefs = this.preferenceProvider.getPreferences();
+        if (GlobalLaunchConfig.is(prefs.launch)) {
+            this.preferences = prefs.launch;
+        }
+    }
+
+    protected emitLaunchChangedEvent(): void {
+        this.onDidLaunchChangedEmitter.fire(undefined);
+    }
+
+    get ready(): Promise<void> {
+        return this._ready.promise;
+    }
+
+    dispose(): void {
+        this.toDispose.dispose();
+    }
+
+    getConfigurationNames(withCompounds = true, resourceUri?: string): string[] {
+        const config = this.preferences;
+        if (!config) {
+            return [];
+        }
+
+        const names = config.configurations
+            .filter(launchConfig => launchConfig && typeof launchConfig.name === 'string')
+            .map(launchConfig => launchConfig.name);
+        if (withCompounds && config.compounds) {
+            const compoundNames = config.compounds
+                .filter(compoundConfig => typeof compoundConfig.name === 'string' && compoundConfig.configurations && compoundConfig.configurations.length)
+                .map(compoundConfig => compoundConfig.name);
+            names.push(...compoundNames);
+        }
+
+        return names;
+    }
+
+}
--- packages/debug/src/browser/debug-schema-updater.ts
+++ packages/debug/src/browser/debug-schema-updater.ts
@@ -14,13 +14,16 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { injectable, inject } from 'inversify';
+import { injectable, inject, postConstruct } from 'inversify';
 import { JsonSchemaStore } from '@theia/core/lib/browser/json-schema-store';
 import { InMemoryResources, deepClone } from '@theia/core/lib/common';
 import { IJSONSchema } from '@theia/core/lib/common/json-schema';
 import URI from '@theia/core/lib/common/uri';
 import { DebugService } from '../common/debug-service';
 import { debugPreferencesSchema } from './debug-preferences';
+import { WorkspaceService } from '@theia/workspace/lib/browser';
+import { LaunchPreferenceProvider, LaunchProviderProvider } from './abstract-launch-preference-provider';
+import { PreferenceSchema, PreferenceSchemaProvider, PreferenceScope } from '@theia/core/lib/browser';
 
 @injectable()
 export class DebugSchemaUpdater {
@@ -28,10 +31,67 @@ export class DebugSchemaUpdater {
     @inject(JsonSchemaStore) protected readonly jsonSchemaStore: JsonSchemaStore;
     @inject(InMemoryResources) protected readonly inmemoryResources: InMemoryResources;
     @inject(DebugService) protected readonly debug: DebugService;
+    @inject(WorkspaceService) protected readonly workspaceService: WorkspaceService;
+    @inject(PreferenceSchemaProvider) protected readonly preferenceSchemaProvider: PreferenceSchemaProvider;
+    @inject(LaunchProviderProvider) protected readonly launchProviderProvider: LaunchProviderProvider;
 
-    async update(): Promise<void> {
+    private launchProviders: LaunchPreferenceProvider[] = [];
+
+    private debugLaunchSchemaId = 'vscode://debug/launch.json';
+
+    private schemaIsSet = false;
+
+    @postConstruct()
+    protected init(): void {
+        this.initializeLaunchProviders();
+    }
+
+    protected initializeLaunchProviders(): void {
+        PreferenceScope.getScopes().forEach(scope => {
+            if (scope === PreferenceScope.Default) {
+                return;
+            }
+            const provider = this.launchProviderProvider(scope);
+            this.launchProviders.push(provider);
+        });
+        this.launchProviders.map(p =>
+            p.onDidLaunchChanged(() => {
+                this.updateDebugLaunchSchema();
+            })
+        );
+    }
+
+    protected async updateDebugLaunchSchema(): Promise<void> {
+        const schema = await this.update();
+        this.setDebugLaunchSchema(schema);
+    }
+
+    protected setDebugLaunchSchema(remoteSchema: IJSONSchema) {
+        if (this.schemaIsSet) {
+            this.preferenceSchemaProvider.setRemoteSchema(remoteSchema);
+            return;
+        }
+
+        this.schemaIsSet = true;
+
+        const debugLaunchPreferencesSchema: PreferenceSchema = {
+            type: 'object',
+            scope: 'resource',
+            properties: {
+                'launch': {
+                    type: 'object',
+                    description: "Global debug launch configuration. Should be used as an alternative to 'launch.json' that is shared across workspaces",
+                    default: { configurations: [], compounds: [] },
+                    $ref: launchSchemaId
+                }
+            }
+        };
+        this.preferenceSchemaProvider.setSchema(debugLaunchPreferencesSchema, remoteSchema);
+    }
+
+    async update(): Promise<IJSONSchema> {
         const types = await this.debug.debugTypes();
-        const launchSchemaUrl = new URI('vscode://debug/launch.json');
+        const launchSchemaUrl = new URI(this.debugLaunchSchemaId);
         const schema = { ...deepClone(launchSchema) };
         const items = (<IJSONSchema>schema!.properties!['configurations'].items);
 
@@ -48,6 +108,26 @@ export class DebugSchemaUpdater {
         }
         items.defaultSnippets!.push(...await this.debug.getConfigurationSnippets());
 
+        await Promise.all(this.launchProviders.map(l => l.ready));
+
+        const compoundConfigurationSchema = (schema.properties!.compounds.items as IJSONSchema).properties!.configurations;
+        const launchNames = this.launchProviders
+            .map(launch => launch.getConfigurationNames(false))
+            .reduce((allNames: string[], names: string[]) => {
+                names.forEach(name => {
+                    if (allNames.indexOf(name) === -1) {
+                        allNames.push(name);
+                    }
+                });
+                return allNames;
+            }, []);
+        (compoundConfigurationSchema.items as IJSONSchema).oneOf![0].enum = launchNames;
+        (compoundConfigurationSchema.items as IJSONSchema).oneOf![1].properties!.name.enum = launchNames;
+
+        const roots = await this.workspaceService.roots;
+        const folderNames = roots.map(root => root.uri);
+        (compoundConfigurationSchema.items as IJSONSchema).oneOf![1].properties!.folder.enum = folderNames;
+
         const contents = JSON.stringify(schema);
         try {
             await this.inmemoryResources.update(launchSchemaUrl, contents);
@@ -58,15 +138,17 @@ export class DebugSchemaUpdater {
                 url: launchSchemaUrl.toString()
             });
         }
+
+        return schema;
     }
 }
 
 // debug general schema
-const defaultCompound = { name: 'Compound', configurations: [] };
+export const defaultCompound = { name: 'Compound', configurations: [] };
 
-const launchSchemaId = 'vscode://schemas/launch';
-const launchSchema: IJSONSchema = {
-    id: launchSchemaId,
+export const launchSchemaId = 'vscode://schemas/launch';
+export const launchSchema: IJSONSchema = {
+    $id: launchSchemaId,
     type: 'object',
     title: 'Launch',
     required: [],
