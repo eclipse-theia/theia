de5b900d0fbcdf2f3b041d51760e471e35743f2f
Florent Benoit <fbenoit@redhat.com>
2019-04-01 17:40:34 +0200

    Allow to choose through CLI the VS Code API version that is provided by vscode.version call in a VS Code extension
    $ yarn run start --vscode-api-version=1.40
    
    (and you'll get 1.40 as API version)
    
    note: default value is displayed in help as well
    
    Change-Id: I371e3b008f8d9a8bbd9e047dada4f75e1137e17a
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/plugin-ext-vscode/src/node/plugin-vscode-init.ts
+++ packages/plugin-ext-vscode/src/node/plugin-vscode-init.ts
@@ -19,6 +19,8 @@
 import * as theia from '@theia/plugin';
 import { BackendInitializationFn, PluginAPIFactory, Plugin, emptyPlugin } from '@theia/plugin-ext';
 
+export const VSCODE_DEFAULT_API_VERSION = '1.32.3';
+
 /** Set up en as a default locale for VS Code extensions using vscode-nls */
 process.env['VSCODE_NLS_CONFIG'] = JSON.stringify({ locale: 'en', availableLanguages: {} });
 process.env['VSCODE_PID'] = process.env['THEIA_PARENT_PID'];
@@ -72,7 +74,7 @@ export const doInitialization: BackendInitializationFn = (apiFactory: PluginAPIF
     };
 
     // override the version for vscode to be a VSCode version
-    (<any>vscode).version = '1.32.3';
+    (<any>vscode).version = process.env['VSCODE_API_VERSION'] || VSCODE_DEFAULT_API_VERSION;
 
     pluginsApiImpl.set(plugin.model.id, vscode);
     plugins.push(plugin);
