a72ac1060f1191efc42f6914ba340e1dc3ca0e85
Paul Maréchal <paul.marechal@ericsson.com>
2019-04-08 16:11:17 -0400

    [electron] Bind plugin components on the backend
    The electron inversify container is missing a couple of bindings
    required by the whole plugin system to work correctly.
    
    This commit binds what is missing for plugins to function on electron.
    
    Signed-off-by: Paul Maréchal <paul.marechal@ericsson.com>

--- packages/plugin-ext/src/plugin-ext-backend-electron-module.ts
+++ packages/plugin-ext/src/plugin-ext-backend-electron-module.ts
@@ -16,7 +16,9 @@
 
 import { ContainerModule } from 'inversify';
 import { bindElectronBackend } from './hosted/node-electron/plugin-ext-hosted-electron-backend-module';
+import { bindMainBackend } from './main/node/plugin-ext-backend-module';
 
 export default new ContainerModule(bind => {
+    bindMainBackend(bind);
     bindElectronBackend(bind);
 });
