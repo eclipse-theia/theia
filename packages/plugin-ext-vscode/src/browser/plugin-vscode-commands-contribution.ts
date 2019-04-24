e147cf39c7f484374b7ec0cd0d04958c3ef00bca
Anton Kosyakov <anton.kosyakov@typefox.io>
2019-04-24 06:01:22 +0000

    [plugin] file management vscode commands
    Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

--- packages/plugin-ext-vscode/src/browser/plugin-vscode-commands-contribution.ts
+++ packages/plugin-ext-vscode/src/browser/plugin-vscode-commands-contribution.ts
@@ -23,11 +23,13 @@ import { ContextKeyService } from '@theia/core/lib/browser/context-key-service';
 import { DiffService } from '@theia/workspace/lib/browser/diff-service';
 import { EditorManager } from '@theia/editor/lib/browser';
 import { WebviewWidget } from '@theia/plugin-ext/lib/main/browser/webview/webview';
-import { ApplicationShell } from '@theia/core/lib/browser';
+import { ApplicationShell, NavigatableWidget, OpenerService, open, Saveable } from '@theia/core/lib/browser';
 import { ResourceProvider } from '@theia/core';
 import { ViewColumn } from '@theia/plugin-ext/lib/plugin/types-impl';
 import { TextDocumentShowOptions } from '@theia/plugin-ext/lib/api/model';
 import { fromViewColumn } from '@theia/plugin-ext/lib/plugin/type-converters';
+import { WorkspaceCommands } from '@theia/workspace/lib/browser';
+import { createUntitledResource } from '@theia/plugin-ext/lib/main/browser/editor/untitled-resource';
 
 export namespace VscodeCommands {
     export const OPEN: Command = {
@@ -61,6 +63,8 @@ export class PluginVscodeCommandsContribution implements CommandContribution {
     protected readonly resources: ResourceProvider;
     @inject(DiffService)
     protected readonly diffService: DiffService;
+    @inject(OpenerService)
+    protected readonly openerService: OpenerService;
 
     registerCommands(commands: CommandRegistry): void {
         commands.registerCommand(VscodeCommands.OPEN, {
@@ -133,8 +137,171 @@ export class PluginVscodeCommandsContribution implements CommandContribution {
                 });
 
             }
-        }
-        );
+        });
+
+        // https://code.visualstudio.com/docs/getstarted/keybindings#_navigation
+        /*
+         * internally, in VS Code, any widget opened in the main area is represented as an editor
+         * operations below apply to them, but not to side-bar widgets, like the explorer
+         *
+         * in Theia, there are not such difference and any widget can be put in any area
+         * because of it we filter out editors from views based on `NavigatableWidget.is`
+         * and apply actions only to them
+         */
+        commands.registerCommand({ id: 'workbench.action.files.newUntitledFile' }, {
+            execute: () => open(this.openerService, createUntitledResource().uri)
+        });
+        commands.registerCommand({ id: 'workbench.action.files.openFile' }, {
+            execute: () => commands.executeCommand(WorkspaceCommands.OPEN_FILE.id)
+        });
+        commands.registerCommand({ id: 'workbench.action.files.openFolder' }, {
+            execute: () => commands.executeCommand(WorkspaceCommands.OPEN_FOLDER.id)
+        });
+        commands.registerCommand({ id: 'workbench.action.files.save', }, {
+            execute: (uri?: monaco.Uri) => {
+                if (uri) {
+                    const uriString = uri.toString();
+                    const widget = this.shell.widgets.find(w => {
+                        const resourceUri = Saveable.is(w) && NavigatableWidget.is(w) && w.getResourceUri();
+                        return (resourceUri && resourceUri.toString()) === uriString;
+                    });
+                    if (Saveable.is(widget)) {
+                        Saveable.save(widget);
+                    }
+                } else {
+                    this.shell.save();
+                }
+            }
+        });
+        commands.registerCommand({ id: 'workbench.action.files.saveAll', }, {
+            execute: () => this.shell.saveAll()
+        });
+        commands.registerCommand({ id: 'workbench.action.closeActiveEditor' }, {
+            execute: (uri?: monaco.Uri) => {
+                let widget = this.editorManager.currentEditor || this.shell.currentWidget;
+                if (uri) {
+                    const uriString = uri.toString();
+                    widget = this.shell.widgets.find(w => {
+                        const resourceUri = NavigatableWidget.is(w) && w.getResourceUri();
+                        return (resourceUri && resourceUri.toString()) === uriString;
+                    });
+                }
+                if (NavigatableWidget.is(widget)) {
+                    widget.close();
+                }
+            }
+        });
+        commands.registerCommand({ id: 'workbench.action.closeOtherEditors' }, {
+            execute: (uri?: monaco.Uri) => {
+                let editor = this.editorManager.currentEditor || this.shell.currentWidget;
+                if (uri) {
+                    const uriString = uri.toString();
+                    editor = this.editorManager.all.find(e => {
+                        const resourceUri = e.getResourceUri();
+                        return (resourceUri && resourceUri.toString()) === uriString;
+                    });
+                }
+                for (const widget of this.shell.widgets) {
+                    if (NavigatableWidget.is(widget) && widget !== editor) {
+                        widget.close();
+                    }
+                }
+            }
+        });
+        commands.registerCommand({ id: 'workbench.action.closeEditorsInGroup' }, {
+            execute: (uri?: monaco.Uri) => {
+                let editor = this.editorManager.currentEditor || this.shell.currentWidget;
+                if (uri) {
+                    const uriString = uri.toString();
+                    editor = this.editorManager.all.find(e => {
+                        const resourceUri = e.getResourceUri();
+                        return (resourceUri && resourceUri.toString()) === uriString;
+                    });
+                }
+                if (editor) {
+                    const tabBar = this.shell.getTabBarFor(editor);
+                    if (tabBar) {
+                        this.shell.closeTabs(tabBar,
+                            ({ owner }) => NavigatableWidget.is(owner) && owner !== editor
+                        );
+                    }
+                }
+            }
+        });
+        commands.registerCommand({ id: 'workbench.action.closeEditorsInOtherGroups' }, {
+            execute: () => {
+                const editor = this.editorManager.currentEditor || this.shell.currentWidget;
+                if (editor) {
+                    const editorTabBar = this.shell.getTabBarFor(editor);
+                    for (const tabBar of this.shell.allTabBars) {
+                        if (tabBar !== editorTabBar) {
+                            this.shell.closeTabs(tabBar,
+                                ({ owner }) => NavigatableWidget.is(owner)
+                            );
+                        }
+                    }
+                }
+            }
+        });
+        commands.registerCommand({ id: 'workbench.action.closeEditorsToTheLeft' }, {
+            execute: () => {
+                const editor = this.editorManager.currentEditor || this.shell.currentWidget;
+                if (editor) {
+                    const tabBar = this.shell.getTabBarFor(editor);
+                    if (tabBar) {
+                        let left = true;
+                        this.shell.closeTabs(tabBar,
+                            ({ owner }) => {
+                                if (owner === editor) {
+                                    left = false;
+                                    return false;
+                                }
+                                return left && NavigatableWidget.is(owner);
+                            }
+                        );
+                    }
+                }
+            }
+        });
+        commands.registerCommand({ id: 'workbench.action.closeEditorsToTheRight' }, {
+            execute: () => {
+                const editor = this.editorManager.currentEditor || this.shell.currentWidget;
+                if (editor) {
+                    const tabBar = this.shell.getTabBarFor(editor);
+                    if (tabBar) {
+                        let left = true;
+                        this.shell.closeTabs(tabBar,
+                            ({ owner }) => {
+                                if (owner === editor) {
+                                    left = false;
+                                    return false;
+                                }
+                                return !left && NavigatableWidget.is(owner);
+                            }
+                        );
+                    }
+                }
+            }
+        });
+        commands.registerCommand({ id: 'workbench.action.closeAllEditors' }, {
+            execute: () => {
+                for (const widget of this.shell.widgets) {
+                    if (NavigatableWidget.is(widget)) {
+                        widget.close();
+                    }
+                }
+            }
+        });
+        /**
+         * TODO:
+         * Keep Open	workbench.action.keepEditor
+         * Open Next	workbench.action.openNextRecentlyUsedEditorInGroup
+         * Open Previous	workbench.action.openPreviousRecentlyUsedEditorInGroup
+         * Copy Path of Active File	workbench.action.files.copyPathOfActiveFile
+         * Reveal Active File in Windows	workbench.action.files.revealActiveFileInWindows
+         * Show Opened File in New Window	workbench.action.files.showOpenedFileInNewWindow
+         * Compare Opened File With	workbench.files.action.compareFileWith
+         */
     }
 
     private getHtml(body: String) {
