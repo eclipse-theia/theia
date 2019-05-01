66c6a98594009f674c594866b626c4c61b1d1986
Paul Maréchal <paul.marechal@ericsson.com>
2019-05-01 17:26:44 -0400

    [electron] Replace ffmpeg library
    The ffmpeg library distributed alongside the Electron binaries contains
    proprietary code for the h264 video codec. Fortunately enough, the
    electron organization also releases a stripped ffmpeg library.
    
    New scripts, to be ran using `npx <script>`:
    - `electron-h264-test`: Fails if the Electron distribution installed
      in `node_modules/electron/dist` can read h264 videos.
    - `electron-replace-ffmpeg`: Replace the `ffmpeg` library for the
      Electron distribution installed in `node_modules/electron/dist`.
    
    The test script will exit with `code = 0` if the video cannot be played,
    or with `code > 0` if anything else happens.
    
    Signed-off-by: Paul Maréchal <paul.marechal@ericsson.com>

--- dev-packages/electron/scripts/skip-replace-ffmpeg.js
+++ dev-packages/electron/scripts/skip-replace-ffmpeg.js
@@ -16,20 +16,8 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-/**
- * Theia's `package.json` contains a `prepare` script which is triggered after each install.
- * But we don't always want to build the whole project after installing dependencies.
- * This script looks for an environment variable to skip the prepare step.
- *
- * Example (Unix): `THEIA_SKIP_NPM_PREPARE=1 yarn`
- *
- * Script returns successfully (code=0) when the variable is set, and fails (code>0)
- * when variable is not set. This allows `script || a && b && ...` which will skip
- * the following commands if `script` returns 0, but will keep going otherwise.
- */
-
-if (process.env.THEIA_SKIP_NPM_PREPARE) {
-    console.error('skipping `npm prepare`')
+if (process.env.THEIA_ELECTRON_SKIP_REPLACE_FFMPEG) {
+    console.error('skipping `electron-replace-ffmpeg`')
     process.exit(0) // stop
 } else {
     process.exit(1) // ok
