9f94facaafeff266f8feae7d7965e6cc56562c24
Anatoliy Bazko <abazko@redhat.com>
2019-05-08 10:54:09 +0300

    Fix
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

--- packages/plugin-ext/src/main/browser/plugin-frontend-contribution.ts
+++ packages/plugin-ext/src/main/browser/plugin-frontend-contribution.ts
@@ -20,10 +20,6 @@ import { HostedPluginManagerClient, HostedPluginCommands } from '../../hosted/br
 import { PluginExtDeployCommandService } from './plugin-ext-deploy-command';
 import { OpenUriCommandHandler } from './commands';
 import URI from '@theia/core/lib/common/uri';
-import { ApplicationShell, NavigatableWidget, OpenerService, open, Saveable } from '@theia/core/lib/browser';
-import { createUntitledResource } from './editor/untitled-resource';
-import { WorkspaceCommands } from '@theia/workspace/lib/browser';
-import { EditorManager } from '@theia/editor/lib/browser';
 
 @injectable()
 export class PluginApiFrontendContribution implements CommandContribution {
@@ -36,15 +32,6 @@ export class PluginApiFrontendContribution implements CommandContribution {
 
     @inject(OpenUriCommandHandler)
     protected readonly openUriCommandHandler: OpenUriCommandHandler;
-
-    @inject(ApplicationShell)
-    protected readonly shell: ApplicationShell;
-
-    @inject(EditorManager)
-    protected readonly editors: EditorManager;
-
-    @inject(OpenerService)
-    protected readonly openerService: OpenerService;
 
     registerCommands(commands: CommandRegistry): void {
         commands.registerCommand(HostedPluginCommands.START, {
@@ -67,174 +54,15 @@ export class PluginApiFrontendContribution implements CommandContribution {
             execute: () => this.pluginExtDeployCommandService.deploy()
         });
 
+        // this command only for compatibility reason
+        commands.registerCommand({ id: 'workbench.action.closeActiveEditor' }, {
+            execute: () => commands.executeCommand('core.close.tab')
+        });
+
         commands.registerCommand(OpenUriCommandHandler.COMMAND_METADATA, {
             execute: (arg: URI) => this.openUriCommandHandler.execute(arg),
             isVisible: () => false
         });
-
-        // https://code.visualstudio.com/docs/getstarted/keybindings#_navigation
-        /*
-         * internally, in VS Code, any widget opened in the main area is represented as an editor
-         * operations below apply to them, but not to side-bar widgets, like the explorer
-         *
-         * in Theia, there are not such difference and any widget can be put in any area
-         * because of it we filter out editors from views based on `NavigatableWidget.is`
-         * and apply actions only to them
-         */
-        commands.registerCommand({ id: 'workbench.action.files.newUntitledFile' }, {
-            execute: () => open(this.openerService, createUntitledResource().uri)
-        });
-        commands.registerCommand({ id: 'workbench.action.files.openFile' }, {
-            execute: () => commands.executeCommand(WorkspaceCommands.OPEN_FILE.id)
-        });
-        commands.registerCommand({ id: 'workbench.action.files.openFolder' }, {
-            execute: () => commands.executeCommand(WorkspaceCommands.OPEN_FOLDER.id)
-        });
-        commands.registerCommand({ id: 'workbench.action.files.save', }, {
-            execute: (uri?: monaco.Uri) => {
-                if (uri) {
-                    const uriString = uri.toString();
-                    const widget = this.shell.widgets.find(w => {
-                        const resourceUri = Saveable.is(w) && NavigatableWidget.is(w) && w.getResourceUri();
-                        return (resourceUri && resourceUri.toString()) === uriString;
-                    });
-                    if (Saveable.is(widget)) {
-                        Saveable.save(widget);
-                    }
-                } else {
-                    this.shell.save();
-                }
-            }
-        });
-        commands.registerCommand({ id: 'workbench.action.files.saveAll', }, {
-            execute: () => this.shell.saveAll()
-        });
-        commands.registerCommand({ id: 'workbench.action.closeActiveEditor' }, {
-            execute: (uri?: monaco.Uri) => {
-                let widget = this.editors.currentEditor || this.shell.currentWidget;
-                if (uri) {
-                    const uriString = uri.toString();
-                    widget = this.shell.widgets.find(w => {
-                        const resourceUri = NavigatableWidget.is(w) && w.getResourceUri();
-                        return (resourceUri && resourceUri.toString()) === uriString;
-                    });
-                }
-                if (NavigatableWidget.is(widget)) {
-                    widget.close();
-                }
-            }
-        });
-        commands.registerCommand({ id: 'workbench.action.closeOtherEditors' }, {
-            execute: (uri?: monaco.Uri) => {
-                let editor = this.editors.currentEditor || this.shell.currentWidget;
-                if (uri) {
-                    const uriString = uri.toString();
-                    editor = this.editors.all.find(e => {
-                        const resourceUri = e.getResourceUri();
-                        return (resourceUri && resourceUri.toString()) === uriString;
-                    });
-                }
-                for (const widget of this.shell.widgets) {
-                    if (NavigatableWidget.is(widget) && widget !== editor) {
-                        widget.close();
-                    }
-                }
-            }
-        });
-        commands.registerCommand({ id: 'workbench.action.closeEditorsInGroup' }, {
-            execute: (uri?: monaco.Uri) => {
-                let editor = this.editors.currentEditor || this.shell.currentWidget;
-                if (uri) {
-                    const uriString = uri.toString();
-                    editor = this.editors.all.find(e => {
-                        const resourceUri = e.getResourceUri();
-                        return (resourceUri && resourceUri.toString()) === uriString;
-                    });
-                }
-                if (editor) {
-                    const tabBar = this.shell.getTabBarFor(editor);
-                    if (tabBar) {
-                        this.shell.closeTabs(tabBar,
-                            ({ owner }) => NavigatableWidget.is(owner) && owner !== editor
-                        );
-                    }
-                }
-            }
-        });
-        commands.registerCommand({ id: 'workbench.action.closeEditorsInOtherGroups' }, {
-            execute: () => {
-                const editor = this.editors.currentEditor || this.shell.currentWidget;
-                if (editor) {
-                    const editorTabBar = this.shell.getTabBarFor(editor);
-                    for (const tabBar of this.shell.allTabBars) {
-                        if (tabBar !== editorTabBar) {
-                            this.shell.closeTabs(tabBar,
-                                ({ owner }) => NavigatableWidget.is(owner)
-                            );
-                        }
-                    }
-                }
-            }
-        });
-        commands.registerCommand({ id: 'workbench.action.closeEditorsToTheLeft' }, {
-            execute: () => {
-                const editor = this.editors.currentEditor || this.shell.currentWidget;
-                if (editor) {
-                    const tabBar = this.shell.getTabBarFor(editor);
-                    if (tabBar) {
-                        let left = true;
-                        this.shell.closeTabs(tabBar,
-                            ({ owner }) => {
-                                if (owner === editor) {
-                                    left = false;
-                                    return false;
-                                }
-                                return left && NavigatableWidget.is(owner);
-                            }
-                        );
-                    }
-                }
-            }
-        });
-        commands.registerCommand({ id: 'workbench.action.closeEditorsToTheRight' }, {
-            execute: () => {
-                const editor = this.editors.currentEditor || this.shell.currentWidget;
-                if (editor) {
-                    const tabBar = this.shell.getTabBarFor(editor);
-                    if (tabBar) {
-                        let left = true;
-                        this.shell.closeTabs(tabBar,
-                            ({ owner }) => {
-                                if (owner === editor) {
-                                    left = false;
-                                    return false;
-                                }
-                                return !left && NavigatableWidget.is(owner);
-                            }
-                        );
-                    }
-                }
-            }
-        });
-        commands.registerCommand({ id: 'workbench.action.closeAllEditors' }, {
-            execute: () => {
-                for (const widget of this.shell.widgets) {
-                    if (NavigatableWidget.is(widget)) {
-                        widget.close();
-                    }
-                }
-            }
-        });
-        /**
-         * TODO:
-         * Keep Open	workbench.action.keepEditor
-         * Open Next	workbench.action.openNextRecentlyUsedEditorInGroup
-         * Open Previous	workbench.action.openPreviousRecentlyUsedEditorInGroup
-         * Copy Path of Active File	workbench.action.files.copyPathOfActiveFile
-         * Reveal Active File in Windows	workbench.action.files.revealActiveFileInWindows
-         * Show Opened File in New Window	workbench.action.files.showOpenedFileInNewWindow
-         * Compare Opened File With	workbench.files.action.compareFileWith
-         */
     }
 
 }
