fe69fb7f5c9f94e15986467ea18cc0d90f2249fe
Brokun <brokun0128@gmail.com>
2019-08-18 21:41:11 +0800

    fix #5229: Add language configuration items for js, ts and jsx
    Signed-off-by: Brokun <brokun0128@gmail.com>

--- packages/textmate-grammars/src/browser/js.ts
+++ packages/textmate-grammars/src/browser/js.ts
@@ -13,6 +13,10 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 import { injectable } from 'inversify';
 import { LanguageGrammarDefinitionContribution, TextmateRegistry, getEncodedLanguageId, GrammarDefinition } from '@theia/monaco/lib/browser/textmate';
@@ -117,6 +121,45 @@ export class JavascriptContribution implements LanguageGrammarDefinitionContribu
     }
 
     protected configuration: monaco.languages.LanguageConfiguration = {
+        // copied and modified from https://github.com/microsoft/vscode/blob/master/extensions/typescript-language-features/src/features/languageConfiguration.ts
+        'indentationRules': {
+            'decreaseIndentPattern': /^((?!.*?\/\*).*\*\/)?\s*[\}\]].*$/,
+            'increaseIndentPattern': /^((?!\/\/).)*(\{[^}"'`]*|\([^)"'`]*|\[[^\]"'`]*)$/
+        },
+        'wordPattern': /(-?\d*\.\d\w*)|([^\`\~\!\@\#\%\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
+        'onEnterRules': [
+            {
+                // e.g. /** | */
+                'beforeText': /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+                'afterText': /^\s*\*\/$/,
+                'action': { indentAction: monaco.languages.IndentAction.IndentOutdent, appendText: ' * ' },
+            },
+            {
+                // e.g. /** ...|
+                'beforeText': /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+                'action': { indentAction: monaco.languages.IndentAction.None, appendText: ' * ' },
+            },
+            {
+                // e.g.  * ...|
+                'beforeText': /^(\t|[ ])*[ ]\*([ ]([^\*]|\*(?!\/))*)?$/,
+                'action': { indentAction: monaco.languages.IndentAction.None, appendText: '* ' },
+            },
+            {
+                // e.g.  */|
+                'beforeText': /^(\t|[ ])*[ ]\*\/\s*$/,
+                'action': { indentAction: monaco.languages.IndentAction.None, removeText: 1 },
+            },
+            {
+                // e.g.  *-----*/|
+                'beforeText': /^(\t|[ ])*[ ]\*[^/]*\*\/\s*$/,
+                'action': { indentAction: monaco.languages.IndentAction.None, removeText: 1 },
+            },
+            {
+                'beforeText': /^\s*(\bcase\s.+:|\bdefault:)$/,
+                'afterText': /^(?!\s*(\bcase\b|\bdefault\b))/,
+                'action': { indentAction: monaco.languages.IndentAction.Indent },
+            }
+        ],
         'comments': {
             'lineComment': '//',
             'blockComment': ['/*', '*/']
