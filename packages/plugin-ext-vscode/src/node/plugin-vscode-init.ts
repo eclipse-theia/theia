03e3ed66e0d58510b701fb4e2f67c0b8035e053e
Doron Nahari <doron.nahari@sap.com>
2019-09-05 14:53:13 +0300

    Update vscode default api version to 1.38.0
    Signed-off-by: Doron Nahari <doron.nahari@sap.com>

--- packages/plugin-ext-vscode/src/node/plugin-vscode-init.ts
+++ packages/plugin-ext-vscode/src/node/plugin-vscode-init.ts
@@ -19,7 +19,7 @@
 import * as theia from '@theia/plugin';
 import { BackendInitializationFn, PluginAPIFactory, Plugin, emptyPlugin } from '@theia/plugin-ext';
 
-export const VSCODE_DEFAULT_API_VERSION = '1.33.1';
+export const VSCODE_DEFAULT_API_VERSION = '1.38.0';
 
 /** Set up en as a default locale for VS Code extensions using vscode-nls */
 process.env['VSCODE_NLS_CONFIG'] = JSON.stringify({ locale: 'en', availableLanguages: {} });
