eclipse-theia/theia

commit 657bda85aeedcc91cd295d4a4eba967ed0fb79c3

fix #5025: reload plugins on reconnect
Also:
- fix #5829: ensure that each plugin is loaded only once
- fix #6186: start only one plugin host process for the same host instead of a new for each load as before
- revert #5257: Reconnect same host plugin process and client" 797db759bcdde66c8bbabf2ab0b041bc7d887d30

Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

packages/task/src/browser/task-problem-matcher-registry.ts
@@ -20,6 +20,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { inject, injectable, postConstruct } from 'inversify';
+import { Disposable, DisposableCollection } from '@theia/core/lib/common/disposable';
 import {
     ApplyToKind, FileLocationKind, NamedProblemMatcher, Severity,
     ProblemPattern, ProblemMatcher, ProblemMatcherContribution, WatchingMatcher
@@ -29,7 +30,7 @@ import { ProblemPatternRegistry } from './task-problem-pattern-registry';
 @injectable()
 export class ProblemMatcherRegistry {
 
-    private matchers: { [name: string]: NamedProblemMatcher };
+    private readonly matchers = new Map<string, NamedProblemMatcher>();
     private readyPromise: Promise<void>;
 
     @inject(ProblemPatternRegistry)
@@ -37,8 +38,6 @@ export class ProblemMatcherRegistry {
 
     @postConstruct()
     protected init(): void {
-        // tslint:disable-next-line:no-null-keyword
-        this.matchers = Object.create(null);
         this.problemPatternRegistry.onReady().then(() => {
             this.fillDefaults();
             this.readyPromise = new Promise<void>((res, rej) => res(undefined));
@@ -54,13 +53,21 @@ export class ProblemMatcherRegistry {
      *
      * @param definition the problem matcher to be added.
      */
-    async register(matcher: ProblemMatcherContribution): Promise<void> {
+    register(matcher: ProblemMatcherContribution): Disposable {
         if (!matcher.name) {
             console.error('Only named Problem Matchers can be registered.');
+            return Disposable.NULL;
+        }
+        const toDispose = new DisposableCollection(Disposable.create(() => {/* mark as not disposed */ }));
+        this.doRegister(matcher, toDispose);
+        return toDispose;
+    }
+    protected async doRegister(matcher: ProblemMatcherContribution, toDispose: DisposableCollection): Promise<void> {
+        const problemMatcher = await this.getProblemMatcherFromContribution(matcher);
+        if (toDispose.disposed) {
             return;
         }
-        const problemMatcher = await this.getProblemMatcherFromContribution(matcher);
-        this.add(problemMatcher as NamedProblemMatcher);
+        toDispose.push(this.add(problemMatcher as NamedProblemMatcher));
     }
 
     /**
@@ -71,9 +78,9 @@ export class ProblemMatcherRegistry {
      */
     get(name: string): NamedProblemMatcher | undefined {
         if (name.startsWith('$')) {
-            return this.matchers[name.slice(1)];
+            return this.matchers.get(name.slice(1));
         }
-        return this.matchers[name];
+        return this.matchers.get(name);
     }
 
     /**
@@ -127,8 +134,9 @@ export class ProblemMatcherRegistry {
         return problemMatcher;
     }
 
-    private add(matcher: NamedProblemMatcher): void {
-        this.matchers[matcher.name] = matcher;
+    private add(matcher: NamedProblemMatcher): Disposable {
+        this.matchers.set(matcher.name, matcher);
+        return Disposable.create(() => this.matchers.delete(matcher.name));
     }
 
     private getFileLocationKindAndPrefix(matcher: ProblemMatcherContribution): { fileLocation: FileLocationKind, filePrefix: string } {

commit 938b0a8f98c6b97c2faba958df65128321596b14

fix problem matcher items in the prompt before task is started
When user starts a task that is not associated with any problem
matchers, s/he should be prompted with a list of problem matchers that
are already defined in Theia. In the current version of Theia, the
problem matcher list is broken by 657bda85aeedcc91cd295d4a4eba967ed0fb79c3, and this change fixes the problem.

Signed-off-by: Liang Huang <liang.huang@ericsson.com>

packages/task/src/browser/task-problem-matcher-registry.ts
@@ -88,7 +88,7 @@ export class ProblemMatcherRegistry {
      */
     getAll(): NamedProblemMatcher[] {
         const all: NamedProblemMatcher[] = [];
-        for (const matcherName of Object.keys(this.matchers)) {
+        for (const matcherName of this.matchers.keys()) {
             all.push(this.get(matcherName)!);
         }
         all.sort((one, other) => one.name.localeCompare(other.name));