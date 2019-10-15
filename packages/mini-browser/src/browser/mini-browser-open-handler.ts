938f3107d096f4f6a9f726546b71aaa3931d47af
二凢 <dingyu.wdy@alibaba-inc.com>
2019-10-15 15:48:34 +0800

    open minibrowser without url encode

--- packages/mini-browser/src/browser/mini-browser-open-handler.ts
+++ packages/mini-browser/src/browser/mini-browser-open-handler.ts
@@ -132,7 +132,7 @@ export class MiniBrowserOpenHandler extends NavigatableWidgetOpenHandler<MiniBro
         let result = await this.defaultOptions();
         if (uri) {
             // Decorate it with a few properties inferred from the URI.
-            const startPage = uri.toString();
+            const startPage = uri.toString(true);
             const name = this.labelProvider.getName(uri);
             const iconClass = `${await this.labelProvider.getIcon(uri)} file-icon`;
             // The background has to be reset to white only for "real" web-pages but not for images, for instance.
