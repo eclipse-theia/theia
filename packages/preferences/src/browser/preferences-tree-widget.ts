5413de0084f963f5d6c19c6835ca1abff4114148
Anatoliy Bazko <abazko@redhat.com>
2019-11-27 11:32:12 +0200

    [json] remove dependencies to @theia/json
    Fixes #4217
    
    Removes dependencies to `@theia/json` from both the
    `@theia/preferences` and `@theia/debug` extensions so that
    we can use vscode builtin extensions instead.
    
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

--- packages/preferences/src/browser/preferences-tree-widget.ts
+++ packages/preferences/src/browser/preferences-tree-widget.ts
@@ -41,7 +41,6 @@ import { UserPreferenceProvider } from './user-preference-provider';
 import { WorkspacePreferenceProvider } from './workspace-preference-provider';
 import { PreferencesEditorWidget, PreferenceEditorContainerTabBarRenderer } from './preference-editor-widget';
 import { EditorWidget, EditorManager } from '@theia/editor/lib/browser';
-import { JSONC_LANGUAGE_ID } from '@theia/json/lib/common';
 import { DisposableCollection, Emitter, Event, MessageService } from '@theia/core';
 import { Deferred } from '@theia/core/lib/common/promise-util';
 import { FileSystem, FileSystemUtils } from '@theia/filesystem/lib/common';
@@ -346,7 +345,7 @@ export class PreferencesEditorsContainer extends DockPanel {
             workspacePreferences.title.label = 'Workspace';
             workspacePreferences.title.caption = `Workspace Preferences: ${await this.getPreferenceEditorCaption(workspacePreferenceUri!)}`;
             workspacePreferences.title.iconClass = 'database-icon medium-yellow file-icon';
-            workspacePreferences.editor.setLanguage(JSONC_LANGUAGE_ID);
+            workspacePreferences.editor.setLanguage('jsonc');
             workspacePreferences.scope = PreferenceScope.Workspace;
         }
         return workspacePreferences;
