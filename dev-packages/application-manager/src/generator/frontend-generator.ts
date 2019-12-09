5e27f516ae64f24771701784b67604e6c673da3d
Akos Kitta <kittaakos@typefox.io>
2019-12-09 20:28:07 +0100

    Explicitly set the electron app name.
    To have better menu items on macOS.
    ("About", "Hide", and "Quit")
    
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- dev-packages/application-manager/src/generator/frontend-generator.ts
+++ dev-packages/application-manager/src/generator/frontend-generator.ts
@@ -141,6 +141,10 @@ const Storage = require('electron-store');
 const electronStore = new Storage();
 
 app.on('ready', () => {
+    // Explicitly set the app name to have better menu items on macOS. ("About", "Hide", and "Quit")
+    // See: https://github.com/electron-userland/electron-builder/issues/2468
+    app.setName(applicationName);
+
     const { screen } = electron;
 
     // Remove the default electron menus, waiting for the application to set its own.
