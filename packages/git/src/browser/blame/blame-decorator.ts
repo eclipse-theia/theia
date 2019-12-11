d06c9fe1c249f48ac3ab9987de0db15d2e1fa4f4
Anton Kosyakov <anton.kosyakov@typefox.io>
2019-12-11 16:06:06 +0000

    apply badge vscode colors
    Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

--- packages/git/src/browser/blame/blame-decorator.ts
+++ packages/git/src/browser/blame/blame-decorator.ts
@@ -210,8 +210,8 @@ export namespace BlameDecorator {
 
     export const defaultGutterStyles = <CSSStyleDeclaration>{
         width: `${maxWidth}ch`,
-        color: 'var(--theia-ui-font-color0)',
-        backgroundColor: 'var(--theia-layout-color1)',
+        color: 'var(--theia-gitlens-gutterForegroundColor)',
+        backgroundColor: 'var(--theia-gitlens-gutterBackgroundColor)',
         height: '100%',
         margin: '0 26px -1px 0',
         display: 'inline-block',
@@ -223,7 +223,7 @@ export namespace BlameDecorator {
     });
 
     export const highlightStyle = new EditorDecorationStyle('git-blame-highlight::before', style => {
-        style.backgroundColor = 'var(--theia-layout-color2)';
+        style.backgroundColor = 'var(--theia-gitlens-lineHighlightBackgroundColor)';
     });
 
 }
