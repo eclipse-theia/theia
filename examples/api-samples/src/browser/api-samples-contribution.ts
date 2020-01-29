deabb78656a56676b3da808fb85e47d17443cfda
Akos Kitta <kittaakos@typefox.io>
2020-01-29 17:07:07 +0100

    Added an example showing the resource leak.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- examples/api-samples/src/browser/api-samples-contribution.ts
+++ examples/api-samples/src/browser/api-samples-contribution.ts
@@ -14,9 +14,12 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { injectable, inject } from 'inversify';
+import { injectable, inject, postConstruct } from 'inversify';
 import { Command, CommandContribution, CommandRegistry, CommandHandler } from '@theia/core';
 import { FrontendApplicationContribution } from '@theia/core/lib/browser';
+import { MessageService } from '@theia/core/lib/common';
+import { OutputChannelManager } from '@theia/output/lib/common/output-channel';
+import { OutputContribution } from '@theia/output/lib/browser/output-contribution';
 import { SampleDynamicLabelProviderContribution } from './sample-dynamic-label-provider-contribution';
 
 export namespace ExampleLabelProviderCommands {
@@ -33,6 +36,37 @@ export class ApiSamplesContribution implements FrontendApplicationContribution, 
 
     @inject(SampleDynamicLabelProviderContribution)
     protected readonly labelProviderContribution: SampleDynamicLabelProviderContribution;
+
+    @inject(MessageService)
+    protected messageService: MessageService;
+
+    @inject(OutputChannelManager)
+    protected readonly outputManager: OutputChannelManager;
+
+    @inject(OutputContribution)
+    protected readonly outputViewContribution: OutputContribution;
+
+    protected timer: number | undefined;
+
+    @postConstruct()
+    init(): void {
+        this.messageService.info("API Sample Contribution is starting and will log a message to the 'Output' view 10 times per second. Stand by...");
+        this.outputViewContribution.openView({ activate: true }).then(() => {
+            const channel = this.outputManager.getChannel('API Sample Contribution');
+            this.outputManager.selectedChannel = channel;
+            this.timer = window.setInterval(() => {
+                this.outputViewContribution.openView({ activate: true }).then(() => {
+                    this.outputManager.getChannel('API Sample Contribution').appendLine('hey there!');
+                });
+            }, 100);
+        });
+    }
+
+    onStop(): void {
+        if (typeof this.timer === 'number') {
+            window.clearInterval(this.timer);
+        }
+    }
 
     initialize(): void { }
 
