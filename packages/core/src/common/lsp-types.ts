8cf1deca3db2ae587942725b3fb68f67a8eab32b
Paul Maréchal <paul.marechal@ericsson.com>
2020-02-04 11:37:36 -0500

    core: fix import in `lsp-types.ts`
    Import statement is refering to a package which is not part of core's
    dependencies.
    
    Signed-off-by: Paul Maréchal <paul.marechal@ericsson.com>

--- packages/core/src/common/lsp-types.ts
+++ packages/core/src/common/lsp-types.ts
@@ -14,7 +14,7 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { Range } from 'vscode-languageserver-protocol';
+import { Range } from 'vscode-languageserver-types';
 
 export interface TextDocumentContentChangeDelta {
     readonly range: Range;
