1fffc9e1496fc8f955d16935927de2ca9ca6b942
Akos Kitta <kittaakos@typefox.io>
2020-02-24 12:00:16 +0100

    GH-7200: Made `RecursivePartial` array aware.
    Closes #7200.
    
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- packages/core/src/common/types.ts
+++ packages/core/src/common/types.ts
@@ -20,7 +20,9 @@ export type Deferred<T> = {
     [P in keyof T]: Promise<T[P]>
 };
 export type RecursivePartial<T> = {
-    [P in keyof T]?: RecursivePartial<T[P]>;
+    [P in keyof T]?: T[P] extends Array<infer I>
+    ? Array<RecursivePartial<I>>
+    : RecursivePartial<T[P]>;
 };
 export type MaybeArray<T> = T | T[];
 export type MaybePromise<T> = T | Promise<T> | PromiseLike<T>;
