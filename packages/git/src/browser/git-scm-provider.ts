84f1e4a0ee4147614683799d5bc9d82b24d4f844
Federico Bozzini <federico.bozzini@gmail.com>
2020-02-25 11:33:16 +0000

    [git] Fixed duplicated entries for git history on merge operations
    Signed-off-by: Federico Bozzini <federico.bozzini@gmail.com>

--- packages/git/src/browser/git-scm-provider.ts
+++ packages/git/src/browser/git-scm-provider.ts
@@ -383,7 +383,6 @@ export class GitAmendSupport implements ScmAmendSupport {
             this.repository,
             {
                 range: { toRevision: amendingHeadCommitSha, fromRevision: latestCommitSha },
-                firstParent: true,
                 maxCount: 50
             }
         );
