3eafd729846389eec3eb5577308bb7df2c9957c2
Vincent Fugnitto <vincent.fugnitto@ericsson.com>
2020-03-10 15:00:18 -0400

    markers: add sorting of problem markers
    Fixes #6572
    
    The following commit updates the `markers` to be sorted based on the following rules:
    - markers are sorted by `severity`.
    - if the first step is equal, markers are sorted by `line number`.
    - if the second step is equal, markers are sorted by `column number`.
    - else return no change.
    
    Signed-off-by: Vincent Fugnitto <vincent.fugnitto@ericsson.com>

---
+++ packages/markers/src/browser/problem/problem-utils.ts
@@ -0,0 +1,48 @@
+/********************************************************************************
+ * Copyright (C) 2020 Ericsson and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import { Marker } from '../../common/marker';
+import { Diagnostic } from '@theia/languages/lib/browser';
+
+export namespace ProblemUtils {
+
+    /**
+     * Comparator for severity.
+     * - The highest severity (`error`) come first followed by others.
+     * - `undefined` severities are treated as the last ones.
+     * @param a the first marker for comparison.
+     * @param b the second marker for comparison.
+     */
+    export const severityCompare = (a: Marker<Diagnostic>, b: Marker<Diagnostic>): number =>
+        (a.data.severity || Number.MAX_SAFE_INTEGER) - (b.data.severity || Number.MAX_SAFE_INTEGER);
+
+    /**
+     * Comparator for line numbers.
+     * - The lowest line number comes first.
+     * @param a the first marker for comparison.
+     * @param b the second marker for comparison.
+     */
+    export const lineNumberCompare = (a: Marker<Diagnostic>, b: Marker<Diagnostic>): number => a.data.range.start.line - b.data.range.start.line;
+
+    /**
+     * Comparator for column numbers.
+     * - The lowest column number comes first.
+     * @param a the first marker for comparison.
+     * @param b the second marker for comparison.
+     */
+    export const columnNumberCompare = (a: Marker<Diagnostic>, b: Marker<Diagnostic>): number => a.data.range.start.character - b.data.range.start.character;
+
+}
