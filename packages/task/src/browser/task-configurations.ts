b6656e9fc37acfe2263f3e2aa77e34df0796229d
Anton Kosyakov <anton.kosyakov@typefox.io>
2020-04-27 14:53:52 +0000

    [task] defer and cache validate tasks function
    Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

--- packages/task/src/browser/task-configurations.ts
+++ packages/task/src/browser/task-configurations.ts
@@ -14,7 +14,6 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import * as Ajv from 'ajv';
 import { inject, injectable, postConstruct } from 'inversify';
 import {
     ContributedTaskConfiguration,
@@ -436,10 +435,7 @@ export class TaskConfigurations implements Disposable {
      * or contributed by Theia extensions and plugins, `false` otherwise.
      */
     private isTaskConfigValid(task: TaskCustomization): boolean {
-        const schema = this.taskSchemaUpdater.getTaskSchema();
-        const ajv = new Ajv();
-        const validateSchema = ajv.compile(schema);
-        return !!validateSchema({ tasks: [task] });
+        return this.taskSchemaUpdater.validate({ tasks: [task] });
     }
 
     /**
