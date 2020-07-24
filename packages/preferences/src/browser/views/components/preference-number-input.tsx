fc0a5fa7326797017abc3d72be747d88346f525c
Anas Shahid <muhammad.shahid@ericsson.com>
2020-07-24 12:39:17 -0400

    [preferences]: Input field validation
    Fixes: https://github.com/eclipse-theia/theia/issues/7741
    
    Added an input field validation in the preference widget.
    Updated the input field type to be `number` instead of `text` for
    numerical fields.
    
    Signed-off-by: Anas Shahid <muhammad.shahid@ericsson.com>

--- packages/preferences/src/browser/views/components/preference-number-input.tsx
+++ packages/preferences/src/browser/views/components/preference-number-input.tsx
@@ -31,27 +31,63 @@ export const PreferenceNumberInput: React.FC<PreferenceNumberInputProps> = ({ pr
 
     const [currentTimeout, setCurrentTimetout] = React.useState<number>(0);
     const [currentValue, setCurrentValue] = React.useState<string>(externalValue);
+    const [validationMessage, setValidationMessage] = React.useState<string>('');
 
     React.useEffect(() => {
         setCurrentValue(externalValue);
     }, [externalValue]);
 
+    const onBlur = React.useCallback(() => {
+        setCurrentValue(externalValue);
+        setValidationMessage('');
+    }, [externalValue]);
+
     const onChange = React.useCallback(e => {
-        const { value: newValue } = e.target;
         clearTimeout(currentTimeout);
-        const newTimeout = setTimeout(() => setPreference(id, Number(newValue)), 750);
-        setCurrentTimetout(Number(newTimeout));
+        const { value: newValue } = e.target;
         setCurrentValue(newValue);
+        const preferenceValue: number = Number(newValue);
+        const { isValid, message } = getInputValidation(preferenceValue);
+        setValidationMessage(message);
+        if (isValid) {
+            const newTimeout = setTimeout(() => setPreference(id, preferenceValue), 750);
+            setCurrentTimetout(Number(newTimeout));
+        }
     }, [currentTimeout]);
 
+    /**
+     * Validates the input.
+     * @param input the input value.
+     */
+    const getInputValidation = (input: number | undefined): { isValid: boolean, message: string } => {
+        const errorMessages: string[] = [];
+        if (!input) {
+            return { isValid: false, message: 'Value must be a number.' };
+        };
+        if (data.minimum && input < data.minimum) {
+            errorMessages.push(`Value must be greater than or equal to ${data.minimum}.`);
+        };
+        if (data.maximum && input > data.maximum) {
+            errorMessages.push(`Value must be less than or equal to ${data.maximum}.`);
+        };
+        if (data.type === 'integer' && input % 1 !== 0) {
+            errorMessages.push('Value must be an integer.');
+        }
+        return { isValid: !errorMessages.length, message: errorMessages.join(' ') };
+    };
+
     return (
-        <input
-            type="text"
-            className="theia-input"
-            pattern="[0-9]*"
-            value={currentValue}
-            onChange={onChange}
-            data-preference-id={id}
-        />
+        <div className='pref-input-container'>
+            <input
+                type="number"
+                className="theia-input"
+                pattern="[0-9]*"
+                value={currentValue}
+                onChange={onChange}
+                onBlur={onBlur}
+                data-preference-id={id}
+            />
+            {!!validationMessage.length ? <div className='pref-error-notification'>{validationMessage}</div> : undefined}
+        </div>
     );
 };
