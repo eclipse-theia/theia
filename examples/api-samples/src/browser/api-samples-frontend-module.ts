9187fac9490d432a006e2eaf98192dbbebd3f199
Akos Kitta <kittaakos@typefox.io>
2020-09-04 17:12:18 +0200

    Added example.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- examples/api-samples/src/browser/api-samples-frontend-module.ts
+++ examples/api-samples/src/browser/api-samples-frontend-module.ts
@@ -19,10 +19,38 @@ import { bindDynamicLabelProvider } from './label/sample-dynamic-label-provider-
 import { bindSampleUnclosableView } from './view/sample-unclosable-view-contribution';
 import { bindSampleOutputChannelWithSeverity } from './output/sample-output-channel-with-severity';
 import { bindSampleMenu } from './menu/sample-menu-contribution';
+import { CommandRegistry, CommandContribution } from '@theia/core/lib/common/command';
 
 export default new ContainerModule(bind => {
     bindDynamicLabelProvider(bind);
     bindSampleUnclosableView(bind);
     bindSampleOutputChannelWithSeverity(bind);
     bindSampleMenu(bind);
+    let timer: NodeJS.Timer | undefined = undefined;
+    let counter = 0;
+    bind(CommandContribution).toConstantValue({
+        registerCommands(commands: CommandRegistry): void {
+            commands.registerCommand({ id: 'start--auto-restart-go', label: 'Start: Auto Restart Go LS' }, {
+                execute: () => {
+                    timer = setInterval(() => {
+                        commands.executeCommand('go.languageserver.restart');
+                        if (++counter % 10 === 0) {
+                            console.log(`Restarted the Go LS ${counter} times.`);
+                        }
+                    }, 1000);
+                },
+                isEnabled: () => !timer
+            });
+            commands.registerCommand({ id: 'stop--auto-restart-go', label: 'Stop: Auto Restart Go LS' }, {
+                execute: () => {
+                    if (timer) {
+                        clearInterval(timer);
+                        timer = undefined;
+                        counter = 0;
+                    }
+                },
+                isEnabled: () => !!timer
+            });
+        }
+    });
 });
