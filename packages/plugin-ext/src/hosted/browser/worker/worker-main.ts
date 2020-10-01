57b4b671f621dc1c5c6eec7f8c6ab1fe0585e4a5
Paul Maréchal <paul.marechal@ericsson.com>
2020-10-01 15:53:03 -0400

    plugin-ext: process plugins in concurrent loop
    Using `await` statements in for loops is generally not recommended,
    unless the data processing requires it to happen this way.
    
    Switched to a concurrent processing approach.
    
    Signed-off-by: Paul Maréchal <paul.marechal@ericsson.com>

--- packages/plugin-ext/src/hosted/browser/worker/worker-main.ts
+++ packages/plugin-ext/src/hosted/browser/worker/worker-main.ts
@@ -19,7 +19,7 @@ import { RPCProtocolImpl } from '../../../common/rpc-protocol';
 import { PluginManagerExtImpl } from '../../../plugin/plugin-manager';
 import { MAIN_RPC_CONTEXT, Plugin, emptyPlugin } from '../../../common/plugin-api-rpc';
 import { createAPIFactory } from '../../../plugin/plugin-context';
-import { getPluginId, PluginMetadata, PluginPackage } from '../../../common/plugin-protocol';
+import { getPluginId, PluginMetadata } from '../../../common/plugin-protocol';
 import * as theia from '@theia/plugin';
 import { PreferenceRegistryExtImpl } from '../../../plugin/preference-registry';
 import { ExtPluginApi } from '../../../common/plugin-ext-api-contribution';
@@ -85,7 +85,12 @@ const pluginManager = new PluginManagerExtImpl({
     async init(rawPluginData: PluginMetadata[]): Promise<[Plugin[], Plugin[]]> {
         const result: Plugin[] = [];
         const foreign: Plugin[] = [];
-        for (const plg of rawPluginData) {
+        // Process the plugins concurrently, making sure to keep the order.
+        const plugins = await Promise.all<{
+            /** Where to push the plugin: `result` or `foreign` */
+            target: Plugin[],
+            plugin: Plugin
+        }>(rawPluginData.map(async plg => {
             const pluginModel = plg.model;
             const pluginLifecycle = plg.lifecycle;
             if (pluginModel.entryPoint!.frontend) {
@@ -96,7 +101,6 @@ const pluginManager = new PluginManagerExtImpl({
                     frontendInitPath = '';
                 }
                 const rawModel = await loadManifest(pluginModel);
-
                 const plugin: Plugin = {
                     pluginPath: pluginModel.entryPoint.frontend!,
                     pluginFolder: pluginModel.packagePath,
@@ -104,23 +108,29 @@ const pluginManager = new PluginManagerExtImpl({
                     lifecycle: pluginLifecycle,
                     rawModel
                 };
-                result.push(plugin);
                 const apiImpl = apiFactory(plugin);
                 pluginsApiImpl.set(plugin.model.id, apiImpl);
                 pluginsModulesNames.set(plugin.lifecycle.frontendModuleName!, plugin);
+                return { target: result, plugin };
             } else {
-                foreign.push({
-                    pluginPath: pluginModel.entryPoint.backend,
-                    pluginFolder: pluginModel.packagePath,
-                    model: pluginModel,
-                    lifecycle: pluginLifecycle,
-                    get rawModel(): PluginPackage {
-                        throw new Error('not supported');
+                return {
+                    target: foreign,
+                    plugin: {
+                        pluginPath: pluginModel.entryPoint.backend,
+                        pluginFolder: pluginModel.packagePath,
+                        model: pluginModel,
+                        lifecycle: pluginLifecycle,
+                        get rawModel(): never {
+                            throw new Error('not supported');
+                        }
                     }
-                });
+                };
             }
+        }));
+        // Collect the ordered plugins and insert them in the target array:
+        for (const { target, plugin } of plugins) {
+            target.push(plugin);
         }
-
         return [result, foreign];
     },
     initExtApi(extApi: ExtPluginApi[]): void {
