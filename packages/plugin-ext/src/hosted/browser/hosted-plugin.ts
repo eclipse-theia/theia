56c22749c3b11cab0c8d20328305c703b0246bba
Vitaliy Gulyy <vgulyy@redhat.com>
2020-11-13 13:55:12 +0200

    Do not store webview if there is no corresponding serializer
    Signed-off-by: Vitaliy Gulyy <vgulyy@redhat.com>

--- packages/plugin-ext/src/hosted/browser/hosted-plugin.ts
+++ packages/plugin-ext/src/hosted/browser/hosted-plugin.ts
@@ -197,19 +197,22 @@ export class HostedPluginSupport {
         this.taskProviderRegistry.onWillProvideTaskProvider(event => this.ensureTaskActivation(event));
         this.taskResolverRegistry.onWillProvideTaskResolver(event => this.ensureTaskActivation(event));
         this.fileService.onWillActivateFileSystemProvider(event => this.ensureFileSystemActivation(event));
+
         this.widgets.onDidCreateWidget(({ factoryId, widget }) => {
             if (factoryId === WebviewWidget.FACTORY_ID && widget instanceof WebviewWidget) {
                 const storeState = widget.storeState.bind(widget);
                 const restoreState = widget.restoreState.bind(widget);
+
                 widget.storeState = () => {
                     if (this.webviewRevivers.has(widget.viewType)) {
                         return storeState();
                     }
-                    return {};
+                    return undefined;
                 };
-                widget.restoreState = oldState => {
-                    if (oldState.viewType) {
-                        restoreState(oldState);
+
+                widget.restoreState = state => {
+                    if (state.viewType) {
+                        restoreState(state);
                         this.preserveWebview(widget);
                     } else {
                         widget.dispose();
