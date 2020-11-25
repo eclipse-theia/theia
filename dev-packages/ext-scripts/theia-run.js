0a721961ac7e8599284afc51e69c7e067f50d74f
Akos Kitta <kittaakos@typefox.io>
2020-11-25 16:06:54 +0100

    Workaround for eclipse-theia/theia#8779.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- dev-packages/ext-scripts/theia-run.js
+++ dev-packages/ext-scripts/theia-run.js
@@ -18,7 +18,16 @@
 // @ts-check
 const path = require('path');
 
-const index = process.argv.findIndex(arg => arg.indexOf('run') !== -1);
+// See: https://github.com/eclipse-theia/theia/issues/8779#issuecomment-733747340
+const filter = require('os').platform() === 'win32'
+    ? arg => arg.indexOf(path.join('ext-scripts', 'theia-run.js')) !== -1
+    : arg => arg.indexOf(path.join('.bin', 'run')) !== -1
+let index = process.argv.findIndex(filter);
+if (index === -1) {
+    // Fall back to the original logic.
+    // https://github.com/eclipse-theia/theia/blob/6ef08676314a2ceca93023ddd149579493ae7914/dev-packages/ext-scripts/theia-run.js#L21
+    index = process.argv.findIndex(arg => arg.indexOf('run') !== -1);
+}
 const args = process.argv.slice(index + 1);
 const scopedArgs = args.length > 1 ? [args[0], '--scope', ...args.slice(1)] : args;
 process.argv = [...process.argv.slice(0, index + 1), 'run', ...scopedArgs];
