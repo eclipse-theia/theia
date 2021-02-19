eclipse-theia/theia

commit f840f60fb2f44aad4aa7c69627e499f80057d056

Fix for DnD files from navigator to main area
Signed-off-by: Balaji V <kuttibalaji.v6@gmail.com>

packages/navigator/src/browser/navigator-widget.tsx
@@ -18,7 +18,7 @@ import { injectable, inject, postConstruct } from 'inversify';
 import { Message } from '@phosphor/messaging';
 import URI from '@theia/core/lib/common/uri';
 import { CommandService, SelectionService } from '@theia/core/lib/common';
-import { CorePreferences, Key, TreeModel } from '@theia/core/lib/browser';
+import { CorePreferences, Key, TreeModel, SelectableTreeNode } from '@theia/core/lib/browser';
 import {
     ContextMenuRenderer, ExpandableTreeNode,
     TreeProps, TreeNode
@@ -102,7 +102,12 @@ export class FileNavigatorWidget extends FileTreeWidget {
         const mainPanelNode = this.shell.mainPanel.node;
         this.addEventListener(mainPanelNode, 'drop', async ({ dataTransfer }) => {
             const treeNodes = dataTransfer && this.getSelectedTreeNodesFromData(dataTransfer) || [];
-            treeNodes.filter(FileNode.is).forEach(treeNode => this.commandService.executeCommand(FileNavigatorCommands.OPEN.id, treeNode.uri));
+            treeNodes.filter(FileNode.is).forEach(treeNode => {
+                if (!SelectableTreeNode.isSelected(treeNode)) {
+                    this.model.toggleNode(treeNode);
+                }
+            });
+            this.commandService.executeCommand(FileNavigatorCommands.OPEN.id);
         });
         const handler = (e: DragEvent) => {
             if (e.dataTransfer) {

commit ba6e5b52f576f2e3517948483ca6bdaee6c226f5

Only execute file open command during drag-and-drop for dragged files
Fix for DnD files to main area #8739

Solves issue introduced in f840f60fb2f44aad4aa7c69627e499f80057d056

Contributed by STMicroelectronics
Signed-off-by: Samuel HULTGREN <samuel.hultgren@st.com>

packages/navigator/src/browser/navigator-widget.tsx
@@ -107,7 +107,9 @@ export class FileNavigatorWidget extends FileTreeWidget {
                     this.model.toggleNode(treeNode);
                 }
             });
-            this.commandService.executeCommand(FileNavigatorCommands.OPEN.id);
+            if (treeNodes.length > 0) {
+                this.commandService.executeCommand(FileNavigatorCommands.OPEN.id);
+            }
         });
         const handler = (e: DragEvent) => {
             if (e.dataTransfer) {