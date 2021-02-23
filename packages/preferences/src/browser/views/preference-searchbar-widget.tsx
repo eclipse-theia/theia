42129906fb13cca6f0af66c2ffa9f50a9d77aa5a
seantan22 <sean.a.tan@ericsson.com>
2021-02-23 11:06:40 -0500

    Preferences: Improve Search Bar
    Fixes [#7743](https://github.com/eclipse-theia/theia/issues/7743)
    
    What it does
    
    - Allows user to clear all when searching preferences/settings.
    - Displays the total result count as search is being typed.
    
    How to test
    
    1. Open preferences.
    2. Type in preference search box, and observe that the results count updates.
    3. Click on clear all button to erase current search.
    
    Signed-off-by: seantan22 <sean.a.tan@ericsson.com>

--- packages/preferences/src/browser/views/preference-searchbar-widget.tsx
+++ packages/preferences/src/browser/views/preference-searchbar-widget.tsx
@@ -24,11 +24,13 @@ import { Disposable, Emitter } from '@theia/core';
 export class PreferencesSearchbarWidget extends ReactWidget {
     static readonly ID = 'settings.header';
     static readonly LABEL = 'Settings Header';
+    static readonly SEARCHBAR_ID = 'preference-searchbar';
 
     protected readonly onFilterStringChangedEmitter = new Emitter<string>();
     readonly onFilterChanged = this.onFilterStringChangedEmitter.event;
 
     protected searchbarRef: React.RefObject<HTMLInputElement> = React.createRef<HTMLInputElement>();
+    protected resultsCount: number = 0;
 
     @postConstruct()
     protected init(): void {
@@ -53,20 +55,87 @@ export class PreferencesSearchbarWidget extends ReactWidget {
         }
     }
 
+    /**
+     * Clears the search input and all search results.
+     * @param e on-click mouse event.
+     */
+    protected clearSearchResults = (e: React.MouseEvent): void => {
+        const search = document.getElementById(PreferencesSearchbarWidget.SEARCHBAR_ID) as HTMLInputElement;
+        if (search) {
+            search.value = '';
+            this.search(search.value);
+            this.update();
+        }
+    };
+
+    /**
+     * Renders all search bar options.
+     */
+    protected renderOptionContainer(): React.ReactNode {
+        const resultsCount = this.renderResultsCountOption();
+        const clearAllOption = this.renderClearAllOption();
+        return <div className="option-buttons"> {resultsCount} {clearAllOption} </div>;
+    }
+
+    /**
+     * Renders a badge displaying search results count.
+     */
+    protected renderResultsCountOption(): React.ReactNode {
+        const resultsFound = `${this.resultsCount === 0 ? 'No' : this.resultsCount} ${this.resultsCount === 1 ? 'Setting Found' : 'Settings Found'}`;
+        return this.searchTermExists() ?
+            (<span
+                className="results-found"
+                title={resultsFound}>
+                {resultsFound}
+            </span>)
+            : '';
+    }
+
+    /**
+     * Renders a clear all button.
+     */
+    protected renderClearAllOption(): React.ReactNode {
+        return <span
+            className={`clear-all option ${(this.searchTermExists() ? 'enabled' : '')}`}
+            title="Clear Search Results"
+            onClick={this.clearSearchResults}
+        />;
+    }
+
+    /**
+     * Determines whether the search input currently has a value.
+     * @returns true, if the search input currently has a value; false, otherwise.
+     */
+    protected searchTermExists(): boolean {
+        return !!this.searchbarRef.current?.value;
+    }
+
     render(): React.ReactNode {
+        const optionContainer = this.renderOptionContainer();
         return (
             <div className='settings-header'>
                 <div className="settings-search-container">
                     <input
                         type="text"
+                        id={PreferencesSearchbarWidget.SEARCHBAR_ID}
                         spellCheck={false}
                         placeholder="Search Settings"
                         className="settings-search-input theia-input"
                         onChange={this.handleSearch}
                         ref={this.searchbarRef}
                     />
+                    {optionContainer}
                 </div>
             </div >
         );
+    }
+
+    /**
+     * Updates the search result count.
+     * @param count the result count.
+     */
+    updateResultsCount(count: number): void {
+        this.resultsCount = count;
+        this.update();
     }
 }
