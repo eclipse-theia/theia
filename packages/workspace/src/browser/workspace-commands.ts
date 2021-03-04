hataejun/eclipse-theia

6636-custom-editor: Adds a prototype of custom editors contributed by extensions with this functionality:
- Adds a new contribution point for custom editors.
- Adds API for registering a custom editor providers.
- Implements CustomEditor extension API - based on VSCode (excluding backup functionality not implemented in this PR).
- Adds CustomEditorWidget extending WebviewWidget containing a model reference to CustomEditorModel.
- Supports two CustomEditorModel implementations: CustomTextEditorModel for text documents and MainCustomEditorModel for binary documents.
- Registers openHandlers for CustomEditors.
- Adds `openWith` command for selecting which editor to use when openning a resource.

Signed-off-by: Dan Arad <dan.arad@sap.com>

packages/workspace/src/browser/workspace-commands.ts
@@ -212,16 +212,7 @@ export class WorkspaceCommandContribution implements CommandContribution {
     }
 
     registerCommands(registry: CommandRegistry): void {
-        this.openerService.getOpeners().then(openers => {
-            for (const opener of openers) {
-                const openWithCommand = WorkspaceCommands.FILE_OPEN_WITH(opener);
-                registry.registerCommand(openWithCommand, this.newUriAwareCommandHandler({
-                    execute: uri => opener.open(uri),
-                    isEnabled: uri => opener.canHandle(uri) > 0,
-                    isVisible: uri => opener.canHandle(uri) > 0 && this.areMultipleOpenHandlersPresent(openers, uri)
-                }));
-            }
-        });
+        this.registerOpenWith(registry);
         registry.registerCommand(WorkspaceCommands.NEW_FILE, this.newWorkspaceRootUriAwareCommandHandler({
             execute: uri => this.getDirectory(uri).then(parent => {
                 if (parent) {
@@ -340,6 +331,24 @@ export class WorkspaceCommandContribution implements CommandContribution {
                 isVisible: uris => this.areWorkspaceRoots(uris) && this.workspaceService.saved
             }));
         });
+    }
+
+    openers: OpenHandler[];
+    protected async registerOpenWith(registry: CommandRegistry): Promise<void> {
+        if (this.openerService.onOpenersStateChanged) {
+            this.openerService.onOpenersStateChanged(async e => {
+                this.openers = await this.openerService.getOpeners();
+            });
+        }
+        const openers = await this.openerService.getOpeners();
+        for (const opener of openers) {
+            const openWithCommand = WorkspaceCommands.FILE_OPEN_WITH(opener);
+            registry.registerCommand(openWithCommand, this.newUriAwareCommandHandler({
+                execute: uri => opener.open(uri),
+                isEnabled: uri => opener.canHandle(uri) > 0,
+                isVisible: uri => opener.canHandle(uri) > 0 && this.areMultipleOpenHandlersPresent(this.openers, uri)
+            }));
+        }
     }
 
     protected newUriAwareCommandHandler(handler: UriCommandHandler<URI>): UriAwareCommandHandler<URI> {

eclipse-theia/theia

packages/workspace/src/browser/workspace-commands.ts
@@ -212,16 +212,7 @@ export class WorkspaceCommandContribution implements CommandContribution {
     }
 
     registerCommands(registry: CommandRegistry): void {
-        this.openerService.getOpeners().then(openers => {
-            for (const opener of openers) {
-                const openWithCommand = WorkspaceCommands.FILE_OPEN_WITH(opener);
-                registry.registerCommand(openWithCommand, this.newUriAwareCommandHandler({
-                    execute: uri => opener.open(uri),
-                    isEnabled: uri => opener.canHandle(uri) > 0,
-                    isVisible: uri => opener.canHandle(uri) > 0 && this.areMultipleOpenHandlersPresent(openers, uri)
-                }));
-            }
-        });
+        this.registerOpenWith(registry);
         registry.registerCommand(WorkspaceCommands.NEW_FILE, this.newWorkspaceRootUriAwareCommandHandler({
             execute: uri => this.getDirectory(uri).then(parent => {
                 if (parent) {
@@ -340,6 +331,24 @@ export class WorkspaceCommandContribution implements CommandContribution {
                 isVisible: uris => this.areWorkspaceRoots(uris) && this.workspaceService.saved
             }));
         });
+    }
+
+    openers: OpenHandler[];
+    protected async registerOpenWith(registry: CommandRegistry): Promise<void> {
+        if (this.openerService.onDidChangeOpeners) {
+            this.openerService.onDidChangeOpeners(async e => {
+                this.openers = await this.openerService.getOpeners();
+            });
+        }
+        const openers = await this.openerService.getOpeners();
+        for (const opener of openers) {
+            const openWithCommand = WorkspaceCommands.FILE_OPEN_WITH(opener);
+            registry.registerCommand(openWithCommand, this.newUriAwareCommandHandler({
+                execute: uri => opener.open(uri),
+                isEnabled: uri => opener.canHandle(uri) > 0,
+                isVisible: uri => opener.canHandle(uri) > 0 && this.areMultipleOpenHandlersPresent(this.openers, uri)
+            }));
+        }
     }
 
     protected newUriAwareCommandHandler(handler: UriCommandHandler<URI>): UriAwareCommandHandler<URI> {