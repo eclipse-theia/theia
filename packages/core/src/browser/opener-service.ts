hataejun/eclipse-theia

6636-custom-editor: Adds a prototype of custom editors contributed by extensions with this functionality:
- Adds a new contribution point for custom editors.
- Adds API for registering a custom editor providers.
- Implements CustomEditor extension API - based on VSCode (excluding backup functionality not implemented in this PR).
- Adds CustomEditorWidget extending WebviewWidget containing a model reference to CustomEditorModel.
- Supports two CustomEditorModel implementations: CustomTextEditorModel for text documents and MainCustomEditorModel for binary documents.
- Registers openHandlers for CustomEditors.
- Adds `openWith` command for selecting which editor to use when openning a resource.

Signed-off-by: Dan Arad <dan.arad@sap.com>

packages/core/src/browser/opener-service.ts
@@ -16,7 +16,7 @@
 
 import { named, injectable, inject } from 'inversify';
 import URI from '../common/uri';
-import { ContributionProvider, Prioritizeable, MaybePromise } from '../common';
+import { ContributionProvider, Prioritizeable, MaybePromise, Emitter, Event, Disposable } from '../common';
 
 export interface OpenerOptions {
 }
@@ -75,6 +75,14 @@ export interface OpenerService {
      * Reject if such does not exist.
      */
     getOpener(uri: URI, options?: OpenerOptions): Promise<OpenHandler>;
+    /**
+     * Add open handler i.e. for custom editors
+     */
+    addHandler(openHandler: OpenHandler): Disposable;
+    /**
+     * Event that fires when a new opener is added or removed.
+     */
+    onOpenersStateChanged?: Event<void>;
 }
 
 export async function open(openerService: OpenerService, uri: URI, options?: OpenerOptions): Promise<object | undefined> {
@@ -84,11 +92,26 @@ export async function open(openerService: OpenerService, uri: URI, options?: Ope
 
 @injectable()
 export class DefaultOpenerService implements OpenerService {
+    // Collection of open-handlers for custom-editor contributions.
+    protected readonly customEditorOpenHandlers: OpenHandler[] = [];
+
+    protected readonly onOpenersStateChangedEmitter = new Emitter<void>();
+    readonly onOpenersStateChanged = this.onOpenersStateChangedEmitter.event;
 
     constructor(
         @inject(ContributionProvider) @named(OpenHandler)
         protected readonly handlersProvider: ContributionProvider<OpenHandler>
     ) { }
+
+    addHandler(openHandler: OpenHandler): Disposable {
+        this.customEditorOpenHandlers.push(openHandler);
+        this.onOpenersStateChangedEmitter.fire();
+
+        return Disposable.create(() => {
+            this.customEditorOpenHandlers.splice(this.customEditorOpenHandlers.indexOf(openHandler), 1);
+            this.onOpenersStateChangedEmitter.fire();
+        });
+    }
 
     async getOpener(uri: URI, options?: OpenerOptions): Promise<OpenHandler> {
         const handlers = await this.prioritize(uri, options);
@@ -114,7 +137,10 @@ export class DefaultOpenerService implements OpenerService {
     }
 
     protected getHandlers(): OpenHandler[] {
-        return this.handlersProvider.getContributions();
+        return [
+            ...this.handlersProvider.getContributions(),
+            ...this.customEditorOpenHandlers
+        ];
     }
 
 }

packages/plugin-ext/src/hosted/browser/hosted-plugin.ts
@@ -62,6 +62,8 @@ import { FrontendApplicationConfigProvider } from '@theia/core/lib/browser/front
 import { environment } from '@theia/application-package/lib/environment';
 import { JsonSchemaStore } from '@theia/core/lib/browser/json-schema-store';
 import { FileService, FileSystemProviderActivationEvent } from '@theia/filesystem/lib/browser/file-service';
+import { PluginCustomEditorRegistry } from '../../main/browser/custom-editors/plugin-custom-editor-registry';
+import { CustomEditorWidget } from '../../main/browser/custom-editors/custom-editor-widget';
 
 export type PluginHost = 'frontend' | string;
 export type DebugActivationEvent = 'onDebugResolve' | 'onDebugInitialConfigurations' | 'onDebugAdapterProtocolTracker';
@@ -151,6 +153,9 @@ export class HostedPluginSupport {
     @inject(JsonSchemaStore)
     protected readonly jsonSchemaStore: JsonSchemaStore;
 
+    @inject(PluginCustomEditorRegistry)
+    protected readonly customEditorRegistry: PluginCustomEditorRegistry;
+
     private theiaReadyPromise: Promise<any>;
 
     protected readonly managers = new Map<string, PluginManagerExt>();
@@ -197,9 +202,10 @@ export class HostedPluginSupport {
         this.taskProviderRegistry.onWillProvideTaskProvider(event => this.ensureTaskActivation(event));
         this.taskResolverRegistry.onWillProvideTaskResolver(event => this.ensureTaskActivation(event));
         this.fileService.onWillActivateFileSystemProvider(event => this.ensureFileSystemActivation(event));
+        this.customEditorRegistry.onPendingOpenCustomEditor(event => this.activateByCustomEditor(event));
 
         this.widgets.onDidCreateWidget(({ factoryId, widget }) => {
-            if (factoryId === WebviewWidget.FACTORY_ID && widget instanceof WebviewWidget) {
+            if ((factoryId === WebviewWidget.FACTORY_ID || factoryId === CustomEditorWidget.FACTORY_ID) && widget instanceof WebviewWidget) {
                 const storeState = widget.storeState.bind(widget);
                 const restoreState = widget.restoreState.bind(widget);
 
@@ -556,6 +562,10 @@ export class HostedPluginSupport {
         await this.activateByEvent(`onCommand:${commandId}`);
     }
 
+    async activateByCustomEditor(viewType: string): Promise<void> {
+        await this.activateByEvent(`onCustomEditor:${viewType}`);
+    }
+
     activateByFileSystem(event: FileSystemProviderActivationEvent): Promise<void> {
         return this.activateByEvent(`onFileSystem:${event.scheme}`);
     }
@@ -713,9 +723,16 @@ export class HostedPluginSupport {
         this.webviewRevivers.delete(viewType);
     }
 
-    protected preserveWebviews(): void {
+    protected async preserveWebviews(): Promise<void> {
         for (const webview of this.widgets.getWidgets(WebviewWidget.FACTORY_ID)) {
             this.preserveWebview(webview as WebviewWidget);
+        }
+        for (const webview of this.widgets.getWidgets(CustomEditorWidget.FACTORY_ID)) {
+            (webview as CustomEditorWidget).modelRef.dispose();
+            if ((webview as any)['closeWithoutSaving']) {
+                delete (webview as any)['closeWithoutSaving'];
+            }
+            this.customEditorRegistry.resolveWidget(webview as CustomEditorWidget);
         }
     }
 

packages/plugin-ext/src/main/browser/custom-editors/custom-editor-opener.tsx
@@ -1,0 +1,94 @@
+/********************************************************************************
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import { inject } from 'inversify';
+import { PreviewEditorOpenerOptions } from '@theia/editor-preview/lib/browser';
+import URI from '@theia/core/lib/common/uri';
+import { ApplicationShell, OpenerOptions, OpenHandler, Widget, WidgetManager } from '@theia/core/lib/browser';
+import { CustomEditorPriority, CustomEditorSelector } from '../../../common';
+import * as glob from './glob';
+import { CustomEditor } from '../../../common';
+import { CustomEditorWidget } from './custom-editor-widget';
+import { v4 } from 'uuid';
+import { Emitter } from '@theia/core';
+
+export class CustomEditorOpener implements OpenHandler {
+
+    readonly id: string;
+    readonly label: string;
+
+    private readonly onOpenCustomEditorEmitter = new Emitter<CustomEditorWidget>();
+    readonly onOpenCustomEditor = this.onOpenCustomEditorEmitter.event;
+
+    constructor(
+        private readonly editor: CustomEditor,
+        @inject(ApplicationShell) protected readonly shell: ApplicationShell,
+        @inject(WidgetManager) protected readonly widgetManager: WidgetManager
+    ) {
+        this.id = `custom-editor-${this.editor.viewType}`;
+        this.label = this.editor.displayName;
+    }
+
+    canHandle(uri: URI, options?: PreviewEditorOpenerOptions): number {
+        if (this.matches(this.editor.selector, uri)) {
+            return this.getPriority();
+        }
+        return 0;
+    }
+
+    getPriority(): number {
+        switch (this.editor.priority) {
+            case CustomEditorPriority.default: return 500;
+            case CustomEditorPriority.builtin: return 400;
+            case CustomEditorPriority.option: return 300;
+            default: return 200;
+        }
+    }
+
+    async open(uri: URI, options?: OpenerOptions): Promise<Widget | undefined> {
+        let widget: CustomEditorWidget | undefined;
+        const widgets = this.widgetManager.getWidgets(CustomEditorWidget.FACTORY_ID) as CustomEditorWidget[];
+        widget = widgets.find(w => w.viewType === this.editor.viewType && w.resource.toString() === uri.toString());
+
+        if (widget?.isVisible) {
+            return this.shell.revealWidget(widget.id);
+        }
+        if (widget?.isAttached) {
+            return this.shell.activateWidget(widget.id);
+        }
+        if (!widget) {
+            const id = v4();
+            widget = await this.widgetManager.getOrCreateWidget<CustomEditorWidget>(CustomEditorWidget.FACTORY_ID, { id });
+            widget.viewType = this.editor.viewType;
+            widget.resource = uri;
+        }
+
+        this.onOpenCustomEditorEmitter.fire(widget);
+    }
+
+    matches(selectors: CustomEditorSelector[], resource: URI): boolean {
+        return selectors.some(selector => this.selectorMatches(selector, resource));
+    }
+
+    selectorMatches(selector: CustomEditorSelector, resource: URI): boolean {
+        if (selector.filenamePattern) {
+            if (glob.match(selector.filenamePattern.toLowerCase(), resource.path.name.toLowerCase() + resource.path.ext.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

packages/workspace/src/browser/workspace-commands.ts
@@ -212,16 +212,7 @@ export class WorkspaceCommandContribution implements CommandContribution {
     }
 
     registerCommands(registry: CommandRegistry): void {
-        this.openerService.getOpeners().then(openers => {
-            for (const opener of openers) {
-                const openWithCommand = WorkspaceCommands.FILE_OPEN_WITH(opener);
-                registry.registerCommand(openWithCommand, this.newUriAwareCommandHandler({
-                    execute: uri => opener.open(uri),
-                    isEnabled: uri => opener.canHandle(uri) > 0,
-                    isVisible: uri => opener.canHandle(uri) > 0 && this.areMultipleOpenHandlersPresent(openers, uri)
-                }));
-            }
-        });
+        this.registerOpenWith(registry);
         registry.registerCommand(WorkspaceCommands.NEW_FILE, this.newWorkspaceRootUriAwareCommandHandler({
             execute: uri => this.getDirectory(uri).then(parent => {
                 if (parent) {
@@ -340,6 +331,24 @@ export class WorkspaceCommandContribution implements CommandContribution {
                 isVisible: uris => this.areWorkspaceRoots(uris) && this.workspaceService.saved
             }));
         });
+    }
+
+    openers: OpenHandler[];
+    protected async registerOpenWith(registry: CommandRegistry): Promise<void> {
+        if (this.openerService.onOpenersStateChanged) {
+            this.openerService.onOpenersStateChanged(async e => {
+                this.openers = await this.openerService.getOpeners();
+            });
+        }
+        const openers = await this.openerService.getOpeners();
+        for (const opener of openers) {
+            const openWithCommand = WorkspaceCommands.FILE_OPEN_WITH(opener);
+            registry.registerCommand(openWithCommand, this.newUriAwareCommandHandler({
+                execute: uri => opener.open(uri),
+                isEnabled: uri => opener.canHandle(uri) > 0,
+                isVisible: uri => opener.canHandle(uri) > 0 && this.areMultipleOpenHandlersPresent(this.openers, uri)
+            }));
+        }
     }
 
     protected newUriAwareCommandHandler(handler: UriCommandHandler<URI>): UriAwareCommandHandler<URI> {

eclipse-theia/theia

packages/core/src/browser/opener-service.ts
@@ -16,7 +16,7 @@
 
 import { named, injectable, inject } from 'inversify';
 import URI from '../common/uri';
-import { ContributionProvider, Prioritizeable, MaybePromise } from '../common';
+import { ContributionProvider, Prioritizeable, MaybePromise, Emitter, Event, Disposable } from '../common';
 
 export interface OpenerOptions {
 }
@@ -75,6 +75,14 @@ export interface OpenerService {
      * Reject if such does not exist.
      */
     getOpener(uri: URI, options?: OpenerOptions): Promise<OpenHandler>;
+    /**
+     * Add open handler i.e. for custom editors
+     */
+    addHandler?(openHandler: OpenHandler): Disposable;
+    /**
+     * Event that fires when a new opener is added or removed.
+     */
+    onDidChangeOpeners?: Event<void>;
 }
 
 export async function open(openerService: OpenerService, uri: URI, options?: OpenerOptions): Promise<object | undefined> {
@@ -84,11 +92,26 @@ export async function open(openerService: OpenerService, uri: URI, options?: Ope
 
 @injectable()
 export class DefaultOpenerService implements OpenerService {
+    // Collection of open-handlers for custom-editor contributions.
+    protected readonly customEditorOpenHandlers: OpenHandler[] = [];
+
+    protected readonly onDidChangeOpenersEmitter = new Emitter<void>();
+    readonly onDidChangeOpeners = this.onDidChangeOpenersEmitter.event;
 
     constructor(
         @inject(ContributionProvider) @named(OpenHandler)
         protected readonly handlersProvider: ContributionProvider<OpenHandler>
     ) { }
+
+    addHandler(openHandler: OpenHandler): Disposable {
+        this.customEditorOpenHandlers.push(openHandler);
+        this.onDidChangeOpenersEmitter.fire();
+
+        return Disposable.create(() => {
+            this.customEditorOpenHandlers.splice(this.customEditorOpenHandlers.indexOf(openHandler), 1);
+            this.onDidChangeOpenersEmitter.fire();
+        });
+    }
 
     async getOpener(uri: URI, options?: OpenerOptions): Promise<OpenHandler> {
         const handlers = await this.prioritize(uri, options);
@@ -114,7 +137,10 @@ export class DefaultOpenerService implements OpenerService {
     }
 
     protected getHandlers(): OpenHandler[] {
-        return this.handlersProvider.getContributions();
+        return [
+            ...this.handlersProvider.getContributions(),
+            ...this.customEditorOpenHandlers
+        ];
     }
 
 }

packages/plugin-ext/src/hosted/browser/hosted-plugin.ts
@@ -62,6 +62,8 @@ import { FrontendApplicationConfigProvider } from '@theia/core/lib/browser/front
 import { environment } from '@theia/application-package/lib/environment';
 import { JsonSchemaStore } from '@theia/core/lib/browser/json-schema-store';
 import { FileService, FileSystemProviderActivationEvent } from '@theia/filesystem/lib/browser/file-service';
+import { PluginCustomEditorRegistry } from '../../main/browser/custom-editors/plugin-custom-editor-registry';
+import { CustomEditorWidget } from '../../main/browser/custom-editors/custom-editor-widget';
 
 export type PluginHost = 'frontend' | string;
 export type DebugActivationEvent = 'onDebugResolve' | 'onDebugInitialConfigurations' | 'onDebugAdapterProtocolTracker';
@@ -151,6 +153,9 @@ export class HostedPluginSupport {
     @inject(JsonSchemaStore)
     protected readonly jsonSchemaStore: JsonSchemaStore;
 
+    @inject(PluginCustomEditorRegistry)
+    protected readonly customEditorRegistry: PluginCustomEditorRegistry;
+
     private theiaReadyPromise: Promise<any>;
 
     protected readonly managers = new Map<string, PluginManagerExt>();
@@ -197,9 +202,10 @@ export class HostedPluginSupport {
         this.taskProviderRegistry.onWillProvideTaskProvider(event => this.ensureTaskActivation(event));
         this.taskResolverRegistry.onWillProvideTaskResolver(event => this.ensureTaskActivation(event));
         this.fileService.onWillActivateFileSystemProvider(event => this.ensureFileSystemActivation(event));
+        this.customEditorRegistry.onWillOpenCustomEditor(event => this.activateByCustomEditor(event));
 
         this.widgets.onDidCreateWidget(({ factoryId, widget }) => {
-            if (factoryId === WebviewWidget.FACTORY_ID && widget instanceof WebviewWidget) {
+            if ((factoryId === WebviewWidget.FACTORY_ID || factoryId === CustomEditorWidget.FACTORY_ID) && widget instanceof WebviewWidget) {
                 const storeState = widget.storeState.bind(widget);
                 const restoreState = widget.restoreState.bind(widget);
 
@@ -556,6 +562,10 @@ export class HostedPluginSupport {
         await this.activateByEvent(`onCommand:${commandId}`);
     }
 
+    async activateByCustomEditor(viewType: string): Promise<void> {
+        await this.activateByEvent(`onCustomEditor:${viewType}`);
+    }
+
     activateByFileSystem(event: FileSystemProviderActivationEvent): Promise<void> {
         return this.activateByEvent(`onFileSystem:${event.scheme}`);
     }
@@ -713,9 +723,16 @@ export class HostedPluginSupport {
         this.webviewRevivers.delete(viewType);
     }
 
-    protected preserveWebviews(): void {
+    protected async preserveWebviews(): Promise<void> {
         for (const webview of this.widgets.getWidgets(WebviewWidget.FACTORY_ID)) {
             this.preserveWebview(webview as WebviewWidget);
+        }
+        for (const webview of this.widgets.getWidgets(CustomEditorWidget.FACTORY_ID)) {
+            (webview as CustomEditorWidget).modelRef.dispose();
+            if ((webview as any)['closeWithoutSaving']) {
+                delete (webview as any)['closeWithoutSaving'];
+            }
+            this.customEditorRegistry.resolveWidget(webview as CustomEditorWidget);
         }
     }
 

packages/plugin-ext/src/main/browser/custom-editors/custom-editor-opener.tsx
@@ -1,0 +1,94 @@
+/********************************************************************************
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import { inject } from 'inversify';
+import { PreviewEditorOpenerOptions } from '@theia/editor-preview/lib/browser';
+import URI from '@theia/core/lib/common/uri';
+import { ApplicationShell, OpenerOptions, OpenHandler, Widget, WidgetManager } from '@theia/core/lib/browser';
+import { CustomEditorPriority, CustomEditorSelector } from '../../../common';
+import * as glob from './glob';
+import { CustomEditor } from '../../../common';
+import { CustomEditorWidget } from './custom-editor-widget';
+import { v4 } from 'uuid';
+import { Emitter } from '@theia/core';
+
+export class CustomEditorOpener implements OpenHandler {
+
+    readonly id: string;
+    readonly label: string;
+
+    private readonly onDidOpenCustomEditorEmitter = new Emitter<CustomEditorWidget>();
+    readonly onDidOpenCustomEditor = this.onDidOpenCustomEditorEmitter.event;
+
+    constructor(
+        private readonly editor: CustomEditor,
+        @inject(ApplicationShell) protected readonly shell: ApplicationShell,
+        @inject(WidgetManager) protected readonly widgetManager: WidgetManager
+    ) {
+        this.id = `custom-editor-${this.editor.viewType}`;
+        this.label = this.editor.displayName;
+    }
+
+    canHandle(uri: URI, options?: PreviewEditorOpenerOptions): number {
+        if (this.matches(this.editor.selector, uri)) {
+            return this.getPriority();
+        }
+        return 0;
+    }
+
+    getPriority(): number {
+        switch (this.editor.priority) {
+            case CustomEditorPriority.default: return 500;
+            case CustomEditorPriority.builtin: return 400;
+            case CustomEditorPriority.option: return 300;
+            default: return 200;
+        }
+    }
+
+    async open(uri: URI, options?: OpenerOptions): Promise<Widget | undefined> {
+        let widget: CustomEditorWidget | undefined;
+        const widgets = this.widgetManager.getWidgets(CustomEditorWidget.FACTORY_ID) as CustomEditorWidget[];
+        widget = widgets.find(w => w.viewType === this.editor.viewType && w.resource.toString() === uri.toString());
+
+        if (widget?.isVisible) {
+            return this.shell.revealWidget(widget.id);
+        }
+        if (widget?.isAttached) {
+            return this.shell.activateWidget(widget.id);
+        }
+        if (!widget) {
+            const id = v4();
+            widget = await this.widgetManager.getOrCreateWidget<CustomEditorWidget>(CustomEditorWidget.FACTORY_ID, { id });
+            widget.viewType = this.editor.viewType;
+            widget.resource = uri;
+        }
+
+        this.onDidOpenCustomEditorEmitter.fire(widget);
+    }
+
+    matches(selectors: CustomEditorSelector[], resource: URI): boolean {
+        return selectors.some(selector => this.selectorMatches(selector, resource));
+    }
+
+    selectorMatches(selector: CustomEditorSelector, resource: URI): boolean {
+        if (selector.filenamePattern) {
+            if (glob.match(selector.filenamePattern.toLowerCase(), resource.path.name.toLowerCase() + resource.path.ext.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

packages/workspace/src/browser/workspace-commands.ts
@@ -217,16 +217,7 @@ export class WorkspaceCommandContribution implements CommandContribution {
     }
 
     registerCommands(registry: CommandRegistry): void {
-        this.openerService.getOpeners().then(openers => {
-            for (const opener of openers) {
-                const openWithCommand = WorkspaceCommands.FILE_OPEN_WITH(opener);
-                registry.registerCommand(openWithCommand, this.newUriAwareCommandHandler({
-                    execute: uri => opener.open(uri),
-                    isEnabled: uri => opener.canHandle(uri) > 0,
-                    isVisible: uri => opener.canHandle(uri) > 0 && this.areMultipleOpenHandlersPresent(openers, uri)
-                }));
-            }
-        });
+        this.registerOpenWith(registry);
         registry.registerCommand(WorkspaceCommands.NEW_FILE, this.newWorkspaceRootUriAwareCommandHandler({
             execute: uri => this.getDirectory(uri).then(parent => {
                 if (parent) {
@@ -345,6 +336,24 @@ export class WorkspaceCommandContribution implements CommandContribution {
                 isVisible: uris => this.areWorkspaceRoots(uris) && this.workspaceService.saved
             }));
         });
+    }
+
+    openers: OpenHandler[];
+    protected async registerOpenWith(registry: CommandRegistry): Promise<void> {
+        if (this.openerService.onDidChangeOpeners) {
+            this.openerService.onDidChangeOpeners(async e => {
+                this.openers = await this.openerService.getOpeners();
+            });
+        }
+        const openers = await this.openerService.getOpeners();
+        for (const opener of openers) {
+            const openWithCommand = WorkspaceCommands.FILE_OPEN_WITH(opener);
+            registry.registerCommand(openWithCommand, this.newUriAwareCommandHandler({
+                execute: uri => opener.open(uri),
+                isEnabled: uri => opener.canHandle(uri) > 0,
+                isVisible: uri => opener.canHandle(uri) > 0 && this.areMultipleOpenHandlersPresent(this.openers, uri)
+            }));
+        }
     }
 
     protected newUriAwareCommandHandler(handler: UriCommandHandler<URI>): UriAwareCommandHandler<URI> {