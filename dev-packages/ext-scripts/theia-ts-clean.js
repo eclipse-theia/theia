ded917a610b75512e6af133fc965b175ea053552
Paul Marechal <paul.marechal@ericsson.com>
2021-09-24 10:24:25 -0400

    fixup

--- dev-packages/ext-scripts/theia-ts-clean.js
+++ dev-packages/ext-scripts/theia-ts-clean.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /********************************************************************************
- * Copyright (C) 2017 TypeFox and others.
+ * Copyright (C) 2021 Ericsson and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
