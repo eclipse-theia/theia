e91bea493a1a74ee2405340903e0419f2e363cfa
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-10-14 15:29:16 -0400

    typedoc: fix docs for `@theia/plugin`
    The commit fixes the missing documentation generation for
    `@theia/plugin` including `theia-proposed.d.ts` and `theia.d.ts`.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- packages/plugin/src/theia.d.ts
+++ packages/plugin/src/theia.d.ts
@@ -25,7 +25,7 @@ import './theia-proposed';
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable max-len */
 
-declare module '@theia/plugin' {
+export module '@theia/plugin' {
 
     /**
      * The version of the Theia API.
@@ -11047,4 +11047,3 @@ declare module '@theia/plugin' {
         export const onDidChangeSessions: Event<AuthenticationSessionsChangeEvent>;
     }
 }
-
