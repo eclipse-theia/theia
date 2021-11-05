5a9430326de84c5d95011d30c4c2f78ec5899a16
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-11-05 10:44:03 -0400

    terminal-preferences: update localization
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- packages/terminal/src/browser/terminal-preferences.ts
+++ packages/terminal/src/browser/terminal-preferences.ts
@@ -143,12 +143,12 @@ export const TerminalConfigSchema: PreferenceSchema = {
         },
         'terminal.integrated.confirmOnExit': {
             type: 'string',
-            description: nls.localize('vscode/terminalConfiguration/terminal.integrated.confirmOnExit', 'Controls whether to confirm when the window closes if there are active terminal sessions'),
+            description: nls.localizeByDefault('Controls whether to confirm when the window closes if there are active terminal sessions'),
             enum: ['never', 'always', 'hasChildProcesses'],
             enumDescriptions: [
-                nls.localize('vscode/terminalConfiguration/terminal.integrated.confirmOnExit.never', 'Never confirm.'),
-                nls.localize('vscode/terminalConfiguration/terminal.integrated.confirmOnExit.always', 'Always confirm if there are terminals.'),
-                nls.localize('vscode/terminalConfiguration/terminal.integrated.confirmOnExit.hasChildProcesses', 'Confirm if there are any terminals that have child processes.'),
+                nls.localizeByDefault('Never confirm.'),
+                nls.localizeByDefault('Always confirm if there are terminals.'),
+                nls.localizeByDefault('Confirm if there are any terminals that have child processes.'),
             ],
             default: 'never'
         }
