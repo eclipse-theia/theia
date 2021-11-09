eclipse-theia/theia

commit cf5ef236f4cd9865ed7b0cb1b15b414e44846eb7

browser: Modal Dialog fixed display issues

packages/plugin-ext/src/plugin/message-registry.ts
@@ -50,14 +50,17 @@ export class MessageRegistryExt {
             } else {
                 if ('modal' in optionsOrFirstItem) {
                     options.modal = optionsOrFirstItem.modal;
+                    if ('detail' in optionsOrFirstItem) {
+                        options.detail = optionsOrFirstItem.detail;
+                    }
                 }
             }
+            for (const item of rest) {
+                pushItem(item);
+            }
+            const actionHandle = await this.proxy.$showMessage(type, message, options, actions);
+            return actionHandle !== undefined ? items[actionHandle] : undefined;
         }
-        for (const item of rest) {
-            pushItem(item);
-        }
-        const actionHandle = await this.proxy.$showMessage(type, message, options, actions);
-        return actionHandle !== undefined ? items[actionHandle] : undefined;
-    }
 
+    }
 }

packages/plugin-ext/src/common/plugin-api-rpc.ts
@@ -383,6 +383,7 @@ export enum MainMessageType {
 }
 
 export interface MainMessageOptions {
+    detail?: string;
     modal?: boolean
     onCloseActionHandle?: number
 }

packages/plugin-ext/src/main/browser/dialogs/modal-notification.ts
@@ -18,7 +18,8 @@ import { Message } from '@theia/core/shared/@phosphor/messaging';
 import { codiconArray, Key } from '@theia/core/lib/browser';
 import { AbstractDialog } from '@theia/core/lib/browser/dialogs';
 import '../../../../src/main/browser/dialogs/style/modal-notification.css';
-import { MainMessageItem } from '../../../common/plugin-api-rpc';
+import { MainMessageItem, MainMessageOptions } from '../../../common/plugin-api-rpc';
+import { FrontendApplicationConfigProvider } from '@theia/core/lib/browser/frontend-application-config-provider';
 
 export enum MessageType {
     Error = 'error',
@@ -29,6 +30,7 @@ export enum MessageType {
 const NOTIFICATION = 'modal-Notification';
 const ICON = 'icon';
 const TEXT = 'text';
+const DETAIL = 'detail';
 
 @injectable()
 export class ModalNotification extends AbstractDialog<string | undefined> {
@@ -36,7 +38,7 @@ export class ModalNotification extends AbstractDialog<string | undefined> {
     protected actionTitle: string | undefined;
 
     constructor() {
-        super({ title: 'Theia' });
+        super({ title: FrontendApplicationConfigProvider.get().applicationName });
     }
 
     protected onCloseRequest(msg: Message): void {
@@ -48,12 +50,12 @@ export class ModalNotification extends AbstractDialog<string | undefined> {
         return this.actionTitle;
     }
 
-    showDialog(messageType: MessageType, text: string, actions: MainMessageItem[]): Promise<string | undefined> {
-        this.contentNode.appendChild(this.createMessageNode(messageType, text, actions));
+    showDialog(messageType: MessageType, text: string, options: MainMessageOptions, actions: MainMessageItem[]): Promise<string | undefined> {
+        this.contentNode.appendChild(this.createMessageNode(messageType, text, options, actions));
         return this.open();
     }
 
-    protected createMessageNode(messageType: MessageType, text: string, actions: MainMessageItem[]): HTMLElement {
+    protected createMessageNode(messageType: MessageType, text: string, options: MainMessageOptions, actions: MainMessageItem[]): HTMLElement {
         const messageNode = document.createElement('div');
         messageNode.classList.add(NOTIFICATION);
 
@@ -67,6 +69,13 @@ export class ModalNotification extends AbstractDialog<string | undefined> {
         const textElement = textContainer.appendChild(document.createElement('p'));
         textElement.textContent = text;
 
+        if (options.detail) {
+            const detailContainer = textContainer.appendChild(document.createElement('div'));
+            detailContainer.classList.add(DETAIL);
+            const detailElement = detailContainer.appendChild(document.createElement('p'));
+            detailElement.textContent = options.detail;
+        }
+
         actions.forEach((action: MainMessageItem) => {
             const button = this.createButton(action.title);
             button.classList.add('main');
@@ -79,8 +88,10 @@ export class ModalNotification extends AbstractDialog<string | undefined> {
                 },
                 'click');
         });
-        if (!actions.some(action => action.isCloseAffordance === true)) {
-            this.appendCloseButton('close');
+        if (actions.length <= 0) {
+            this.appendAcceptButton();
+        } else if (!actions.some(action => action.isCloseAffordance === true)) {
+            this.appendCloseButton('Close');
         }
 
         return messageNode;

packages/plugin-ext/src/main/browser/dialogs/style/modal-notification.css
@@ -42,10 +42,10 @@
     order: 1;
 }
 
-.modal-Notification .icon .fa {
+.modal-Notification .icon .codicon {
     line-height: inherit;
     vertical-align: middle;
-    font-size: 120%;
+    font-size: calc(var(--theia-ui-padding)*5);
     color: var(--theia-editorInfo-foreground);
 }
 
@@ -105,4 +105,19 @@
 
 .modal-Notification .buttons > button:hover {
     background-color: var(--theia-button-hoverBackground);
+}
+
+.modal-Notification .detail {
+    align-self: center;
+    order: 3;
+    flex: 1 100%;
+    color: var(--theia-descriptionForeground);
+}
+
+.modal-Notification .detail > p {
+    margin: calc(var(--theia-ui-padding) * 2) 0px 0px 0px;
+}
+
+.modal-Notification .text {
+    padding: calc(var(--theia-ui-padding)*1.5);
 }

packages/plugin-ext/src/main/browser/message-registry-main.ts
@@ -40,7 +40,7 @@ export class MessageRegistryMainImpl implements MessageRegistryMain {
                 type === MainMessageType.Warning ? MessageType.Warning :
                     MessageType.Info;
             const modalNotification = new ModalNotification();
-            return modalNotification.showDialog(messageType, message, actions);
+            return modalNotification.showDialog(messageType, message, options, actions);
         }
         switch (type) {
             case MainMessageType.Info:

packages/plugin/src/theia.d.ts
@@ -2395,6 +2395,13 @@ declare module '@theia/plugin' {
      */
     export interface MessageOptions {
 
+        /*
+         * Human-readable detail message that is rendered
+         * less prominent. Note that detail is only shown
+         * for modal messages.
+         */
+        detail?: string;
+
         /**
          * Indicates that this message should be modal.
          */

commit 477af4445c5e3b50c93286ab81a7a8c6c362e2ad

Always show plugin notification regardless of options (#10399)
Fixes a bug introduced by #10245 which prevented plugin notifications from being shown

packages/plugin-ext/src/plugin/message-registry.ts
@@ -55,12 +55,11 @@ export class MessageRegistryExt {
                     }
                 }
             }
-            for (const item of rest) {
-                pushItem(item);
-            }
-            const actionHandle = await this.proxy.$showMessage(type, message, options, actions);
-            return actionHandle !== undefined ? items[actionHandle] : undefined;
         }
-
+        for (const item of rest) {
+            pushItem(item);
+        }
+        const actionHandle = await this.proxy.$showMessage(type, message, options, actions);
+        return actionHandle !== undefined ? items[actionHandle] : undefined;
     }
 }