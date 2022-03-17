6ff4c6f97e6f03486ee12fba5da7f6bdebce9016
Colin Grant <colin.grant@ericsson.com>
2022-03-17 13:58:36 -0600

    newline after header

--- packages/core/src/browser/tree/tree-container.spec.ts
+++ packages/core/src/browser/tree/tree-container.spec.ts
@@ -13,6 +13,7 @@
 //
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 // *****************************************************************************
+
 import * as assert from 'assert';
 import { Container } from 'inversify';
 import { createTreeContainer, isTreeServices } from './tree-container';
