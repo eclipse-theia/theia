003aeb93aab78f779d7161cf19f84b94f132c254
vince-fugnitto <vincent.fugnitto@ericsson.com>
2022-03-24 09:26:47 -0400

    plugin: add `SourceFixAll` declaration
    The commit adds the `SourceFixAll` declaration to the framework's VS
    Code API so it is marked as supported in our compatibility report.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- packages/plugin/src/theia.d.ts
+++ packages/plugin/src/theia.d.ts
@@ -8070,6 +8070,14 @@ export module '@theia/plugin' {
          */
         static readonly SourceOrganizeImports: CodeActionKind;
 
+        /**
+         * Base kind for auto-fix source actions: `source.fixAll`.
+         *
+         * Fix all actions automatically fix errors that have a clear fix that do not require user input.
+         * They should not suppress errors or perform unsafe fixes such as generating new types or classes.
+         */
+        static readonly SourceFixAll: CodeActionKind;
+
         private constructor(value: string);
 
         /**
