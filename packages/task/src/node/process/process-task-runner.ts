b4338cad5e45e361d39c637dc6164b83de8a7e1d
jdmchp <jose.diaz@microchip.com>
2022-04-08 16:04:24 -0700

    Remove quoting commands without args even if they have spaces

--- packages/task/src/node/process/process-task-runner.ts
+++ packages/task/src/node/process/process-task-runner.ts
@@ -203,29 +203,23 @@ export class ProcessTaskRunner implements TaskRunner {
         return { command, args, commandLine, options };
     }
 
-    protected quoteShellCommandOrArg(commandOrArg: string | ShellQuotedString, quotingFunctions?: ShellQuotingFunctions): string | ShellQuotedString {
-        // We want to quote arguments only if needed.
-        if (quotingFunctions && typeof commandOrArg === 'string' && this.argumentNeedsQuotes(commandOrArg, quotingFunctions)) {
-            return {
-                quoting: ShellQuoting.Strong,
-                value: commandOrArg,
-            };
-        } else {
-            return commandOrArg;
-        }
-    }
     protected buildShellCommand(systemSpecificCommand: OsSpecificCommand, quotingFunctions?: ShellQuotingFunctions): string {
         if (Array.isArray(systemSpecificCommand.args)) {
-            const commandLineElements: Array<string | ShellQuotedString> =
-                [systemSpecificCommand.command, ...systemSpecificCommand.args].map(arg => this.quoteShellCommandOrArg(arg));
+            const commandLineElements: Array<string | ShellQuotedString> = [systemSpecificCommand.command, ...systemSpecificCommand.args].map(arg => {
+                // We want to quote arguments only if needed.
+                if (quotingFunctions && typeof arg === 'string' && this.argumentNeedsQuotes(arg, quotingFunctions)) {
+                    return {
+                        quoting: ShellQuoting.Strong,
+                        value: arg,
+                    };
+                } else {
+                    return arg;
+                }
+            });
             return createShellCommandLine(commandLineElements, quotingFunctions);
         } else {
             // No arguments are provided, so `command` is actually the full command line to execute.
-            if (systemSpecificCommand.command) {
-                return createShellCommandLine([this.quoteShellCommandOrArg(systemSpecificCommand.command, quotingFunctions)], quotingFunctions);
-            } else {
-                return '';
-            }
+            return systemSpecificCommand.command ?? '';
         }
     }
 
