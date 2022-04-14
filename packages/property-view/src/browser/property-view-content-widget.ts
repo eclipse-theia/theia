04b19d7b63946ddbcfa115a0edc7ab37a91cd92c
Nina Doschek <ndoschek@eclipsesource.com>
2022-04-14 15:22:27 +0200

    #11021 Add API documentation for property-view and selection-service
    - Add missing API documentation for interfaces and classes in the property-view package
    - Add missing API documentation for the Theia selection-service
    
    Resolves https://github.com/eclipse-theia/theia/issues/11021
    
    Contributed on behalf of STMicroelectronics.
    
    Signed-off-by: Nina Doschek <ndoschek@eclipsesource.com>

--- packages/property-view/src/browser/property-view-content-widget.ts
+++ packages/property-view/src/browser/property-view-content-widget.ts
@@ -17,6 +17,11 @@
 import { Widget } from '@theia/core/lib/browser/widgets/widget';
 import { PropertyDataService } from './property-data-service';
 
+/**
+ * A widget that fetches the property data via the given {@link PropertyDataService} and the given selection
+ * and renders that property data.
+ * This widget can be provided by a registered `PropertyViewWidgetProvider`.
+ */
 export interface PropertyViewContentWidget extends Widget {
     updatePropertyViewContent(propertyDataService?: PropertyDataService, selection?: Object): void;
 }
