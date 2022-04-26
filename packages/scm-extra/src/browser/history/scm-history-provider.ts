3bf151354324ffadd08c840783a9c5c9c3062822
Paul Marechal <paul.marechal@ericsson.com>
2022-04-26 13:38:46 -0400

    fix lint errors

--- packages/scm-extra/src/browser/history/scm-history-provider.ts
+++ packages/scm-extra/src/browser/history/scm-history-provider.ts
@@ -14,7 +14,6 @@
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 // *****************************************************************************
 
-
 import { ScmProvider } from '@theia/scm/lib/browser/scm-provider';
 import { ScmHistorySupport } from './scm-history-widget';
 
