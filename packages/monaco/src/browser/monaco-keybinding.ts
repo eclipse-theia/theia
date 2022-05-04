eb0930a267cda577b31532474a404e45b5ae350b
colin-grant-work <colin.grant@ericsson.com>
2022-05-04 17:08:28 -0600

    Update Monaco keybindings on keybinding change (#11101)
    Fixes a bug preventing Monaco from rendering keyboard
    shortcut hints correctly.

--- packages/monaco/src/browser/monaco-keybinding.ts
+++ packages/monaco/src/browser/monaco-keybinding.ts
@@ -14,19 +14,33 @@
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 // *****************************************************************************
 
-import { injectable, inject } from '@theia/core/shared/inversify';
-import { KeybindingContribution, KeybindingRegistry } from '@theia/core/lib/browser';
+import { injectable, inject, postConstruct } from '@theia/core/shared/inversify';
+import { KeybindingContribution, KeybindingRegistry, KeybindingScope, KeyCode } from '@theia/core/lib/browser';
 import { MonacoCommands } from './monaco-command';
 import { MonacoCommandRegistry } from './monaco-command-registry';
-import { environment } from '@theia/core';
+import { CommandRegistry, DisposableCollection, environment, isOSX } from '@theia/core';
 import { MonacoResolvedKeybinding } from './monaco-resolved-keybinding';
 import { KeybindingsRegistry } from '@theia/monaco-editor-core/esm/vs/platform/keybinding/common/keybindingsRegistry';
+import { StandaloneKeybindingService, StandaloneServices } from '@theia/monaco-editor-core/esm/vs/editor/standalone/browser/standaloneServices';
+import { IKeybindingService } from '@theia/monaco-editor-core/esm/vs/platform/keybinding/common/keybinding';
+import { MonacoContextKeyService } from './monaco-context-key-service';
+import { KEY_CODE_MAP } from './monaco-keycode-map';
+import * as monaco from '@theia/monaco-editor-core';
 
 @injectable()
 export class MonacoKeybindingContribution implements KeybindingContribution {
 
-    @inject(MonacoCommandRegistry)
-    protected readonly commands: MonacoCommandRegistry;
+    protected toDisposeOnKeybindingChange = new DisposableCollection();
+
+    @inject(MonacoCommandRegistry) protected readonly commands: MonacoCommandRegistry;
+    @inject(KeybindingRegistry) protected readonly keybindings: KeybindingRegistry;
+    @inject(CommandRegistry) protected readonly theiaCommandRegistry: CommandRegistry;
+    @inject(MonacoContextKeyService) protected readonly contextKeyService: MonacoContextKeyService;
+
+    @postConstruct()
+    protected init(): void {
+        this.keybindings.onKeybindingsChanged(() => this.updateMonacoKeybindings());
+    }
 
     registerKeybindings(registry: KeybindingRegistry): void {
         const defaultKeybindings = KeybindingsRegistry.getDefaultKeybindings();
@@ -43,5 +57,55 @@ export class MonacoKeybindingContribution implements KeybindingContribution {
                 registry.registerKeybinding({ command, keybinding, when });
             }
         }
+    }
+
+    protected updateMonacoKeybindings(): void {
+        const monacoKeybindingRegistry = StandaloneServices.get(IKeybindingService);
+        if (monacoKeybindingRegistry instanceof StandaloneKeybindingService) {
+            this.toDisposeOnKeybindingChange.dispose();
+            for (const binding of this.keybindings.getKeybindingsByScope(KeybindingScope.USER).concat(this.keybindings.getKeybindingsByScope(KeybindingScope.WORKSPACE))) {
+                const resolved = this.keybindings.resolveKeybinding(binding);
+                const command = binding.command;
+                const when = binding.when
+                    ? this.contextKeyService.parse(binding.when)
+                    : binding.context
+                        ? this.contextKeyService.parse(binding.context)
+                        : undefined;
+                this.toDisposeOnKeybindingChange.push(monacoKeybindingRegistry.addDynamicKeybinding(
+                    binding.command,
+                    this.toMonacoKeybindingNumber(resolved),
+                    (_, ...args) => this.theiaCommandRegistry.executeCommand(command, ...args),
+                    when,
+                ));
+            }
+        }
+    }
+
+    protected toMonacoKeybindingNumber(codes: KeyCode[]): number {
+        const [firstPart, secondPart] = codes;
+        if (codes.length > 2) {
+            console.warn('Key chords should not consist of more than two parts; got ', codes);
+        }
+        const encodedFirstPart = this.toSingleMonacoKeybindingNumber(firstPart);
+        const encodedSecondPart = secondPart ? this.toSingleMonacoKeybindingNumber(secondPart) << 16 : 0;
+        return monaco.KeyMod.chord(encodedFirstPart, encodedSecondPart);
+    }
+
+    protected toSingleMonacoKeybindingNumber(code: KeyCode): number {
+        const keyCode = code.key?.keyCode !== undefined ? KEY_CODE_MAP[code.key.keyCode] : 0;
+        let encoded = (keyCode >>> 0) & 0x000000FF;
+        if (code.alt) {
+            encoded |= monaco.KeyMod.Alt;
+        }
+        if (code.shift) {
+            encoded |= monaco.KeyMod.Shift;
+        }
+        if (code.ctrl) {
+            encoded |= monaco.KeyMod.WinCtrl;
+        }
+        if (code.meta && isOSX) {
+            encoded |= monaco.KeyMod.CtrlCmd;
+        }
+        return encoded;
     }
 }
