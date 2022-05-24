3dcfe3dde14b136a4887bab29bfaca0ae5f47ccd
Danbo Duan <bob170731@gmail.com>
2022-05-24 10:14:44 +0800

    Update packages/task/src/node/task-problem-collector.spec.ts
    update test case
    
    Co-authored-by: colin-grant-work <colin.grant@est.tech>

--- packages/task/src/node/task-problem-collector.spec.ts
+++ packages/task/src/node/task-problem-collector.spec.ts
@@ -232,7 +232,7 @@ describe('ProblemCollector', () => {
 
         expect(allMatches.length).to.eq(4);
 
-        expect((allMatches[0] as ProblemMatchData).resource!.path).eq('/home/test/test-dir.js');
+        expect((allMatches[0] as ProblemMatchData).resource?.path).eq('/home/test/test-dir.js');
         expect((allMatches[0] as ProblemMatchData).marker).deep.equal({
             range: { start: { line: 0, character: 0 }, end: { line: 0, character: 0 } },
             severity: DiagnosticSeverity.Warning,
