a6f438c27fc65b8fd4504881c296d48c81d9da61
Simon Graband <sgraband@eclipsesource.com>
2022-06-02 15:02:30 +0200

    Move WebviewViewResolveContext to root namespace (#11216)
    The WebviewViewResolveContext was previously in the window namespace.
    Removed it in the window namespace and added it to root namespace.
    
    Fixes #11149.
    
    Contributed on behalf of STMicroelectronics.

--- packages/plugin/src/theia.d.ts
+++ packages/plugin/src/theia.d.ts
@@ -4269,6 +4269,44 @@ export module '@theia/plugin' {
     }
 
     /**
+     * Additional information the webview view being resolved.
+     *
+     * @param T Type of the webview's state.
+     */
+    interface WebviewViewResolveContext<T = unknown> {
+        /**
+         * Persisted state from the webview content.
+         *
+         * To save resources, VS Code normally deallocates webview documents (the iframe content) that are not visible.
+         * For example, when the user collapse a view or switches to another top level activity in the sidebar, the
+         * `WebviewView` itself is kept alive but the webview's underlying document is deallocated. It is recreated when
+         * the view becomes visible again.
+         *
+         * You can prevent this behavior by setting `retainContextWhenHidden` in the `WebviewOptions`. However this
+         * increases resource usage and should be avoided wherever possible. Instead, you can use persisted state to
+         * save off a webview's state so that it can be quickly recreated as needed.
+         *
+         * To save off a persisted state, inside the webview call `acquireVsCodeApi().setState()` with
+         * any json serializable object. To restore the state again, call `getState()`. For example:
+         *
+         * ```js
+         * // Within the webview
+         * const vscode = acquireVsCodeApi();
+         *
+         * // Get existing state
+         * const oldState = vscode.getState() || { value: 0 };
+         *
+         * // Update state
+         * setState({ value: oldState.value + 1 })
+         * ```
+         *
+         * VS Code ensures that the persisted state is saved correctly when a webview is hidden and across
+         * editor restarts.
+         */
+        readonly state: T | undefined;
+    }
+
+    /**
      * Common namespace for dealing with window and editor, showing messages and user input.
      */
     export namespace window {
@@ -4608,44 +4646,6 @@ export module '@theia/plugin' {
          * @param viewType Type of the webview panel that can be serialized.
          * @param serializer Webview serializer.
          */
-
-        /**
-         * Additional information the webview view being resolved.
-         *
-         * @param T Type of the webview's state.
-         */
-        interface WebviewViewResolveContext<T = unknown> {
-            /**
-             * Persisted state from the webview content.
-             *
-             * To save resources, VS Code normally deallocates webview documents (the iframe content) that are not visible.
-             * For example, when the user collapse a view or switches to another top level activity in the sidebar, the
-             * `WebviewView` itself is kept alive but the webview's underlying document is deallocated. It is recreated when
-             * the view becomes visible again.
-             *
-             * You can prevent this behavior by setting `retainContextWhenHidden` in the `WebviewOptions`. However this
-             * increases resource usage and should be avoided wherever possible. Instead, you can use persisted state to
-             * save off a webview's state so that it can be quickly recreated as needed.
-             *
-             * To save off a persisted state, inside the webview call `acquireVsCodeApi().setState()` with
-             * any json serializable object. To restore the state again, call `getState()`. For example:
-             *
-             * ```js
-             * // Within the webview
-             * const vscode = acquireVsCodeApi();
-             *
-             * // Get existing state
-             * const oldState = vscode.getState() || { value: 0 };
-             *
-             * // Update state
-             * setState({ value: oldState.value + 1 })
-             * ```
-             *
-             * VS Code ensures that the persisted state is saved correctly when a webview is hidden and across
-             * editor restarts.
-             */
-            readonly state: T | undefined;
-        }
 
         export function registerWebviewViewProvider(viewId: string, provider: WebviewViewProvider, options?: {
             /**
