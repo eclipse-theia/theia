9a0ae2ed858c2cac36cb05a9307268f11d807180
Rob Moran <rob.moran@arm.com>
2022-06-24 15:58:36 +0100

    Mimic VS Code Experimental devices (#11323)
    * Add experimental device access from VS Code
    
    Signed-off-by: thegecko <rob.moran@arm.com>
    
    * Expose interfaces
    
    Signed-off-by: robmor01 <Rob.Moran@arm.com>

--- packages/plugin-ext-vscode/src/browser/plugin-vscode-commands-contribution.ts
+++ packages/plugin-ext-vscode/src/browser/plugin-vscode-commands-contribution.ts
@@ -102,6 +102,42 @@ export namespace VscodeCommands {
     };
 }
 
+// https://wicg.github.io/webusb/
+
+export interface UsbDeviceData {
+    readonly deviceClass: number;
+    readonly deviceProtocol: number;
+    readonly deviceSubclass: number;
+    readonly deviceVersionMajor: number;
+    readonly deviceVersionMinor: number;
+    readonly deviceVersionSubminor: number;
+    readonly manufacturerName?: string;
+    readonly productId: number;
+    readonly productName?: string;
+    readonly serialNumber?: string;
+    readonly usbVersionMajor: number;
+    readonly usbVersionMinor: number;
+    readonly usbVersionSubminor: number;
+    readonly vendorId: number;
+}
+
+// https://wicg.github.io/serial/
+
+export interface SerialPortData {
+    readonly usbVendorId?: number | undefined;
+    readonly usbProductId?: number | undefined;
+}
+
+// https://wicg.github.io/webhid/
+
+export interface HidDeviceData {
+    readonly opened: boolean;
+    readonly vendorId: number;
+    readonly productId: number;
+    readonly productName: string;
+    readonly collections: [];
+}
+
 @injectable()
 export class PluginVscodeCommandsContribution implements CommandContribution {
     @inject(CommandService)
@@ -724,6 +760,86 @@ export class PluginVscodeCommandsContribution implements CommandContribution {
                         model.selectNode(node);
                     }
                 }
+            }
+        });
+
+        commands.registerCommand({
+            id: 'workbench.experimental.requestUsbDevice'
+        }, {
+            execute: async (options?: { filters?: unknown[] }): Promise<UsbDeviceData | undefined> => {
+                const usb = (navigator as any).usb;
+                if (!usb) {
+                    return undefined;
+                }
+
+                const device = await usb.requestDevice({ filters: options?.filters ?? [] });
+                if (!device) {
+                    return undefined;
+                }
+
+                return {
+                    deviceClass: device.deviceClass,
+                    deviceProtocol: device.deviceProtocol,
+                    deviceSubclass: device.deviceSubclass,
+                    deviceVersionMajor: device.deviceVersionMajor,
+                    deviceVersionMinor: device.deviceVersionMinor,
+                    deviceVersionSubminor: device.deviceVersionSubminor,
+                    manufacturerName: device.manufacturerName,
+                    productId: device.productId,
+                    productName: device.productName,
+                    serialNumber: device.serialNumber,
+                    usbVersionMajor: device.usbVersionMajor,
+                    usbVersionMinor: device.usbVersionMinor,
+                    usbVersionSubminor: device.usbVersionSubminor,
+                    vendorId: device.vendorId,
+                };
+            }
+        });
+
+        commands.registerCommand({
+            id: 'workbench.experimental.requestSerialPort'
+        }, {
+            execute: async (options?: { filters?: unknown[] }): Promise<SerialPortData | undefined> => {
+                const serial = (navigator as any).serial;
+                if (!serial) {
+                    return undefined;
+                }
+
+                const port = await serial.requestPort({ filters: options?.filters ?? [] });
+                if (!port) {
+                    return undefined;
+                }
+
+                const info = port.getInfo();
+                return {
+                    usbVendorId: info.usbVendorId,
+                    usbProductId: info.usbProductId
+                };
+            }
+        });
+
+        commands.registerCommand({
+            id: 'workbench.experimental.requestHidDevice'
+        }, {
+            execute: async (options?: { filters?: unknown[] }): Promise<HidDeviceData | undefined> => {
+                const hid = (navigator as any).hid;
+                if (!hid) {
+                    return undefined;
+                }
+
+                const devices = await hid.requestDevice({ filters: options?.filters ?? [] });
+                if (!devices.length) {
+                    return undefined;
+                }
+
+                const device = devices[0];
+                return {
+                    opened: device.opened,
+                    vendorId: device.vendorId,
+                    productId: device.productId,
+                    productName: device.productName,
+                    collections: device.collections
+                };
             }
         });
     }
