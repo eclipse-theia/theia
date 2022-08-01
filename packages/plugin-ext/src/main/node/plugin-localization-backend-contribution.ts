992fa8495de97807cae2c37a81cc939f13688fc9
Alberto Iannaccone <a.iannaccone@arduino.cc>
2022-08-01 14:41:15 +0200

    wait for initialization in localization backend

--- packages/plugin-ext/src/main/node/plugin-localization-backend-contribution.ts
+++ packages/plugin-ext/src/main/node/plugin-localization-backend-contribution.ts
@@ -1,35 +1,21 @@
-import * as express from 'express';
 import { inject, injectable } from '@theia/core/shared/inversify';
 import { LocalizationBackendContribution } from '@theia/core/lib/node/i18n/localization-backend-contribution';
-import { Deferred } from '@theia/core/lib/common/promise-util';
 import { PluginDeployer } from '../../common/plugin-protocol';
 import { PluginDeployerImpl } from './plugin-deployer-impl';
+import { Deferred } from '@theia/core/src/common/promise-util';
 
 @injectable()
 export class PluginLocalizationBackendContribution extends LocalizationBackendContribution {
-  @inject(PluginDeployer)
-  protected readonly pluginDeployer: PluginDeployerImpl;
+    @inject(PluginDeployer)
+    protected readonly pluginDeployer: PluginDeployerImpl;
 
-  protected readonly initialized = new Deferred<void>();
+    override async initialize(): Promise<void> {
+        const pluginsDeployed = new Deferred();
+        this.pluginDeployer.onDidDeploy(() => {
+            pluginsDeployed.resolve();
+        });
 
-  override async initialize(): Promise<void> {
-    this.pluginDeployer.onDidDeploy(() => {
-      this.initialized.resolve();
-    });
-    return super.initialize();
-  }
-
-  override configure(app: express.Application): void {
-    app.get('/i18n/:locale', async (req, res) => {
-      let locale = req.params.locale;
-      await this.initialized.promise;
-      locale = this.localizationProvider
-        .getAvailableLanguages()
-        .some((e) => e.languageId === locale)
-        ? locale
-        : 'en';
-      this.localizationProvider.setCurrentLanguage(locale);
-      res.send(this.localizationProvider.loadLocalization(locale));
-    });
-  }
+        await Promise.all([this.localizationRegistry.initialize(), pluginsDeployed.promise]);
+        this.initialized.resolve();
+    }
 }
