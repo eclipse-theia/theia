f165c5b7981c01f9081dd637b381159ed5f407cf
Colin Grant <colin.grant@ericsson.com>
2022-08-29 17:08:49 -0600

    Remove redundant Memento declaration

--- packages/plugin/src/theia.d.ts
+++ packages/plugin/src/theia.d.ts
@@ -11227,39 +11227,6 @@ declare module '@theia/plugin' {
         export const onDidEndTaskProcess: Event<TaskProcessEndEvent>;
     }
 
-    /**
-     * A memento represents a storage utility. It can store and retrieve
-     * values.
-     */
-    export interface Memento {
-
-        /**
-         * Return a value.
-         *
-         * @param key A string.
-         * @return The stored value or `undefined`.
-         */
-        get<T>(key: string): T | undefined;
-
-        /**
-         * Return a value.
-         *
-         * @param key A string.
-         * @param defaultValue A value that should be returned when there is no
-         * value (`undefined`) with the given key.
-         * @return The stored value or the defaultValue.
-         */
-        get<T>(key: string, defaultValue: T): T;
-
-        /**
-         * Store a value. The value must be JSON-stringifyable.
-         *
-         * @param key A string.
-         * @param value A value. MUST not contain cyclic references.
-         */
-        update(key: string, value: any): Thenable<void>;
-    }
-
     /* The workspace symbol provider interface defines the contract between extensions
     * and the [symbol search](https://code.visualstudio.com/docs/editor/intellisense)-feature.
     */
