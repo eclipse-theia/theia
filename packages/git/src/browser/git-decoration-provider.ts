559c6cafd2f8242a784e1b0077d9f6c6f79c2152
Vincent Fugnitto <vincent.fugnitto@ericsson.com>
2022-09-20 09:45:24 -0400

    git: re-add support for decoration preferences (#11674)
    The commit re-adds support for `git.decorations.enabled` and `git.decorations.colors` which were previously not referenced and supported after a change in `v1.14.0`.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>
    Co-authored-by: Mark Sujew <mark.sujew@typefox.io>

--- packages/git/src/browser/git-decoration-provider.ts
+++ packages/git/src/browser/git-decoration-provider.ts
@@ -14,41 +14,55 @@
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 // *****************************************************************************
 
-import { inject, injectable } from '@theia/core/shared/inversify';
+import { inject, injectable, postConstruct } from '@theia/core/shared/inversify';
 import { GitFileChange, GitFileStatus, GitStatusChangeEvent } from '../common';
 import { CancellationToken, Emitter, Event } from '@theia/core/lib/common';
 import { Decoration, DecorationsProvider } from '@theia/core/lib/browser/decorations-service';
 import { GitRepositoryTracker } from './git-repository-tracker';
 import URI from '@theia/core/lib/common/uri';
+import { GitConfiguration, GitPreferences } from './git-preferences';
+import { PreferenceChangeEvent } from '@theia/core/lib/browser';
 
 @injectable()
 export class GitDecorationProvider implements DecorationsProvider {
 
+    @inject(GitPreferences) protected readonly preferences: GitPreferences;
+    @inject(GitRepositoryTracker) protected readonly gitRepositoryTracker: GitRepositoryTracker;
+
+    protected decorationsEnabled: boolean;
+    protected colorsEnabled: boolean;
+
+    protected decorations = new Map<string, Decoration>();
+    protected uris = new Set<string>();
+
     private readonly onDidChangeDecorationsEmitter = new Emitter<URI[]>();
     readonly onDidChange: Event<URI[]> = this.onDidChangeDecorationsEmitter.event;
 
-    private decorations = new Map<string, Decoration>();
-
-    constructor(@inject(GitRepositoryTracker) protected readonly gitRepositoryTracker: GitRepositoryTracker) {
-        this.gitRepositoryTracker.onGitEvent((event: GitStatusChangeEvent | undefined) => {
-            this.onGitEvent(event);
-        });
+    @postConstruct()
+    protected init(): void {
+        this.decorationsEnabled = this.preferences['git.decorations.enabled'];
+        this.colorsEnabled = this.preferences['git.decorations.colors'];
+        this.gitRepositoryTracker.onGitEvent((event: GitStatusChangeEvent | undefined) => this.handleGitEvent(event));
+        this.preferences.onPreferenceChanged((event: PreferenceChangeEvent<GitConfiguration>) => this.handlePreferenceChange(event));
     }
 
-    private async onGitEvent(event: GitStatusChangeEvent | undefined): Promise<void> {
+    protected async handleGitEvent(event: GitStatusChangeEvent | undefined): Promise<void> {
+        this.updateDecorations(event);
+        this.triggerDecorationChange();
+    }
+
+    protected updateDecorations(event?: GitStatusChangeEvent): void {
         if (!event) {
             return;
         }
-
         const newDecorations = new Map<string, Decoration>();
         this.collectDecorationData(event.status.changes, newDecorations);
 
-        const uris = new Set([...this.decorations.keys()].concat([...newDecorations.keys()]));
+        this.uris = new Set([...this.decorations.keys()].concat([...newDecorations.keys()]));
         this.decorations = newDecorations;
-        this.onDidChangeDecorationsEmitter.fire(Array.from(uris, value => new URI(value)));
     }
 
-    private collectDecorationData(changes: GitFileChange[], bucket: Map<string, Decoration>): void {
+    protected collectDecorationData(changes: GitFileChange[], bucket: Map<string, Decoration>): void {
         changes.forEach(change => {
             const color = GitFileStatus.getColor(change.status, change.staged);
             bucket.set(change.uri, {
@@ -61,7 +75,48 @@ export class GitDecorationProvider implements DecorationsProvider {
     }
 
     provideDecorations(uri: URI, token: CancellationToken): Decoration | Promise<Decoration | undefined> | undefined {
-        return this.decorations.get(uri.toString());
+        if (this.decorationsEnabled) {
+            const decoration = this.decorations.get(uri.toString());
+            if (decoration && !this.colorsEnabled) {
+                // Remove decoration color if disabled.
+                return {
+                    ...decoration,
+                    colorId: undefined
+                };
+            }
+            return decoration;
+        }
+        return undefined;
     }
+
+    protected handlePreferenceChange(event: PreferenceChangeEvent<GitConfiguration>): void {
+        const { preferenceName, newValue } = event;
+        let updateDecorations = false;
+        if (preferenceName === 'git.decorations.enabled') {
+            updateDecorations = true;
+            const decorationsEnabled = !!newValue;
+            if (this.decorationsEnabled !== decorationsEnabled) {
+                this.decorationsEnabled = decorationsEnabled;
+            }
+        }
+        if (preferenceName === 'git.decorations.colors') {
+            updateDecorations = true;
+            const colorsEnabled = !!newValue;
+            if (this.colorsEnabled !== colorsEnabled) {
+                this.colorsEnabled = colorsEnabled;
+            }
+        }
+        if (updateDecorations) {
+            this.triggerDecorationChange();
+        }
+    }
+
+    /**
+     * Notify that the provider has been updated to trigger a re-render of decorations.
+     */
+    protected triggerDecorationChange(): void {
+        this.onDidChangeDecorationsEmitter.fire(Array.from(this.uris, value => new URI(value)));
+    }
+
 }
 
