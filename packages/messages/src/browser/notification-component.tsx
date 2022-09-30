fd845f65c84bcc5b8064f04c621ae6b6baa014a0
colin-grant-work <colin.grant@ericsson.com>
2022-09-30 12:24:11 -0600

    Add wrapper with opaque background to toasts (#11714)

--- packages/messages/src/browser/notification-component.tsx
+++ packages/messages/src/browser/notification-component.tsx
@@ -73,53 +73,55 @@ export class NotificationComponent extends React.Component<NotificationComponent
         const { messageId, message, type, progress, collapsed, expandable, source, actions } = this.props.notification;
         const isProgress = type === 'progress' || typeof progress === 'number';
         const icon = type === 'progress' ? 'info' : type;
-        return (<div key={messageId} className='theia-notification-list-item' tabIndex={0}>
-            <div className={`theia-notification-list-item-content ${collapsed ? 'collapsed' : ''}`}>
-                <div className='theia-notification-list-item-content-main'>
-                    <div className={`theia-notification-icon ${codicon(icon)} ${icon}`} />
-                    <div className='theia-notification-message'>
-                        <span
-                            // eslint-disable-next-line react/no-danger
-                            dangerouslySetInnerHTML={{
-                                __html: DOMPurify.sanitize(message, {
-                                    ALLOW_UNKNOWN_PROTOCOLS: true // DOMPurify usually strips non http(s) links from hrefs
-                                })
-                            }}
-                            onClick={this.onMessageClick}
-                        />
+        return (<div key={messageId} className='theia-notification-list-item-container'>
+            <div className='theia-notification-list-item' tabIndex={0}>
+                <div className={`theia-notification-list-item-content ${collapsed ? 'collapsed' : ''}`}>
+                    <div className='theia-notification-list-item-content-main'>
+                        <div className={`theia-notification-icon ${codicon(icon)} ${icon}`} />
+                        <div className='theia-notification-message'>
+                            <span
+                                // eslint-disable-next-line react/no-danger
+                                dangerouslySetInnerHTML={{
+                                    __html: DOMPurify.sanitize(message, {
+                                        ALLOW_UNKNOWN_PROTOCOLS: true // DOMPurify usually strips non http(s) links from hrefs
+                                    })
+                                }}
+                                onClick={this.onMessageClick}
+                            />
+                        </div>
+                        <ul className='theia-notification-actions'>
+                            {expandable && (
+                                <li className={codicon('chevron-down', true) + (collapsed ? ' expand' : ' collapse')} title={collapsed ? 'Expand' : 'Collapse'}
+                                    data-message-id={messageId} onClick={this.onToggleExpansion} />
+                            )}
+                            {!isProgress && (<li className={codicon('close', true)} title={nls.localizeByDefault('Clear')} data-message-id={messageId}
+                                onClick={this.onClear} />)}
+                        </ul>
                     </div>
-                    <ul className='theia-notification-actions'>
-                        {expandable && (
-                            <li className={codicon('chevron-down', true) + (collapsed ? ' expand' : ' collapse')} title={collapsed ? 'Expand' : 'Collapse'}
-                                data-message-id={messageId} onClick={this.onToggleExpansion} />
-                        )}
-                        {!isProgress && (<li className={codicon('close', true)} title={nls.localizeByDefault('Clear')} data-message-id={messageId}
-                            onClick={this.onClear} />)}
-                    </ul>
+                    {(source || !!actions.length) && (
+                        <div className='theia-notification-list-item-content-bottom'>
+                            <div className='theia-notification-source'>
+                                {source && (<span>{source}</span>)}
+                            </div>
+                            <div className='theia-notification-buttons'>
+                                {actions && actions.map((action, index) => (
+                                    <button key={messageId + `-action-${index}`} className='theia-button'
+                                        data-message-id={messageId} data-action={action}
+                                        onClick={this.onAction}>
+                                        {action}
+                                    </button>
+                                ))}
+                            </div>
+                        </div>
+                    )}
                 </div>
-                {(source || !!actions.length) && (
-                    <div className='theia-notification-list-item-content-bottom'>
-                        <div className='theia-notification-source'>
-                            {source && (<span>{source}</span>)}
-                        </div>
-                        <div className='theia-notification-buttons'>
-                            {actions && actions.map((action, index) => (
-                                <button key={messageId + `-action-${index}`} className='theia-button'
-                                    data-message-id={messageId} data-action={action}
-                                    onClick={this.onAction}>
-                                    {action}
-                                </button>
-                            ))}
-                        </div>
+                {isProgress && (
+                    <div className='theia-notification-item-progress'>
+                        <div className={`theia-notification-item-progressbar ${progress ? 'determinate' : 'indeterminate'}`}
+                            style={{ width: `${progress ?? '100'}%` }} />
                     </div>
                 )}
             </div>
-            {isProgress && (
-                <div className='theia-notification-item-progress'>
-                    <div className={`theia-notification-item-progressbar ${progress ? 'determinate' : 'indeterminate'}`}
-                        style={{ width: `${progress ?? '100'}%` }} />
-                </div>
-            )}
         </div>);
     }
 
