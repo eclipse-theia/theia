42e3c54b6894c9b95724f869da807a129d90f391
Jonah Iden <jonah.iden@typefox.io>
2023-01-10 16:49:41 +0100

    removed duplicate
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- packages/plugin/src/theia.d.ts
+++ packages/plugin/src/theia.d.ts
@@ -5389,17 +5389,6 @@ export module '@theia/plugin' {
         valueSelection: readonly [number, number];
 
         /**
-         * Selection range in the input value. Defined as tuple of two number where the
-         * first is the inclusive start index and the second the exclusive end index. When `undefined` the whole
-         * pre-filled value will be selected, when empty (start equals end) only the cursor will be set,
-         * otherwise the defined range will be selected.
-         *
-         * This property does not get updated when the user types or makes a selection,
-         * but it can be updated by the extension.
-         */
-        valueSelection: readonly [number, number];
-
-        /**
          * An event signaling when the value has changed.
          */
         readonly onDidChangeValue: Event<string>;
