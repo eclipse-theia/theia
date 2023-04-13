a28c9c261afdbf84c817ef8d9a406d0b5880dc4a
Mark Sujew <mark.sujew@typefox.io>
2023-04-13 15:46:17 +0000

    Fix backend sourcemaps

--- dev-packages/application-manager/src/generator/webpack-generator.ts
+++ dev-packages/application-manager/src/generator/webpack-generator.ts
@@ -390,7 +390,8 @@ const config = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'lib', 'backend')
+        path: path.resolve(__dirname, 'lib', 'backend'),
+        devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]?[loaders]',
     },
     ${this.ifElectron(`externals: {
         electron: 'require("electron")'
@@ -418,6 +419,11 @@ const config = {
                 options: {
                     name: 'native/[name].[ext]'
                 }
+            },
+            {
+                test: /\\.js$/,
+                enforce: 'pre',
+                loader: 'source-map-loader'
             },
             // jsonc-parser exposes its UMD implementation by default, which
             // confuses Webpack leading to missing js in the bundles.
