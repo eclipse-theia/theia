57f8a210a139ce84f50192224c9532e9155059f9
Erez Odier <2045191+erezmus@users.noreply.github.com>
2023-04-18 13:39:27 +0100

    fix error not being caught

--- packages/git/src/node/git-repository-manager.ts
+++ packages/git/src/node/git-repository-manager.ts
@@ -30,7 +30,7 @@ export class GitRepositoryManager {
 
     run<T>(repository: Repository, op: () => Promise<T>): Promise<T> {
         const result = op();
-        result.then(() => this.sync(repository));
+        result.then(() => this.sync(repository)).catch(e => console.log(e));
         return result;
     }
 
@@ -41,6 +41,7 @@ export class GitRepositoryManager {
     protected async sync(repository: Repository): Promise<void> {
         const reference = await this.getWatcher(repository);
         const watcher = reference.object;
+
         // dispose the reference once the next sync cycle is actually completed
         watcher.sync().then(() => reference.dispose());
     }
