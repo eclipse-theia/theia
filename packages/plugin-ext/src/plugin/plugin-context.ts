d8a40aeab69733d57e016125a454741990919983
Marc Dumais <marc.dumais@ericsson.com>
2023-05-09 09:42:49 -0400

    [vscode] Stub for proposed API: EditSessionIdentityProvider
    EditSessionIdentityProvider is a new proposed API. As of 1.72.2
    it's only used in built-in git (vscode.git).
    
    This commit adds a stub implementation, sufficient to use
    vscode.git 1.72.2 and hopefully later versions, if we need
    it to. The eventual goal is to implement this API with a
    working implementation.
    
    Fixes #12437
    
    Signed-off-by: Marc Dumais <marc.dumais@ericsson.com>

--- packages/plugin-ext/src/plugin/plugin-context.ts
+++ packages/plugin-ext/src/plugin/plugin-context.ts
@@ -714,6 +714,9 @@ export function createAPIFactory(
             },
             get onDidGrantWorkspaceTrust(): theia.Event<void> {
                 return workspaceExt.onDidGrantWorkspaceTrust;
+            },
+            registerEditSessionIdentityProvider(scheme: string, provider: theia.EditSessionIdentityProvider) {
+                return workspaceExt.$registerEditSessionIdentityProvider(scheme, provider);
             }
         };
 
