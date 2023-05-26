c15e9f48382cf176a6d870e886400a67298be282
Pavel Nikolaev <pnik@1c.ru>
2023-05-26 15:52:42 +0300

    12568 Localized debug session status

--- packages/debug/src/browser/debug-session.tsx
+++ packages/debug/src/browser/debug-session.tsx
@@ -43,6 +43,7 @@ import { DebugContribution } from './debug-contribution';
 import { Deferred, waitForEvent } from '@theia/core/lib/common/promise-util';
 import { WorkspaceService } from '@theia/workspace/lib/browser';
 import { DebugInstructionBreakpoint } from './model/debug-instruction-breakpoint';
+import { nls } from '@theia/core';
 
 export enum DebugState {
     Inactive,
@@ -867,7 +868,11 @@ export class DebugSession implements CompositeTreeElement {
         }
         return <div className='theia-debug-session' title='Session'>
             <span className='label'>{this.label + label}</span>
-            <span className='status'>{this.state === DebugState.Stopped ? 'Paused' : 'Running'}</span>
+            <span className='status'>
+                    {this.state === DebugState.Stopped
+                        ? nls.localize('theia/debug/paused', 'Paused')
+                        : nls.localize('theia/debug/running', 'Running')}
+                </span>
         </div>;
     }
 
