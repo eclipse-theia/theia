e78ef60b2f5ac80bb17450a164a259b8b3b6dfdb
Mark Sujew <mark.sujew@typefox.io>
2023-06-20 21:42:27 +0000

    Fix missing whitespace

--- packages/scm-extra/src/browser/history/scm-history-widget.tsx
+++ packages/scm-extra/src/browser/history/scm-history-widget.tsx
@@ -308,7 +308,7 @@ export class ScmHistoryWidget extends ScmNavigableListWidget<ScmHistoryListNode>
                     const repoName = repo ? `${this.labelProvider.getName(new URI(repo.provider.rootUri))}` : '';
 
                     const relPathAndRepo = [relPath, repoName].filter(Boolean).join(
-                        nls.localize('theia/git/prepositionIn', 'in')
+                        ` ${nls.localize('theia/git/prepositionIn', 'in')} `
                     );
                     path = `${relPathAndRepo}`;
                 }
