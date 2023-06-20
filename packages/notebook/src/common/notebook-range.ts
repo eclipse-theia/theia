b6529cab1b89217ea184d60c12383c2c5b1bca0c
Jonah Iden <jonah.iden@typefox.io>
2023-06-20 16:38:52 +0200

    fixed file headers
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- packages/notebook/src/common/notebook-range.ts
+++ packages/notebook/src/common/notebook-range.ts
@@ -1,5 +1,5 @@
 // *****************************************************************************
-// Copyright (C) 2023 TypeFox and others.
+// Copyright (C) 2023 Typefox and others.
 //
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v. 2.0 which is available at
@@ -11,7 +11,7 @@
 // with the GNU Classpath Exception which is available at
 // https://www.gnu.org/software/classpath/license.html.
 //
-// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
 // *****************************************************************************
 
 /**
