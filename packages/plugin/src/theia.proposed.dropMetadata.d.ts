813ee110794ea162ab2e2a6d4c0299a6ecbcb8e2
Mark Sujew <mark.sujew@typefox.io>
2023-07-21 13:45:06 +0000

    Fix build error

--- packages/plugin/src/theia.proposed.dropMetadata.d.ts
+++ packages/plugin/src/theia.proposed.dropMetadata.d.ts
@@ -20,7 +20,7 @@
  *--------------------------------------------------------------------------------------------*/
 // code copied and modified from https://github.com/microsoft/vscode/blob/1.79.0/src/vscode-dts/vscode.proposed.dropMetadata.d.ts
 
-declare module '@theia/plugin' {
+export module '@theia/plugin' {
 
     // https://github.com/microsoft/vscode/issues/179430
 
