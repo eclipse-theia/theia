b7609f8cda2e0a3de2c96c90205a4acf87ab3800
Vlad Arama <vlad.arama@ericsson.com>
2023-09-15 15:24:37 -0400

    fix linting
    This commit fixes a few linting errors related to file-headers.
    
    Signed-off-by: Vlad Arama <vlad.arama@ericsson.com>

--- packages/monaco/src/browser/monaco-icon-registry-types.ts
+++ packages/monaco/src/browser/monaco-icon-registry-types.ts
@@ -1,4 +1,3 @@
-
 // *****************************************************************************
 // Copyright (C) 2023 Ericsson and others.
 //
@@ -12,8 +11,9 @@
 // with the GNU Classpath Exception which is available at
 // https://www.gnu.org/software/classpath/license.html.
 //
-// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
 // *****************************************************************************
+
 import { Event } from '@theia/core';
 import { IJSONSchema } from '@theia/core/lib/common/json-schema';
 import { ThemeIcon } from '@theia/monaco-editor-core/esm/vs/platform/theme/common/themeService';
