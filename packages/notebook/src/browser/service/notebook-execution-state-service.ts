fae19fd6c1d31d52ed8d21400428b2b4026e1a35
Jonah Iden <jonah.iden@typefox.io>
2023-10-24 16:51:00 +0200

    moved CellEditOperation from common to browser
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- packages/notebook/src/browser/service/notebook-execution-state-service.ts
+++ packages/notebook/src/browser/service/notebook-execution-state-service.ts
@@ -23,8 +23,9 @@ import { inject, injectable } from '@theia/core/shared/inversify';
 import { NotebookService } from './notebook-service';
 import {
     CellEditType, CellExecuteOutputEdit, CellExecuteOutputItemEdit, CellExecutionUpdateType,
-    CellUri, CellPartialInternalMetadataEditByHandle, NotebookCellExecutionState, CellEditOperation, NotebookCellInternalMetadata
+    CellUri, NotebookCellExecutionState, NotebookCellInternalMetadata
 } from '../../common';
+import { CellPartialInternalMetadataEditByHandle, CellEditOperation } from '../notebook-types';
 import { NotebookModel } from '../view-model/notebook-model';
 import { v4 } from 'uuid';
 
