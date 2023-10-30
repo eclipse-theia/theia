199f814e78371a0c22bda4238c1195eab967a1e1
Olaf Lessenich <olessenich@eclipsesource.com>
2023-10-30 11:57:26 +0100

    fix: Remove duplicated bindings
    Contributed on behalf of STMicroelectronics
    
    Signed-off-by: Olaf Lessenich <olessenich@eclipsesource.com>

--- packages/filesystem/src/electron-browser/file-dialog/electron-file-dialog-module.ts
+++ packages/filesystem/src/electron-browser/file-dialog/electron-file-dialog-module.ts
@@ -15,33 +15,10 @@
 // *****************************************************************************
 
 import { ContainerModule } from '@theia/core/shared/inversify';
-import { DefaultFileDialogService, FileDialogService } from '../../browser/file-dialog/file-dialog-service';
 import { ElectronFileDialogService } from './electron-file-dialog-service';
-import { LocationListRenderer, LocationListRendererFactory, LocationListRendererOptions } from '../../browser/location';
-import { FileDialogHiddenFilesToggleRenderer, HiddenFilesToggleRendererFactory } from '../../browser/file-dialog/file-dialog-hidden-files-renderer';
-import { FileDialogTree } from '../../browser/file-dialog/file-dialog-tree';
-import { FileDialogTreeFiltersRenderer, FileDialogTreeFiltersRendererFactory, FileDialogTreeFiltersRendererOptions } from '../../browser/file-dialog';
+import { FileDialogService } from '../../browser';
 
 export default new ContainerModule((bind, _unbind, _isBound, rebind) => {
-    bind(DefaultFileDialogService).toSelf().inSingletonScope();
-    bind(LocationListRendererFactory).toFactory(context => (options: LocationListRendererOptions) => {
-        const childContainer = context.container.createChild();
-        childContainer.bind(LocationListRendererOptions).toConstantValue(options);
-        childContainer.bind(LocationListRenderer).toSelf().inSingletonScope();
-        return childContainer.get(LocationListRenderer);
-    });
-    bind(FileDialogTreeFiltersRendererFactory).toFactory(context => (options: FileDialogTreeFiltersRendererOptions) => {
-        const childContainer = context.container.createChild();
-        childContainer.bind(FileDialogTreeFiltersRendererOptions).toConstantValue(options);
-        childContainer.bind(FileDialogTreeFiltersRenderer).toSelf().inSingletonScope();
-        return childContainer.get(FileDialogTreeFiltersRenderer);
-    });
-    bind(HiddenFilesToggleRendererFactory).toFactory(({ container }) => (fileDialogTree: FileDialogTree) => {
-        const child = container.createChild();
-        child.bind(FileDialogTree).toConstantValue(fileDialogTree);
-        child.bind(FileDialogHiddenFilesToggleRenderer).toSelf().inSingletonScope();
-        return child.get(FileDialogHiddenFilesToggleRenderer);
-    });
     bind(ElectronFileDialogService).toSelf().inSingletonScope();
     rebind(FileDialogService).toService(ElectronFileDialogService);
 });
