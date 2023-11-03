eclipse-theia/theia

commit cd74848f835476f8929c2a68c040a2e83daba7bf

Fire breakpoint events only if breakpoint metadata changes (#12183)
Fixes a problem with breakpoints in split editors causing a stack overflow.

packages/debug/src/browser/breakpoint/breakpoint-manager.ts
@@ -14,6 +14,7 @@
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
 // *****************************************************************************
 
+import * as deepEqual from 'fast-deep-equal';
 import { injectable, inject } from '@theia/core/shared/inversify';
 import { Emitter } from '@theia/core/lib/common';
 import { StorageService } from '@theia/core/lib/browser';
@@ -60,18 +61,22 @@ export class BreakpointManager extends MarkerManager<SourceBreakpoint> {
     readonly onDidChangeInstructionBreakpoints = this.onDidChangeInstructionBreakpointsEmitter.event;
 
     override setMarkers(uri: URI, owner: string, newMarkers: SourceBreakpoint[]): Marker<SourceBreakpoint>[] {
-        const result = super.setMarkers(uri, owner, newMarkers);
+        const result = this.findMarkers({ uri, owner });
         const added: SourceBreakpoint[] = [];
         const removed: SourceBreakpoint[] = [];
         const changed: SourceBreakpoint[] = [];
-        const oldMarkers = new Map(result.map(({ data }) => [data.id, data] as [string, SourceBreakpoint]));
+        const oldMarkers = new Map(result.map(({ data }) => [data.id, data]));
         const ids = new Set<string>();
+        let didChangeMarkers = false;
         for (const newMarker of newMarkers) {
             ids.add(newMarker.id);
-            if (oldMarkers.has(newMarker.id)) {
-                changed.push(newMarker);
-            } else {
+            const oldMarker = oldMarkers.get(newMarker.id);
+            if (!oldMarker) {
                 added.push(newMarker);
+            } else {
+                // We emit all existing markers as 'changed', but we only fire an event if something really did change.
+                didChangeMarkers ||= !!added.length || !deepEqual(oldMarker, newMarker);
+                changed.push(newMarker);
             }
         }
         for (const [id, data] of oldMarkers.entries()) {
@@ -79,7 +84,10 @@ export class BreakpointManager extends MarkerManager<SourceBreakpoint> {
                 removed.push(data);
             }
         }
-        this.onDidChangeBreakpointsEmitter.fire({ uri, added, removed, changed });
+        if (added.length || removed.length || didChangeMarkers) {
+            super.setMarkers(uri, owner, newMarkers);
+            this.onDidChangeBreakpointsEmitter.fire({ uri, added, removed, changed });
+        }
         return result;
     }
 

packages/debug/src/browser/editor/debug-editor-model.ts
@@ -61,7 +61,7 @@ export class DebugEditorModel implements Disposable {
     protected uri: URI;
 
     protected breakpointDecorations: string[] = [];
-    protected breakpointRanges = new Map<string, monaco.Range>();
+    protected breakpointRanges = new Map<string, [monaco.Range, SourceBreakpoint]>();
 
     protected currentBreakpointDecorations: string[] = [];
 
@@ -248,12 +248,12 @@ export class DebugEditorModel implements Disposable {
         this.renderCurrentBreakpoints();
     }
     protected renderBreakpoints(): void {
-        const decorations = this.createBreakpointDecorations();
-        this.breakpointDecorations = this.deltaDecorations(this.breakpointDecorations, decorations);
-        this.updateBreakpointRanges();
-    }
-    protected createBreakpointDecorations(): monaco.editor.IModelDeltaDecoration[] {
         const breakpoints = this.breakpoints.getBreakpoints(this.uri);
+        const decorations = this.createBreakpointDecorations(breakpoints);
+        this.breakpointDecorations = this.deltaDecorations(this.breakpointDecorations, decorations);
+        this.updateBreakpointRanges(breakpoints);
+    }
+    protected createBreakpointDecorations(breakpoints: SourceBreakpoint[]): monaco.editor.IModelDeltaDecoration[] {
         return breakpoints.map(breakpoint => this.createBreakpointDecoration(breakpoint));
     }
     protected createBreakpointDecoration(breakpoint: SourceBreakpoint): monaco.editor.IModelDeltaDecoration {
@@ -267,11 +267,14 @@ export class DebugEditorModel implements Disposable {
             }
         };
     }
-    protected updateBreakpointRanges(): void {
+
+    protected updateBreakpointRanges(breakpoints: SourceBreakpoint[]): void {
         this.breakpointRanges.clear();
-        for (const decoration of this.breakpointDecorations) {
+        for (let i = 0; i < this.breakpointDecorations.length; i++) {
+            const decoration = this.breakpointDecorations[i];
+            const breakpoint = breakpoints[i];
             const range = this.editor.getControl().getModel()!.getDecorationRange(decoration)!;
-            this.breakpointRanges.set(decoration, range);
+            this.breakpointRanges.set(decoration, [range, breakpoint]);
         }
     }
 
@@ -312,7 +315,7 @@ export class DebugEditorModel implements Disposable {
         }
         for (const decoration of this.breakpointDecorations) {
             const range = this.editor.getControl().getModel()!.getDecorationRange(decoration);
-            const oldRange = this.breakpointRanges.get(decoration)!;
+            const oldRange = this.breakpointRanges.get(decoration)![0];
             if (!range || !range.equalsRange(oldRange)) {
                 return true;
             }
@@ -328,8 +331,7 @@ export class DebugEditorModel implements Disposable {
             if (range && !lines.has(range.startLineNumber)) {
                 const line = range.startLineNumber;
                 const column = range.startColumn;
-                const oldRange = this.breakpointRanges.get(decoration);
-                const oldBreakpoint = oldRange && this.breakpoints.getInlineBreakpoint(uri, oldRange.startLineNumber, oldRange.startColumn);
+                const oldBreakpoint = this.breakpointRanges.get(decoration)?.[1];
                 const breakpoint = SourceBreakpoint.create(uri, { line, column }, oldBreakpoint);
                 breakpoints.push(breakpoint);
                 lines.add(line);

commit 5964e6c3a89dccd689b16b4b9e0fd9eb613415d4

Fix #12546: UI not updated upon editing breakpoint condition
Fixes #12546, which is a regression introduced by #12183, by ensuring that
`BreakpointManager.setMarkers` fires a `SourceBreakpointsChangeEvent` when
`oldMarker === newMarker`, as there is no way to actually detect a change
in this case.

packages/debug/src/browser/breakpoint/breakpoint-manager.ts
@@ -75,7 +75,9 @@ export class BreakpointManager extends MarkerManager<SourceBreakpoint> {
                 added.push(newMarker);
             } else {
                 // We emit all existing markers as 'changed', but we only fire an event if something really did change.
-                didChangeMarkers ||= !!added.length || !deepEqual(oldMarker, newMarker);
+                // We also fire an event if oldMarker === newMarker, as we cannot actually detect a change in this case
+                // (https://github.com/eclipse-theia/theia/issues/12546).
+                didChangeMarkers ||= !!added.length || oldMarker === newMarker || !deepEqual(oldMarker, newMarker);
                 changed.push(newMarker);
             }
         }