1edbc9e5e0a56c7adffc3fdfbf937dc6e1ef0bea
shanehsi <shane.hsi@foxmail.com>
2024-01-04 21:08:34 +0800

    A simple command contribution

--- examples/hello-world/src/browser/hello-world-frontend-module.ts
+++ examples/hello-world/src/browser/hello-world-frontend-module.ts
@@ -1,5 +1,25 @@
 // tslint:disable:file-header
-import { ContainerModule, interfaces } from '@theia/core/shared/inversify';
+import { CommandContribution, CommandRegistry, MessageService } from '@theia/core';
+import { ContainerModule, inject, injectable, interfaces } from '@theia/core/shared/inversify';
+
+@injectable()
+class SampleCommandContribution implements CommandContribution {
+    @inject(MessageService) protected readonly messageService: MessageService;
+
+    registerCommands(registry: CommandRegistry): void {
+        registry.registerCommand(
+            {
+                id: 'say-hi.command',
+                category: 'Examples',
+                label: 'Say Hi'
+            }, {
+            execute: () => {
+                this.messageService.info('Hello world!')
+            }
+        });
+    }
+
+}
 
 export default new ContainerModule((
     bind: interfaces.Bind,
@@ -7,5 +27,5 @@ export default new ContainerModule((
     isBound: interfaces.IsBound,
     rebind: interfaces.Rebind,
 ) => {
-
+    bind(CommandContribution).to(SampleCommandContribution);
 });
