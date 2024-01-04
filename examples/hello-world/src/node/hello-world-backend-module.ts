eb3aa20d6185f02f4370f0a7f80600efdfa6e49f
shanehsi <shane.hsi@foxmail.com>
2024-01-04 20:45:48 +0800

    add hello-world plugin

---
+++ examples/hello-world/src/node/hello-world-backend-module.ts
@@ -0,0 +1,11 @@
+// tslint:disable:file-header
+import { ContainerModule, interfaces } from '@theia/core/shared/inversify';
+
+export default new ContainerModule((
+    bind: interfaces.Bind,
+    unbind: interfaces.Unbind,
+    isBound: interfaces.IsBound,
+    rebind: interfaces.Rebind,
+) => {
+
+});
