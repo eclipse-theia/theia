001d5b5081658202d2fa29b6052e2a8b8d83e1c5
Thomas Mäder <t.s.maeder@gmail.com>
2024-03-19 14:13:38 +0100

    Reworked patching mechanism
    Signed-off-by: Thomas Mäder <t.s.maeder@gmail.com>

---
+++ dev-packages/cli/bin/theia-patch.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+// *****************************************************************************
+// Copyright (C) 2024 STMicroelectronics and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0.
+//
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License v. 2.0 are satisfied: GNU General Public License, version 2
+// with the GNU Classpath Exception which is available at
+// https://www.gnu.org/software/classpath/license.html.
+//
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
+// *****************************************************************************
+// @ts-check
+const path = require('path');
+const cp = require('child_process');
+
+const patchesDir = path.join('.', 'node_modules', '@theia', 'cli', 'patches');
+
+console.log(`patchesdir = ${patchesDir}`);
+
+const env = Object.assign({}, process.env);
+const scriptProcess = cp.exec(`patch-package --patch-dir "${patchesDir}"`, {
+    cwd: process.cwd(),
+    env,
+
+});
+
+scriptProcess.stdout.pipe(process.stdout);
+scriptProcess.stderr.pipe(process.stderr);
