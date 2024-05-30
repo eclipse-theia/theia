998f55c7e623b1cebe2a6b029cd05a844a54b741
Jonah Iden <jonah.iden@typefox.io>
2024-05-30 09:15:55 +0200

    dont scroll into cell when click into text
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- packages/notebook/src/browser/view/notebook-cell-editor.tsx
+++ packages/notebook/src/browser/view/notebook-cell-editor.tsx
@@ -129,7 +129,7 @@ export class CellEditor extends React.Component<CellEditorProps, {}> {
             }));
             this.toDispose.push(this.editor.getControl().onDidFocusEditorText(() => {
                 this.props.notebookContextManager.onDidEditorTextFocus(true);
-                this.props.notebookModel.setSelectedCell(cell);
+                this.props.notebookModel.setSelectedCell(cell, false);
             }));
             this.toDispose.push(this.editor.getControl().onDidBlurEditorText(() => {
                 this.props.notebookContextManager.onDidEditorTextFocus(false);
