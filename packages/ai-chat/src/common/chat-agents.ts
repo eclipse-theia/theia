5368ee7447dd01f6e4f09466e280671f5bd4e682
Camille Letavernier <cletavernier@eclipsesource.com>
2024-07-29 17:47:23 +0200

    38: add mock templates to default agent

--- packages/ai-chat/src/common/chat-agents.ts
+++ packages/ai-chat/src/common/chat-agents.ts
@@ -71,7 +71,13 @@ export class DefaultChatAgent implements ChatAgent {
     name: string = 'Default Chat Agent';
     description: string = 'The default chat agent provided by Theia.';
     variables: string[] = [];
-    promptTemplates: PromptTemplate[] = [];
+    promptTemplates: PromptTemplate[] = [{
+        id: 'test-template',
+        template: 'Hello, ${name}! This is just a mock template. Nothing useful here.',
+    }, {
+        id: 'mock-template',
+        template: 'Hello, ${name}! This is just another mock template. Nothing useful here either.',
+    }];
     languageModelRequirements: Omit<LanguageModelSelector, 'agentId'>[] = [];
     locations: ChatAgentLocation[] = [];
 
