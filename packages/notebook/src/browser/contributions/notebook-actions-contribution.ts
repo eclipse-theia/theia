8d247e0e16062d83253effe4bc4efae1a51a26a3
Mark Sujew <mark.sujew@typefox.io>
2024-08-05 15:13:02 +0200

    Support find widget in notebooks (#13982)

--- packages/notebook/src/browser/contributions/notebook-actions-contribution.ts
+++ packages/notebook/src/browser/contributions/notebook-actions-contribution.ts
@@ -83,6 +83,11 @@ export namespace NotebookCommands {
         id: 'notebook.cell.paste',
         category: 'Notebook',
     });
+
+    export const NOTEBOOK_FIND = Command.toDefaultLocalizedCommand({
+        id: 'notebook.find',
+        category: 'Notebook',
+    });
 }
 
 export enum CellChangeDirection {
@@ -251,6 +256,12 @@ export class NotebookActionsContribution implements CommandContribution, MenuCon
             }
         });
 
+        commands.registerCommand(NotebookCommands.NOTEBOOK_FIND, {
+            execute: () => {
+                this.notebookEditorWidgetService.focusedEditor?.showFindWidget();
+            }
+        });
+
     }
 
     protected editableCommandHandler(execute: (notebookModel: NotebookModel) => void): CommandHandler {
@@ -326,17 +337,22 @@ export class NotebookActionsContribution implements CommandContribution, MenuCon
             {
                 command: NotebookCommands.CUT_SELECTED_CELL.id,
                 keybinding: 'ctrlcmd+x',
-                when: `!editorTextFocus && ${NOTEBOOK_EDITOR_FOCUSED} && ${NOTEBOOK_CELL_FOCUSED}`
+                when: `${NOTEBOOK_EDITOR_FOCUSED} && !inputFocus`
             },
             {
                 command: NotebookCommands.COPY_SELECTED_CELL.id,
                 keybinding: 'ctrlcmd+c',
-                when: `!editorTextFocus && ${NOTEBOOK_EDITOR_FOCUSED} && ${NOTEBOOK_CELL_FOCUSED}`
+                when: `${NOTEBOOK_EDITOR_FOCUSED} && !inputFocus`
             },
             {
                 command: NotebookCommands.PASTE_CELL.id,
                 keybinding: 'ctrlcmd+v',
-                when: `!editorTextFocus && ${NOTEBOOK_EDITOR_FOCUSED}`
+                when: `${NOTEBOOK_EDITOR_FOCUSED} && !inputFocus`
+            },
+            {
+                command: NotebookCommands.NOTEBOOK_FIND.id,
+                keybinding: 'ctrlcmd+f',
+                when: `${NOTEBOOK_EDITOR_FOCUSED}`
             },
         );
     }
