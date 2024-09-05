4f6f158bd27c5dd608dddc036910698a51d8c953
Thomas Mäder <tsmaeder@users.noreply.github.com>
2024-09-05 14:58:15 +0200

    Fix selection of contributed menu action argument adapters (#14132)
    Fixes #14072
    
    Contributed on behalf of STMicroelectronics
    
    Signed-off-by: Thomas Mäder <t.s.maeder@gmail.com>om>

--- packages/core/src/browser/shell/tab-bar-toolbar/tab-bar-toolbar-menu-adapters.ts
+++ packages/core/src/browser/shell/tab-bar-toolbar/tab-bar-toolbar-menu-adapters.ts
@@ -20,7 +20,7 @@ import { NAVIGATION, RenderedToolbarItem } from './tab-bar-toolbar-types';
 export const TOOLBAR_WRAPPER_ID_SUFFIX = '-as-tabbar-toolbar-item';
 
 export class ToolbarMenuNodeWrapper implements RenderedToolbarItem {
-    constructor(protected readonly menuNode: MenuNode, readonly group?: string, readonly menuPath?: MenuPath) { }
+    constructor(protected readonly menuNode: MenuNode, readonly group: string | undefined, readonly delegateMenuPath: MenuPath, readonly menuPath?: MenuPath) { }
     get id(): string { return this.menuNode.id + TOOLBAR_WRAPPER_ID_SUFFIX; }
     get command(): string { return this.menuNode.command ?? ''; };
     get icon(): string | undefined { return this.menuNode.icon; }
