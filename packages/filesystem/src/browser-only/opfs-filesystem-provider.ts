8fa511ba01df2676567209a9f9a4e4e88cd8f8d2
Robert Jandow <robert.jandow@tum.de>
2024-10-17 13:20:17 +0200

    Fix linting issues

--- packages/filesystem/src/browser-only/opfs-filesystem-provider.ts
+++ packages/filesystem/src/browser-only/opfs-filesystem-provider.ts
@@ -1,3 +1,19 @@
+// *****************************************************************************
+// Copyright (C) 2024 EclipseSource and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0.
+//
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License v. 2.0 are satisfied: GNU General Public License, version 2
+// with the GNU Classpath Exception which is available at
+// https://www.gnu.org/software/classpath/license.html.
+//
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
+// *****************************************************************************
+
 import { inject, injectable, postConstruct } from '@theia/core/shared/inversify';
 import {
     FileChange, FileChangeType, FileDeleteOptions,
@@ -222,12 +238,13 @@ export class OPFSFileSystemProvider implements FileSystemProviderWithFileReadWri
         }
     }
 
-    /** MARK: copy
+    /**
      * Returns the FileSystemHandle for the given resource given by a URI.
      * @param resource URI/path of the resource
      * @param options Options for the creation of the handle while traversing the path
      * @returns FileSystemHandle for the given resource
      */
+    // MARK: copy
     private async toFileSystemHandle(resource: URI, options?: CreateFileSystemHandleOptions): Promise<FileSystemHandle> {
         const pathParts = resource.path.toString().split(Path.separator).filter(Boolean);
 
