ea62f67ded6e53f6c7d5a6986ba085f343970396
Mark Sujew <mark.sujew@typefox.io>
2024-10-30 14:17:28 +0100

    Improve preference renderer linking (#14311)

--- packages/preferences/src/browser/views/components/preference-markdown-renderer.ts
+++ packages/preferences/src/browser/views/components/preference-markdown-renderer.ts
@@ -18,12 +18,17 @@ import { inject, injectable } from '@theia/core/shared/inversify';
 import { PreferenceTreeModel } from '../../preference-tree-model';
 import { PreferenceTreeLabelProvider } from '../../util/preference-tree-label-provider';
 import * as markdownit from '@theia/core/shared/markdown-it';
+import { CommandRegistry } from '@theia/core';
 
 @injectable()
 export class PreferenceMarkdownRenderer {
 
-    @inject(PreferenceTreeModel) protected readonly model: PreferenceTreeModel;
-    @inject(PreferenceTreeLabelProvider) protected readonly labelProvider: PreferenceTreeLabelProvider;
+    @inject(PreferenceTreeModel)
+    protected readonly model: PreferenceTreeModel;
+    @inject(PreferenceTreeLabelProvider)
+    protected readonly labelProvider: PreferenceTreeLabelProvider;
+    @inject(CommandRegistry)
+    protected readonly commandRegistry: CommandRegistry;
 
     protected _renderer?: markdownit;
 
@@ -47,19 +52,26 @@ export class PreferenceMarkdownRenderer {
         engine.renderer.rules.code_inline = (tokens, idx, options, env, self) => {
             const token = tokens[idx];
             const content = token.content;
-            if (content.startsWith('#') && content.endsWith('#')) {
-                const preferenceId = content.substring(1, content.length - 1);
-                const preferenceNode = this.model.getNodeFromPreferenceId(preferenceId);
+            if (content.length > 2 && content.startsWith('#') && content.endsWith('#')) {
+                const id = content.substring(1, content.length - 1);
+                // First check whether there's a preference with the given ID
+                const preferenceNode = this.model.getNodeFromPreferenceId(id);
                 if (preferenceNode) {
                     let name = this.labelProvider.getName(preferenceNode);
                     const prefix = this.labelProvider.getPrefix(preferenceNode, true);
                     if (prefix) {
                         name = prefix + name;
                     }
-                    return `<a title="${preferenceId}" href="preference:${preferenceId}">${name}</a>`;
-                } else {
-                    console.warn(`Linked preference "${preferenceId}" not found.`);
+                    return `<a title="${id}" href="preference:${id}">${name}</a>`;
                 }
+                // If no preference was found, check whether there's a command with the given ID
+                const command = this.commandRegistry.getCommand(id);
+                if (command) {
+                    const name = `${command.category ? `${command.category}: ` : ''}${command.label}`;
+                    return `<span class="command-link" title="${id}">${name}</span>`;
+                }
+                // If nothing was found, print a warning
+                console.warn(`Linked preference "${id}" not found.`);
             }
             return inlineCode ? inlineCode(tokens, idx, options, env, self) : '';
         };
