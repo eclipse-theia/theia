c79551358735fa23d615c599359a92d93de9131f
Jonah Iden <jonah.iden@typefox.io>
2024-12-06 13:05:45 +0100

    show local files button
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- packages/remote/src/electron-browser/remote-electron-file-dialog-service.ts
+++ packages/remote/src/electron-browser/remote-electron-file-dialog-service.ts
@@ -14,13 +14,14 @@
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
 // *****************************************************************************
 
-import { MaybeArray, URI } from '@theia/core';
+import { MaybeArray, URI, nls } from '@theia/core';
 import { inject, injectable } from '@theia/core/shared/inversify';
 import { OpenFileDialogProps, SaveFileDialogProps } from '@theia/filesystem/lib/browser/file-dialog';
 import { FileStat } from '@theia/filesystem/lib/common/files';
 import { DefaultFileDialogService } from '@theia/filesystem/lib/browser/file-dialog/file-dialog-service';
 import { ElectronFileDialogService } from '@theia/filesystem/lib/electron-browser/file-dialog/electron-file-dialog-service';
 import { RemoteService } from './remote-service';
+import { LOCAL_FILE_SCHEME } from './local-backend-services';
 
 @injectable()
 export class RemoteElectronFileDialogService extends ElectronFileDialogService {
@@ -31,6 +32,7 @@ export class RemoteElectronFileDialogService extends ElectronFileDialogService {
     override showOpenDialog(props: OpenFileDialogProps, folder?: FileStat | undefined): Promise<URI | undefined>;
     override showOpenDialog(props: OpenFileDialogProps, folder?: FileStat): Promise<MaybeArray<URI> | undefined> | Promise<URI | undefined> {
         if (this.remoteService.isConnected()) {
+            this.addLocalFilesButton(props);
             return DefaultFileDialogService.prototype.showOpenDialog.call(this, props, folder);
         } else {
             return super.showOpenDialog(props, folder);
@@ -42,6 +44,22 @@ export class RemoteElectronFileDialogService extends ElectronFileDialogService {
             return DefaultFileDialogService.prototype.showSaveDialog.call(this, props, folder);
         } else {
             return super.showSaveDialog(props, folder);
+        }
+    }
+
+    protected addLocalFilesButton(props: OpenFileDialogProps): void {
+        const localFilesButton: [string, (res: typeof Promise.resolve) => void] = ['Show Local Files', async resolve => {
+            const localFile = await super.showOpenDialog({ ...props, title: nls.localizeByDefault('Show Local'), fileScheme: LOCAL_FILE_SCHEME });
+            if (localFile) {
+                resolve({ uri: localFile });
+            } else {
+                resolve(undefined);
+            }
+        }];
+        if (props.additionalButtons) {
+            props.additionalButtons.push(localFilesButton);
+        } else {
+            props.additionalButtons = [localFilesButton];
         }
     }
 }
