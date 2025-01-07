a1bc7e8e00b56e04ad5fd0d80a8fe79aaacf3aa0
Robert Jandow <robert.jandow@tum.de>
2025-01-07 18:05:56 +0100

    Rename mock plugin metadata to static plugin metadata

--- examples/api-samples/src/browser-only/plugin-sample/example-plugin-initialization.ts
+++ examples/api-samples/src/browser-only/plugin-sample/example-plugin-initialization.ts
@@ -1,11 +1,11 @@
 import { interfaces } from '@theia/core/shared/inversify';
 import { PluginLocalOptions } from '@theia/plugin-ext/lib/hosted/browser-only/frontend-hosted-plugin-server';
-import { mockData } from './mock-plugin-metadata';
+import { staticMetadata } from './example-static-plugin-metadata';
 
 
 export const bindPluginInitialization = (bind: interfaces.Bind, rebind: interfaces.Rebind): void => {
     const pluginLocalOptions = {
-        pluginMetadata: mockData,
+        pluginMetadata: staticMetadata,
     };
     bind(PluginLocalOptions).toConstantValue(pluginLocalOptions);
 };
