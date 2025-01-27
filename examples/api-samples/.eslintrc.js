6cacac481e428d7281ed98f1d5d6cf3bb08698f5
Robert Jandow <robert.jandow@tum.de>
2025-01-27 15:20:37 +0100

    More linting fixes

--- examples/api-samples/.eslintrc.js
+++ examples/api-samples/.eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
     extends: [
         '../../configs/build.eslintrc.json'
     ],
+    ignores: [
+        "./src/browser-only/plugin-sample/example-static-plugin-metadata.ts" // Ignoring this file as it only contains static metadata
+    ],
     parserOptions: {
         tsconfigRootDir: __dirname,
         project: 'tsconfig.json'
