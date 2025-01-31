ec01e70d87ce863dc861c3c9dd4ba71a5b2c9230
Robert Jandow <robert.jandow@tum.de>
2025-01-31 14:25:26 +0100

    Update copyright information

--- packages/terminal/src/browser-only/terminal-frontend-only-module.ts
+++ packages/terminal/src/browser-only/terminal-frontend-only-module.ts
@@ -1,5 +1,5 @@
 // *****************************************************************************
-// Copyright (C) 2017 TypeFox and others.
+// Copyright (C) 2024 robertjndw
 //
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v. 2.0 which is available at
@@ -13,7 +13,6 @@
 //
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
 // *****************************************************************************
-
 
 import { ContainerModule } from '@theia/core/shared/inversify';
 import { TerminalFrontendOnlyContribution } from './terminal-frontend-only-contribution';
