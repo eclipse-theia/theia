syntax-syndicate/theia-ide-framework

Support manual override of text blocks

examples/api-samples/src/browser/api-samples-preload-module.ts
@@ -1,0 +1,23 @@
+// *****************************************************************************
+// Copyright (C) 2024 TypeFox and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0.
+//
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License v. 2.0 are satisfied: GNU General Public License, version 2
+// with the GNU Classpath Exception which is available at
+// https://www.gnu.org/software/classpath/license.html.
+//
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
+// *****************************************************************************
+
+import { ContainerModule } from '@theia/core/shared/inversify';
+import { I18nReplacementContribution } from '@theia/core/lib/browser/preload/i18n-replacement-contribution';
+import { I18nSampleReplacementContribution } from './i18n/i18n-replacement-sample';
+
+export default new ContainerModule(bind => {
+    bind(I18nReplacementContribution).to(I18nSampleReplacementContribution).inSingletonScope();
+});

packages/core/src/browser/preload/preload-module.ts
@@ -21,19 +21,21 @@ import { I18nPreloadContribution } from './i18n-preload-contribution';
 import { OSPreloadContribution } from './os-preload-contribution';
 import { ThemePreloadContribution } from './theme-preload-contribution';
 import { LocalizationServer, LocalizationServerPath } from '../../common/i18n/localization-server';
-import { WebSocketConnectionProvider } from '../messaging/ws-connection-provider';
+import { ServiceConnectionProvider } from '../messaging/service-connection-provider';
 import { OSBackendProvider, OSBackendProviderPath } from '../../common/os';
+import { I18nReplacementContribution } from './i18n-replacement-contribution';
 
 export default new ContainerModule(bind => {
     bind(Preloader).toSelf().inSingletonScope();
     bindContributionProvider(bind, PreloadContribution);
+    bindContributionProvider(bind, I18nReplacementContribution);
 
     bind(LocalizationServer).toDynamicValue(ctx =>
-        WebSocketConnectionProvider.createProxy<LocalizationServer>(ctx.container, LocalizationServerPath)
+        ServiceConnectionProvider.createProxy<LocalizationServer>(ctx.container, LocalizationServerPath)
     ).inSingletonScope();
 
     bind(OSBackendProvider).toDynamicValue(ctx =>
-        WebSocketConnectionProvider.createProxy<OSBackendProvider>(ctx.container, OSBackendProviderPath)
+        ServiceConnectionProvider.createProxy<OSBackendProvider>(ctx.container, OSBackendProviderPath)
     ).inSingletonScope();
 
     bind(I18nPreloadContribution).toSelf().inSingletonScope();

eclipse-theia/theia

examples/api-samples/src/browser/api-samples-preload-module.ts
@@ -1,0 +1,23 @@
+// *****************************************************************************
+// Copyright (C) 2025 TypeFox and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0.
+//
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License v. 2.0 are satisfied: GNU General Public License, version 2
+// with the GNU Classpath Exception which is available at
+// https://www.gnu.org/software/classpath/license.html.
+//
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
+// *****************************************************************************
+
+import { ContainerModule } from '@theia/core/shared/inversify';
+import { TextReplacementContribution } from '@theia/core/lib/browser/preload/text-replacement-contribution';
+import { TextSampleReplacementContribution } from './preload/text-replacement-sample';
+
+export default new ContainerModule(bind => {
+    bind(TextReplacementContribution).to(TextSampleReplacementContribution).inSingletonScope();
+});

packages/core/src/browser/preload/preload-module.ts
@@ -21,19 +21,21 @@ import { I18nPreloadContribution } from './i18n-preload-contribution';
 import { OSPreloadContribution } from './os-preload-contribution';
 import { ThemePreloadContribution } from './theme-preload-contribution';
 import { LocalizationServer, LocalizationServerPath } from '../../common/i18n/localization-server';
-import { WebSocketConnectionProvider } from '../messaging/ws-connection-provider';
+import { ServiceConnectionProvider } from '../messaging/service-connection-provider';
 import { OSBackendProvider, OSBackendProviderPath } from '../../common/os';
+import { TextReplacementContribution } from './text-replacement-contribution';
 
 export default new ContainerModule(bind => {
     bind(Preloader).toSelf().inSingletonScope();
     bindContributionProvider(bind, PreloadContribution);
+    bindContributionProvider(bind, TextReplacementContribution);
 
     bind(LocalizationServer).toDynamicValue(ctx =>
-        WebSocketConnectionProvider.createProxy<LocalizationServer>(ctx.container, LocalizationServerPath)
+        ServiceConnectionProvider.createProxy<LocalizationServer>(ctx.container, LocalizationServerPath)
     ).inSingletonScope();
 
     bind(OSBackendProvider).toDynamicValue(ctx =>
-        WebSocketConnectionProvider.createProxy<OSBackendProvider>(ctx.container, OSBackendProviderPath)
+        ServiceConnectionProvider.createProxy<OSBackendProvider>(ctx.container, OSBackendProviderPath)
     ).inSingletonScope();
 
     bind(I18nPreloadContribution).toSelf().inSingletonScope();