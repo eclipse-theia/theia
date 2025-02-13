df5ec4dd2ac57a5e7c4a6940210b02a8c0278509
Akos Kitta <kittaakos@gmail.com>
2025-02-13 16:50:53 +0100

    fix(doc): remove incorrect default value

--- packages/debug/src/common/debug-configuration.ts
+++ packages/debug/src/common/debug-configuration.ts
@@ -85,7 +85,7 @@ export interface DebugConfiguration {
     /** When true, the debug viewlet will not be automatically revealed for this session. */
     suppressDebugView?: boolean;
 
-    /** Disable the warning when trying to start the same debug configuration more than once. `true` by default. */
+    /** Disable the warning when trying to start the same debug configuration more than once. */
     suppressMultipleSessionWarning?: boolean;
 }
 export namespace DebugConfiguration {
