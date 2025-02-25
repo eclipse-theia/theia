98e08f51206e11b1a728461062c33fc13b40ed94
Mark Sujew <mark.sujew@typefox.io>
2025-02-25 10:28:56 +0100

    Improve documentation

--- packages/core/src/browser/preload/text-replacement-contribution.ts
+++ packages/core/src/browser/preload/text-replacement-contribution.ts
@@ -16,6 +16,38 @@
 
 export const TextReplacementContribution = Symbol('TextReplacementContribution');
 
+/**
+ * Enables adopters to override text in the application. All `TextReplacementContribution`s need to be bound in the `frontendPreload` scope of the package.json.
+ *
+ * @example Create a text replacement contribution
+ * ```typescript
+ *          import { TextReplacementContribution } from '@theia/core/lib/browser/preload/text-replacement-contribution';
+ *          export class TextSampleReplacementContribution implements TextReplacementContribution {
+ *              getReplacement(locale: string): Record<string, string> {
+ *                  switch (locale) {
+ *                      case 'en': {
+ *                          return {
+ *                              'About': 'About Theia',
+ *                          };
+ *                      }
+ *                      case 'de': {
+ *                          return {
+ *                              'About': 'Über Theia',
+ *                          };
+ *                      }
+ *                  }
+ *                  return {};
+ *              }
+ *          }
+ * ```
+ */
 export interface TextReplacementContribution {
+    /**
+     * This method returns a map of **default values** and their replacement values for the specified locale.
+     * **Do not** use the keys of the `nls.localization` call, but the English default values.
+     *
+     * @param locale The locale for which the replacement should be returned.
+     * @returns A map of default values and their replacement values.
+     */
     getReplacement(locale: string): Record<string, string>;
 }
