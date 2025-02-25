a3c8d4df6e31d85b415d51404b222ccb65d61ebe
Florian Richter <florian.richter@mvtec.com>
2025-02-25 17:08:27 +0100

    Await PreferenceService to be ready
    Signed-off-by: Florian Richter <florian.richter@mvtec.com>

--- packages/ai-core/src/browser/ai-activation-service.ts
+++ packages/ai-core/src/browser/ai-activation-service.ts
@@ -15,7 +15,7 @@
 // *****************************************************************************
 import { inject, injectable } from '@theia/core/shared/inversify';
 import { FrontendApplicationContribution, PreferenceService } from '@theia/core/lib/browser';
-import { Emitter, MaybePromise, Event, } from '@theia/core';
+import { Emitter, Event } from '@theia/core';
 import { ContextKeyService, ContextKey } from '@theia/core/lib/browser/context-key-service';
 import { PREFERENCE_NAME_ENABLE_AI } from './ai-core-preferences';
 
@@ -44,7 +44,8 @@ export class AIActivationService implements FrontendApplicationContribution {
         return this.isAiEnabledKey.get() ?? false;
     }
 
-    initialize(): MaybePromise<void> {
+    async initialize(): Promise<void> {
+        await this.preferenceService.ready;
         const value = this.preferenceService.get<boolean>(ENABLE_AI_CONTEXT_KEY);
         this.isAiEnabledKey = this.contextKeyService.createKey(ENABLE_AI_CONTEXT_KEY, value);
         this.preferenceService.onPreferenceChanged(e => {
