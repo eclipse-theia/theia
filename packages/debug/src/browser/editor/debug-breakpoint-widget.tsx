076c5ff97b1dd61be9cbd882c31c7d37de6f001b
Thomas Mäder <t.s.maeder@gmail.com>
2025-04-24 10:39:40 +0200

    Reacto to editor model changing.
    Fixes, #15498, #15497, #15496
    
    Signed-off-by: Thomas Mäder <t.s.maeder@gmail.com>

--- packages/debug/src/browser/editor/debug-breakpoint-widget.tsx
+++ packages/debug/src/browser/editor/debug-breakpoint-widget.tsx
@@ -161,8 +161,11 @@ export class DebugBreakpointWidget implements Disposable {
                 }
             }));
         this.toDispose.push(this.zone.onDidLayoutChange(dimension => this.layout(dimension)));
+        this.toDispose.push(this.editor.getControl().onDidChangeModel(() => {
+            this.zone.hide();
+        }));
         this.toDispose.push(input.getControl().onDidChangeModelContent(() => {
-            const heightInLines = input.getControl().getModel()?.getLineCount() || 0 + 1;
+            const heightInLines = (input.getControl().getModel()?.getLineCount() || 0) + 1;
             this.zone.layout(heightInLines);
             this.updatePlaceholder();
         }));
