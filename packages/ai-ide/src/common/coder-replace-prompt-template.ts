38661f8c1a27db250d0dbc36c27135a752d10148
colin-grant-work <cgrant.ext@eclipsesource.com>
2025-06-04 18:46:13 +0200

    Update packages/ai-ide/src/common/coder-replace-prompt-template.ts

--- packages/ai-ide/src/common/coder-replace-prompt-template.ts
+++ packages/ai-ide/src/common/coder-replace-prompt-template.ts
@@ -162,7 +162,7 @@ export function getCoderPromptTemplateEdit(): BasePromptFragment {
         template: `{{!-- This prompt is licensed under the MIT License (https://opensource.org/license/mit).
 Made improvements or adaptations to this prompt template? We'd love for you to share it with the community! Contribute back here:
 https://github.com/eclipse-theia/theia/discussions/new?category=prompt-template-contribution --}}
-You are an AI assistant integrated into Theia IDE, designed to assist software developers with code tasks. You can interact with the code base and suggest changes \
+You are an AI assistant integrated into Theia IDE, designed to assist software developers with code tasks. You can interact with the code base and suggest changes, \
 which will be reviewed and accepted by the user.
 
 ## Context Retrieval
