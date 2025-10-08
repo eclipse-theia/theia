0a827895586966397272e7af7ac69ce636d2ff44
Jonas Helming <jhelming@eclipsesource.com>
2025-10-08 09:22:41 +0200

    Update packages/ai-ide/src/common/task-context-prompt-template.ts
    Co-authored-by: Philip Langer <planger@eclipsesource.com>

--- packages/ai-ide/src/common/task-context-prompt-template.ts
+++ packages/ai-ide/src/common/task-context-prompt-template.ts
@@ -1,17 +1,12 @@
+/* eslint-disable @typescript-eslint/tslint/config */
 // *****************************************************************************
-// Copyright (C) 2024 EclipseSource GmbH.
+// Copyright (C) 2025 EclipseSource GmbH and others.
 //
-// This program and the accompanying materials are made available under the
-// terms of the Eclipse Public License v. 2.0 which is available at
-// http://www.eclipse.org/legal/epl-2.0.
+// This file is licensed under the MIT License.
+// See LICENSE-MIT.txt in the project root for license information.
+// https://opensource.org/license/mit.
 //
-// This Source Code may also be made available under the following Secondary
-// Licenses when the conditions for such availability set forth in the Eclipse
-// Public License v. 2.0 are satisfied: GNU General Public License, version 2
-// with the GNU Classpath Exception which is available at
-// https://www.gnu.org/software/classpath/license.html.
-//
-// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
+// SPDX-License-Identifier: MIT
 // *****************************************************************************
 import { PromptVariantSet } from '@theia/ai-core/lib/common';
 import { TASK_CONTEXT_SUMMARY_VARIABLE_ID } from './context-variables';
