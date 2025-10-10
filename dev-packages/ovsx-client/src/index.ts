0e85731b8d6b22ec862ca4ef7a35a9b81a744107
Nina Doschek <ndoschek@eclipsesource.com>
2025-10-10 20:19:25 +0200

    chore: move ovsx-client test code into dedicated folder
    - according to coding guidelines

--- dev-packages/ovsx-client/src/index.ts
+++ dev-packages/ovsx-client/src/index.ts
@@ -16,7 +16,7 @@
 
 export { OVSXApiFilter, OVSXApiFilterImpl, OVSXApiFilterProvider } from './ovsx-api-filter';
 export { OVSXHttpClient, OVSX_RATE_LIMIT } from './ovsx-http-client';
-export { OVSXMockClient } from './ovsx-mock-client';
+export { OVSXMockClient } from './test/ovsx-mock-client';
 export { OVSXRouterClient, OVSXRouterConfig, OVSXRouterFilterFactory as FilterFactory } from './ovsx-router-client';
 export * from './ovsx-router-filters';
 export * from './ovsx-types';
