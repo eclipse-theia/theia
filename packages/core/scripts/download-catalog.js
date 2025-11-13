32774aaeba91ad32de6cca0e5f0b69ce03fa9ff3
Stefan Dirix <sdirix@eclipsesource.com>
2025-11-13 14:10:41 +0100

    readd path import

--- packages/core/scripts/download-catalog.js
+++ packages/core/scripts/download-catalog.js
@@ -13,7 +13,7 @@
 //
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
 // *****************************************************************************
-
+const path = require('path');
 const { Downloader } = require('nodejs-file-downloader');
 
 const url = 'https://schemastore.org/api/json/catalog.json';
