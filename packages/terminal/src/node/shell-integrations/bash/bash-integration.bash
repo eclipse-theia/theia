16df7990fd360727f7f45ac02b2b14f73bd5f875
Fangxing Liu <fx.liu@tum.de>
2025-12-09 14:55:31 +0100

    feat: add sources to license banner
    Signed-off-by: Fangxing Liu <fx.liu@tum.de>

--- packages/terminal/src/node/shell-integrations/bash/bash-integration.bash
+++ packages/terminal/src/node/shell-integrations/bash/bash-integration.bash
@@ -13,6 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions
 # and limitations under the License.
+#
+# Source:
+# https://github.com/JetBrains/intellij-community/blob/master/plugins/terminal/resources/shell-integrations/bash/bash-integration.bash
 # *****************************************************************************
 
 echo 'test'
