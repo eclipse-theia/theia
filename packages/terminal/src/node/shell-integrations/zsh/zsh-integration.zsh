42642ee0a65ce99f47944dc070ec58b85458c371
Fangxing Liu <fx.liu@tum.de>
2025-12-12 00:46:24 +0100

    chore: add changes list to all shell scripts by jetbrains
    Signed-off-by: Fangxing Liu <fx.liu@tum.de>

--- packages/terminal/src/node/shell-integrations/zsh/zsh-integration.zsh
+++ packages/terminal/src/node/shell-integrations/zsh/zsh-integration.zsh
@@ -13,6 +13,12 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 #
+# Modifications:
+# - Adapted directory variable: `JETBRAINS_INTELLIJ_ZSH_DIR` -> `THEIA_ZSH_DIR`.
+# - Removed IntelliJ session bootstrap (JEDITERM_SOURCE and `_INTELLIJ_FORCE_SET_*` / `_INTELLIJ_FORCE_PREPEND_*` handling,
+#   plus the one-time `precmd_functions` hook injection).
+# - Removed legacy command block support handling.
+#
 # Source: 
 # https://github.com/JetBrains/intellij-community/blob/8d02751ced444e5b70784fe0a757f960fe495a67/plugins/terminal/resources/shell-integrations/zsh/zsh-integration.zsh
 # *****************************************************************************
