34fab0fe2fced5bc9d23384e611feceb03e2e61d
Fangxing Liu <fx.liu@tum.de>
2025-12-13 01:55:48 +0100

    chore: add console.debug for current history after each executed command
    Signed-off-by: Fangxing Liu <fx.liu@tum.de>

--- packages/terminal/src/browser/terminal-widget-impl.ts
+++ packages/terminal/src/browser/terminal-widget-impl.ts
@@ -1094,6 +1094,7 @@ export class TerminalWidgetImpl extends TerminalWidget implements StatefulWidget
                     command: this.currentCommand,
                     output: this.sanitizeCommandOutput(this.commandOutputBuffer)
                 };
+                console.debug('Current command history:', this._commandHistory);
                 console.debug('Terminal command result captured:', terminalBlock);
                 this._commandHistory.push(terminalBlock);
                 this.resetCommandHistoryState();
