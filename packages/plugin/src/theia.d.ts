81694823614bef837adae9f2b0adb7771c729eda
Fangxing Liu <fx.liu@tum.de>
2026-01-07 23:47:22 +0100

    chore: fix trailing whitespase
    Signed-off-by: Fangxing Liu <fx.liu@tum.de>

--- packages/plugin/src/theia.d.ts
+++ packages/plugin/src/theia.d.ts
@@ -12642,7 +12642,7 @@ export module '@theia/plugin' {
         createResourceGroup(id: string, label: string): SourceControlResourceGroup;
 
         /*
-         * Optional action button displayed under the source control's input box. 
+         * Optional action button displayed under the source control's input box.
          */
         actionButton?: ScmActionButton;
 
