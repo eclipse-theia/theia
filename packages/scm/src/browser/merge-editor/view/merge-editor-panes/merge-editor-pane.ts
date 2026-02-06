47d924a11bcb2031618869b12bd0a7cbd2b03c85
Vladimir Piskarev <pisv@1c.ru>
2026-02-06 17:36:57 +0300

    [merge editor] Improve merge conflict revealing

--- packages/scm/src/browser/merge-editor/view/merge-editor-panes/merge-editor-pane.ts
+++ packages/scm/src/browser/merge-editor/view/merge-editor-panes/merge-editor-pane.ts
@@ -130,11 +130,11 @@ export abstract class MergeEditorPane extends BoxPanel {
 
     goToMergeRange(mergeRange: MergeRange, options?: { reveal?: boolean }): void {
         const { editor } = this;
-        const { startLineNumber, endLineNumberExclusive } = this.getLineRangeForMergeRange(mergeRange);
+        const { startLineNumber } = this.getLineRangeForMergeRange(mergeRange);
         editor.cursor = { line: startLineNumber, character: 0 };
         const reveal = options?.reveal ?? true;
         if (reveal) {
-            editor.getControl().revealLinesNearTop(startLineNumber + 1, endLineNumberExclusive + 1);
+            editor.getControl().setScrollTop(editor.getControl().getTopForLineNumber(startLineNumber + 1, true));
         }
     }
 
