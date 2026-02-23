9ba569a8f4879f38111fc15c31816148e0eb1abd
Nina Doschek <ndoschek@eclipsesource.com>
2026-02-23 19:23:46 +0100

    [vscode] bump API compatibility to 1.109.4
    Resolves GH-17046

--- dev-packages/application-package/src/api.ts
+++ dev-packages/application-package/src/api.ts
@@ -18,4 +18,4 @@
  * The default supported API version the framework supports.
  * The version should be in the format `x.y.z`.
  */
-export const DEFAULT_SUPPORTED_API_VERSION = '1.108.0';
+export const DEFAULT_SUPPORTED_API_VERSION = '1.109.4';
