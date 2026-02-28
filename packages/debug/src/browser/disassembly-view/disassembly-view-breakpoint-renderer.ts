b5b8732c0964588897024a6a241cf707400c9414
Colin Grant <cgrant.ext@eclipsesource.com>
2026-02-28 10:06:33 -0700

    Update license years

--- packages/debug/src/browser/disassembly-view/disassembly-view-breakpoint-renderer.ts
+++ packages/debug/src/browser/disassembly-view/disassembly-view-breakpoint-renderer.ts
@@ -1,5 +1,5 @@
 // *****************************************************************************
-// Copyright (C) 2022 Ericsson and others.
+// Copyright (C) 2022-2026 Ericsson and others.
 //
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v. 2.0 which is available at
--- packages/plugin-ext/src/main/browser/debug/debug-main.ts
+++ packages/plugin-ext/src/main/browser/debug/debug-main.ts
@@ -1,5 +1,5 @@
 // *****************************************************************************
-// Copyright (C) 2018 Red Hat, Inc. and others.
+// Copyright (C) 2018-2026 Red Hat, Inc. and others.
 //
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v. 2.0 which is available at
