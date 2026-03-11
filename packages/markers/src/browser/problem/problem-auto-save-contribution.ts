ab6b03076f2a021a25e151dfe82773ea8fb66c31
safi <safi@k2view.com>
2026-03-11 15:07:19 +0200

    update copyright

--- packages/markers/src/browser/problem/problem-auto-save-contribution.ts
+++ packages/markers/src/browser/problem/problem-auto-save-contribution.ts
@@ -1,5 +1,5 @@
 // *****************************************************************************
-// Copyright (C) 2026 and others.
+// Copyright (C) 2026 Safi Seid-Ahmad, K2view.
 //
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v. 2.0 which is available at
