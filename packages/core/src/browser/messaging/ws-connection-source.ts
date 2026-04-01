f0fc573be7820bd0202b465fe4193bc75231d773
Mark Sujew <mark.sujew@typefox.io>
2026-04-01 10:39:01 +0200

    Limit imports of code in the preload section

--- packages/core/src/browser/messaging/ws-connection-source.ts
+++ packages/core/src/browser/messaging/ws-connection-source.ts
@@ -14,17 +14,19 @@
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
 // *****************************************************************************
 
-import { AbstractChannel, Channel, Disposable, DisposableCollection, Emitter, Event, servicesPath } from '../../common';
+import { Emitter, Event } from '../../common/event';
 import { ConnectionSource } from './connection-source';
 import { Socket, io } from 'socket.io-client';
 import { Endpoint } from '../endpoint';
-import { ForwardingChannel } from '../../common/message-rpc/channel';
+import { AbstractChannel, Channel, ForwardingChannel } from '../../common/message-rpc/channel';
 import { Uint8ArrayReadBuffer, Uint8ArrayWriteBuffer } from '../../common/message-rpc/uint8-array-message-buffer';
 import { inject, injectable, postConstruct } from 'inversify';
 import { FrontendIdProvider } from './frontend-id-provider';
 import { FrontendApplicationConfigProvider } from '../frontend-application-config-provider';
 import { SocketWriteBuffer } from '../../common/messaging/socket-write-buffer';
 import { ConnectionManagementMessages } from '../../common/messaging/connection-management';
+import { servicesPath } from '../../common/messaging/handler';
+import { Disposable, DisposableCollection } from '../../common/disposable';
 
 @injectable()
 export class WebSocketConnectionSource implements ConnectionSource {
