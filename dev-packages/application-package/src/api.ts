7397d0816893c25d7118b94d6576be5a7d1c993a
Nina Doschek <ndoschek@eclipsesource.com>
2026-04-17 13:14:29 +0200

    [vscode] bump API compatibility to 1.116.0
    Resolves GH-17365
    
    Contributed on behalf of STMicroelectronics

--- dev-packages/application-package/src/api.ts
+++ dev-packages/application-package/src/api.ts
@@ -18,7 +18,7 @@
  * The default supported API version the framework supports.
  * The version should be in the format `x.y.z`.
  */
-export const DEFAULT_SUPPORTED_API_VERSION = '1.110.1';
+export const DEFAULT_SUPPORTED_API_VERSION = '1.116.0';
 
 /**
  * The default supported monaco editor version the framework supports.
